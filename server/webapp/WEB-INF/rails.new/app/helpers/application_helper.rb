3620b6bf0d950b6e2434fdad1ad406028c410822
Sharanya <sharanya.bathey@gmail.com>
2014-05-21 11:50:28 +0530

    #130 - Including JavaImports in application_controller

--- server/webapp/WEB-INF/rails.new/app/helpers/application_helper.rb
+++ server/webapp/WEB-INF/rails.new/app/helpers/application_helper.rb
@@ -178,6 +178,10 @@ module ApplicationHelper
     system_environment.use_compressed_js()
   end
 
+  def current_user
+    instance_variable_get(:@user)
+  end
+
   def can_view_admin_page?
     security_service.canViewAdminPage(current_user)
   end

b65e28f56ebd602f431ca56a096e1126676b60ed
Sharanya <sharanya.bathey@gmail.com>
2014-05-26 13:48:03 +0530

    Revert "Workaround for specs to pass."
    This reverts commit cced4692fb04140e381b9412cec22b9517528b84.

--- server/webapp/WEB-INF/rails.new/spec/spec_helper.rb
+++ server/webapp/WEB-INF/rails.new/spec/spec_helper.rb
@@ -55,12 +55,7 @@ include JavaSpecImports
 def java_date_utc(year, month, day, hour, minute, second)
   org.joda.time.DateTime.new(year, month, day, hour, minute, second, 0, org.joda.time.DateTimeZone::UTC).toDate()
 end
-
 def stub_server_health_messages
-  assign(:current_server_health_states, com.thoughtworks.go.serverhealth.ServerHealthStates.new)
-end
-
-def stub_server_health_messages_for_controllers
   assigns[:current_server_health_states] = com.thoughtworks.go.serverhealth.ServerHealthStates.new
 end
 

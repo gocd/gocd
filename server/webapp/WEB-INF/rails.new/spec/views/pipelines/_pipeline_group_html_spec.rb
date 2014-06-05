d29abafddd1950850146fc06cb3f1600fd6b9541
Aravind & Ali <mdaliejaz@gmail.com>
2014-06-05 10:26:10 +0530

    #130 - Move pipeline group view of dashboard to new Rails.
    * Custom cache store implementation for Rails 4 has changed a bit. read => read_entry, etc.
    * :skip_digest is a new key that should be set to true to get old behavior of cache key.

--- server/webapp/WEB-INF/rails.new/spec/views/pipelines/_pipeline_group_html_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/views/pipelines/_pipeline_group_html_spec.rb
@@ -17,9 +17,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/pipelines/_pipeline_group.html" do
+  include PipelineModelMother
+
   it "should cache pipeline partials of different pipelines separately" do
-    pipeline_group1 = pipeline_group_model_for_caching(JobState::Building, JobResult::Unknown)
-    pipeline_group2 = pipeline_group_model_for_caching(JobState::Completed, JobResult::Failed)
+    pipeline_group1 = pipeline_group_model_for_caching("pipelineName", JobState::Building, JobResult::Unknown)
+    pipeline_group2 = pipeline_group_model_for_caching("pipelineName", JobState::Completed, JobResult::Failed)
     key_proc = proc {|pipeline_group| [ViewCacheKey.new.forPipelineModelBox(pipeline_group.getPipelineModels().get(0)), {:subkey => "pipeline_html"}]}
     check_fragment_caching(pipeline_group1, pipeline_group2, key_proc) do |pipeline_group|
       render :partial => "pipelines/pipeline_group", :locals => {:scope => {:pipeline_group => pipeline_group}}
@@ -27,39 +29,58 @@ describe "/pipelines/_pipeline_group.html" do
   end
 
   it "should always show compare link" do
-    pipeline_group = pipeline_group_model_for_caching(JobState::Building, JobResult::Unknown)
+    pipeline_group = pipeline_group_model_for_caching("pipelineName", JobState::Building, JobResult::Unknown)
     render :partial => "pipelines/pipeline_group", :locals => {:scope => {:pipeline_group => pipeline_group}}
 
-    response.body.should have_tag("span.compare_pipeline")
+    expect(response.body).to have_selector("span.compare_pipeline")
   end
 
-  it "should render pipelines in the group using a partial" do
-    pipeline_group = pipeline_group_model_for_caching(JobState::Building, JobResult::Unknown)
+  it "should render a pipeline in the group using a partial" do
+    pipeline_group = pipeline_group_model_for_caching("pipelineName", JobState::Building, JobResult::Unknown)
+    common_attributes = {:should_display_previously_blurb => true, :show_controls => true, :show_changes => true, :show_compare => true}
+    stub_template "shared/_pipeline.html.erb" => "PIPELINE"
 
-    attributes = {:pipeline_model => pipeline_group.getPipelineModels().get(0), :should_display_previously_blurb => true, :show_controls => true, :show_changes => true, :show_compare => true}
-    template.should_receive(:render).with(:partial => "shared/pipeline.html", :locals => {:scope => attributes}).and_return("\"pipeline\"")
+    render :partial => "pipelines/pipeline_group", locals: {scope: {pipeline_group: pipeline_group}}
 
-    render :partial => "pipelines/pipeline_group", :locals => {:scope => {:pipeline_group => pipeline_group}}
+    assert_template partial: "shared/pipeline.html", locals: {scope: common_attributes.merge({pipeline_model: pipeline_group.getPipelineModels().get(0)})}
   end
 
-  def pipeline_group_model_for_caching(latest_pipeline_job_state, latest_pipeline_job_result)
-    model = PipelineModel.new("pipelineName", true, true, PipelinePauseInfo.notPaused())
+  it "should render multiple pipelines in the group using a partial" do
+    group_with_multiple_pipelines = PipelineGroupModel.new("group1")
+    group_with_multiple_pipelines.add(create_pipeline_model("pipelineName_1", JobState::Building, JobResult::Unknown))
+    group_with_multiple_pipelines.add(create_pipeline_model("pipelineName_2", JobState::Building, JobResult::Unknown))
+
+    common_attributes = {:should_display_previously_blurb => true, :show_controls => true, :show_changes => true, :show_compare => true}
+    stub_template "shared/_pipeline.html.erb" => "PIPELINE"
+
+    render :partial => "pipelines/pipeline_group", locals: {scope: {pipeline_group: group_with_multiple_pipelines}}
+
+    assert_template partial: "shared/pipeline.html", locals: {scope: common_attributes.merge({pipeline_model: group_with_multiple_pipelines.getPipelineModels().get(0)})}
+    assert_template partial: "shared/pipeline.html", locals: {scope: common_attributes.merge({pipeline_model: group_with_multiple_pipelines.getPipelineModels().get(1)})}
+  end
+
+  def pipeline_group_model_for_caching(pipeline_name, latest_pipeline_job_state, latest_pipeline_job_result)
+    group = PipelineGroupModel.new("group1")
+    group.add(create_pipeline_model(pipeline_name, latest_pipeline_job_state, latest_pipeline_job_result))
+    group
+  end
+
+  def create_pipeline_model(pipeline_name, latest_pipeline_job_state, latest_pipeline_job_result)
+    model = PipelineModel.new(pipeline_name, true, true, PipelinePauseInfo.notPaused())
     stages = StageInstanceModels.new
     stages.add(stage_instance("stageName", 13, latest_pipeline_job_state, latest_pipeline_job_result))
     stages.add(NullStageHistoryItem.new("stage2", true))
-    pipeline_instance = PipelineInstanceModel.createPipeline("pipelineName", 10, "label-10", BuildCause.createExternal(), stages)
+    pipeline_instance = PipelineInstanceModel.createPipeline(pipeline_name, 10, "label-10", BuildCause.createExternal(), stages)
     pipeline_instance.setId(12)
     model.addPipelineInstance(pipeline_instance)
 
     stages2 = StageInstanceModels.new()
     stages2.add(stage_instance("stageName", 7, JobState::Completed, JobResult::Passed))
     stages2.add(stage_instance("stage2", 10, JobState::Assigned, JobResult::Unknown))
-    pipeline_instance2 = PipelineInstanceModel.createPipeline("pipelineName", 7, "label-7", BuildCause.createExternal(), stages2)
+    pipeline_instance2 = PipelineInstanceModel.createPipeline(pipeline_name, 7, "label-7", BuildCause.createExternal(), stages2)
     pipeline_instance2.setId(14)
     model.addPipelineInstance(pipeline_instance2)
-    group = PipelineGroupModel.new("group1")
-    group.add(model)
-    group
+    model
   end
 
   def stage_instance(name, id, state, job_result)

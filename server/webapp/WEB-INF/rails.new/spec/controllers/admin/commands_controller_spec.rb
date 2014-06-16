65d1466159ad64e9f6268b704b8bc11a81569a39
Srini <srinivas.upadhya@gmail.com>
2014-06-16 11:51:39 +0530

    moving controllers/admin/ {commands, configuration, garage, package_definition, package_repostiories, pipelines_snippets}, controllers/admin/plugins, controllers/admin_controller, required libs & util

---
+++ server/webapp/WEB-INF/rails.new/spec/controllers/admin/commands_controller_spec.rb
@@ -0,0 +1,118 @@
+##########################GO-LICENSE-START################################
+# Copyright 2014 ThoughtWorks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################GO-LICENSE-END##################################
+
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe Admin::CommandsController do
+
+  describe "routes" do
+    it "should resolve commands" do
+      params_from(:get, "/admin/commands").should == {:controller => "admin/commands", :action => "index"}
+    end
+
+    it "should generate command" do
+      route_for(:controller => "admin/commands", :action => "index").should == "/admin/commands"
+      admin_commands_path.should == "/admin/commands"
+    end
+
+    it "should resolve command_definition" do
+      params_from(:get, "/admin/commands/show?command_name=maven-clean").should == {:controller => "admin/commands", :action => "show", :command_name => 'maven-clean'}
+    end
+
+    it "should generate command_definition" do
+      admin_command_definition_path(:command_name => "foo").should == "/admin/commands/show?command_name=foo"
+      route_for(:controller => "admin/commands", :action => "show", :command_name=> 'maven-clean').should == "/admin/commands/show?command_name=maven-clean"
+    end
+
+    it "should resolve lookup autocomplete path" do
+      params_from(:get, "/admin/commands/lookup?lookup_prefix=some-prefix").should == {:controller => "admin/commands", :action => "lookup", :lookup_prefix => "some-prefix", :format => "text"}
+    end
+
+    it "should generate path for command lookup" do
+      route_for(:controller => "admin/commands", :action => "lookup", :format => "text", :lookup_prefix => "some_prefix").should == "/admin/commands/lookup?lookup_prefix=some_prefix"
+      admin_command_lookup_path.should == "/admin/commands/lookup"
+    end
+  end
+
+  describe "actions" do
+
+    before :each do
+      @command_repository_service = stub_service(:command_repository_service)
+    end
+
+    describe "index" do
+
+      it "should respond to html with list of tasks in alphabetical order" do
+        invalid_command_snippet_1 = CommandSnippetMother.invalidSnippetWithEmptyCommand("bad1")
+        invalid_command_snippet_2 = CommandSnippetMother.invalidSnippetWithInvalidContentInArg("bad0")
+        @command_repository_service.should_receive(:getAllInvalidCommandSnippets).and_return([invalid_command_snippet_1, invalid_command_snippet_2])
+
+        get :index, {:format => :html}
+
+        assigns[:invalid_commands].should == [invalid_command_snippet_2, invalid_command_snippet_1]
+      end
+    end
+
+    describe "definition" do
+      integrate_views
+
+      before do
+        @snippet = CommandSnippetMother.validSnippetWithKeywords("robocopy", ["cOpY12", "windows"].to_java(:string))
+      end
+
+      it "should render command definition as json" do
+        @command_repository_service.should_receive(:getCommandSnippetByRelativePath).with("maven").and_return(@snippet)
+
+        get :show, :command_name => 'maven'
+
+        expected_response = {'name' => 'robocopy', 'description' => 'some-description', 'author' => 'Go Team',
+                             'authorinfo' => 'TWEr@thoughtworks.com', 'moreinfo' => 'http://some-url', 'command' => 'robocopy', 'arguments' => "pack\ncomponent.nuspec"}
+        actual_response = JSON.parse(response.body)
+        actual_response.should == expected_response
+      end
+
+      it "should render parts of response with nil when it is not available" do
+        snippet = CommandSnippetMother.validSnippet("robocopy1")
+        @command_repository_service.should_receive(:getCommandSnippetByRelativePath).with("maven").and_return(snippet)
+
+        get :show, :command_name => 'maven'
+
+        JSON.parse(response.body).should == {"name" => "robocopy1", "description" => nil, "author" => nil, "authorinfo" => nil,
+                                             "moreinfo" => nil, "command" => "robocopy1", "arguments" => "pack\ncomponent.nuspec"}
+      end
+
+      it "should fail request if task definition cannot be found" do
+        @command_repository_service.should_receive(:getCommandSnippetByRelativePath).with("robo").and_return(nil)
+
+        get :show, :command_name => "robo"
+
+        response.response_code.should == 404
+        response.body.should == "Command Definition for 'robo' could not be found"
+      end
+    end
+
+    describe "lookup" do
+      it "should perform lookup of command name" do
+        matched_commands = [CommandSnippetMother.validSnippet("robocopy1"), CommandSnippetMother.validSnippet("robocopy2")]
+        @command_repository_service.should_receive(:lookupCommand).with("robo").and_return(matched_commands)
+
+        get :lookup, :lookup_prefix => "robo", :format => "text"
+
+        response.body.should == "robocopy1|/some/path/robocopy1.xml\nrobocopy2|/some/path/robocopy2.xml"
+      end
+    end
+  end
+end
---
+++ server/webapp/WEB-INF/rails.new/spec/controllers/admin/plugins/plugins_controller_spec.rb
@@ -0,0 +1,77 @@
+##########################GO-LICENSE-START################################
+# Copyright 2014 ThoughtWorks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################GO-LICENSE-END##################################
+
+describe Admin::Plugins::PluginsController do
+
+  describe :routes do
+
+    it "should resolve the route" do
+      params_from(:get, "/admin/plugins").should == {:controller => "admin/plugins/plugins", :action => "index"}
+      route_for(:controller => "admin/plugins/plugins", :action => "index").should == "admin/plugins"
+      plugins_listing_path.should == "/admin/plugins"
+    end
+  end
+
+  describe :index do
+
+    before :each do
+      controller.stub!(:default_plugin_manager).and_return(@plugin_manager = mock('plugin_manager'))
+      @plugin_1 = plugin("id", "name")
+      @plugin_2 = plugin("yum", "yum plugin")
+      @plugin_3 = plugin("A-id", "Name")
+      @plugin_4 = plugin("Yum-id", "Yum Exec Plugin")
+      @plugin_5 = plugin("Another-id", nil)
+      @plugin_6 = plugin("plugin.jar", nil)
+    end
+
+    it "should populate the tab name" do
+      @plugin_manager.should_receive(:plugins).and_return([@plugin_1, @plugin_2])
+      get :index
+      assigns[:tab_name].should == "plugins-listing"
+    end
+
+    it "should populate the current list of plugins and the external plugins path" do
+      @plugin_manager.should_receive(:plugins).and_return([@plugin_1, @plugin_2])
+      controller.should_receive(:system_environment).and_return(@system_environment = mock("system_environment"))
+      @system_environment.should_receive(:getExternalPluginAbsolutePath).and_return("some_path")
+      get :index
+      assigns[:plugin_descriptors].should == [plugin_descriptors(@plugin_1), plugin_descriptors(@plugin_2)]
+      assigns[:external_plugin_location].should == "some_path"
+    end
+
+    it "should populate the current list of plugins in case insensitive alphabetical order when plugin names are given" do
+      @plugin_manager.should_receive(:plugins).and_return([@plugin_1, @plugin_2, @plugin_3, @plugin_4])
+      get :index
+      assigns[:plugin_descriptors].should == [plugin_descriptors(@plugin_1), plugin_descriptors(@plugin_3), plugin_descriptors(@plugin_4), plugin_descriptors(@plugin_2)]
+    end
+
+    it "should populate the current list of plugins in case insensitive alphabetical order when plugin names are not given" do
+      @plugin_manager.should_receive(:plugins).and_return([@plugin_1, @plugin_2, @plugin_3, @plugin_4, @plugin_5, @plugin_6])
+      get :index
+      assigns[:plugin_descriptors].should == [plugin_descriptors(@plugin_5), plugin_descriptors(@plugin_1), plugin_descriptors(@plugin_3), plugin_descriptors(@plugin_6), plugin_descriptors(@plugin_4), plugin_descriptors(@plugin_2)]
+    end
+
+    def plugin(id, name)
+      about = com.thoughtworks.go.plugin.infra.plugininfo.GoPluginDescriptor::About.new(name, nil, nil, nil, nil, [])
+      GoPluginDescriptor.new(id, nil, about, nil, nil, true)
+    end
+
+    def plugin_descriptors(plugin)
+      GoPluginDescriptorModel::convertToDescriptorWithAllValues plugin
+    end
+  end
+
+end

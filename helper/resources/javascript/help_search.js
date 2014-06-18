4cc600031f0193cf08ee5b4fb958787d9575e7ce
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2014-06-18 16:21:51 +0530

    Updating google custom search key for release 14.2.0

--- helper/resources/javascript/help_search.js
+++ helper/resources/javascript/help_search.js
@@ -86,7 +86,7 @@ GoHelpSearch = Class.create({
       this.resultsElement = $(resultsDomId);
       var options = new google.search.DrawOptions();
       options.setSearchFormRoot(formDomId);
-      var searcher = new google.search.CustomSearchControl('013109281384884948049:gcpiyhasjsk');
+      var searcher = new google.search.CustomSearchControl('013109281384884948049:cgm8nr4lrvy');
       searcher.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
       searcher.setNoResultsString('Your search did not match any help pages.');
       searcher.setSearchCompleteCallback(this, this.onSearchComplete.bind(this));

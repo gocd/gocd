9bf762d853a07cb94b9afc945c2ce08c35ab2c1b
Rik Tyer <riktyer@gmail.com>
2014-07-13 20:53:56 +0100

    Remove ArtifactPropertiesGenerator sqlmap and move functionality to Hibernate.

---
+++ server/src/com/thoughtworks/go/server/persistence/ArtifactPropertiesGeneratorRepository.java
@@ -0,0 +1,35 @@
+package com.thoughtworks.go.server.persistence;
+
+
+import com.thoughtworks.go.config.ArtifactPropertiesGenerator;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @understands persisting and retrieving artifact properties generator
+ */
+@Service
+public class ArtifactPropertiesGeneratorRepository extends HibernateDaoSupport {
+
+    private static final String GET_ARTIFACT_PROPERTY_GENERATORS_BY_BUILD_ID =
+            "SELECT a FROM ArtifactPropertiesGenerator a WHERE a.jobId = ? ORDER BY a.id";
+
+    @Autowired
+    public ArtifactPropertiesGeneratorRepository(SessionFactory sessionFactory) {
+        setSessionFactory(sessionFactory);
+    }
+
+    public void save(ArtifactPropertiesGenerator artifactPropertiesGenerator) {
+        if (artifactPropertiesGenerator != null) {
+            getHibernateTemplate().save(artifactPropertiesGenerator);
+        }
+    }
+
+    public List<ArtifactPropertiesGenerator> findByBuildId(long buildId) {
+        return (List<ArtifactPropertiesGenerator>) getHibernateTemplate().find(GET_ARTIFACT_PROPERTY_GENERATORS_BY_BUILD_ID, buildId);
+    }
+}

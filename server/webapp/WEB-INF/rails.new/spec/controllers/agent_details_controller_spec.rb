a3c2448e135cf7b566b505e2a92cd57fc280408c
Praveen <praveends@thoughtworks.com>
2014-07-14 16:29:17 +0530

    #137 fixed specs for  agent details controller new rails

--- server/webapp/WEB-INF/rails.new/spec/controllers/agent_details_controller_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/controllers/agent_details_controller_spec.rb
@@ -16,23 +16,24 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+
 describe AgentDetailsController do
 
   before do
-    controller.stub!(:set_locale)
+    controller.stub(:set_locale)
     controller.stub(:licensed_agent_limit)
     controller.stub(:populate_config_validity)
   end
 
   describe :routes do
     it "should resolve the route to an agent" do
-      params_from(:get, "/agents/uuid").should == {:controller => "agent_details", :action => 'show', :uuid => "uuid"}
-      agent_detail_path(:uuid => "uuid").should == "/agents/uuid"
+      expect(:get => "/agents/uuid").to route_to({:controller => "agent_details", :action => 'show',:uuid => "uuid"})
+      expect(controller.send(:agent_detail_path,:uuid=>"uuid")).to eq("/agents/uuid")
     end
 
     it "should resolve the route to an job run history for an agent" do
-      params_from(:get, "/agents/uuid/job_run_history").should == {:controller => "agent_details", :action => 'job_run_history', :uuid => "uuid"}
-      job_run_history_on_agent_path(:uuid => "uuid").should == "/agents/uuid/job_run_history"
+      expect(:get => "/agents/uuid/job_run_history").to route_to({:controller => "agent_details", :action => 'job_run_history',:uuid => "uuid"})
+      expect(controller.send(:job_run_history_on_agent_path, :uuid=>"uuid")).to eq("/agents/uuid/job_run_history")
     end
   end
 
@@ -49,48 +50,49 @@ describe AgentDetailsController do
     it "should show agent details" do
       @agent_service.should_receive(:findAgentViewModel).with(@uuid).and_return(@agent)
 
-      controller.should_receive(:render).with(:layout => "agent_detail")
-
       get "show", :uuid =>@uuid
 
-      assigns[:agent].should == @agent
+      assert_template layout: :agent_detail
+      assert_template "show"
+      expect(assigns(:agent)).to eq(@agent)
     end
 
     describe "when bad uuid is given" do
-      integrate_views
+      render_views
 
       it "should show 404 when an agent is not found" do
         @agent_service.should_receive(:findAgentViewModel).with(@uuid).and_return(AgentViewModel.new(com.thoughtworks.go.domain.NullAgentInstance.new(@uuid)))
 
         get "show", :uuid => @uuid
-
-        response.status.should == "404 Not Found"
-        response.body.should =~ /Agent with uuid '#{@uuid}' not found\./
+        expect(response.status).to eq(404)
+        expect(response.body).to have_content(/Agent with uuid '#{@uuid}' not found\./)
       end
     end
 
     it "should show job run history" do
       @agent_service.should_receive(:findAgentViewModel).with(@uuid).and_return(@agent)
-      controller.should_receive(:render).with(:layout => "agent_detail")
       controller.should_receive(:job_instance_service).and_return(@job_instance_service = Object.new)
       @job_instance_service.should_receive(:completedJobsOnAgent).with(@uuid, AgentDetailsController::JobHistoryColumns.completed, SortOrder::DESC, 1, AgentDetailsController::PAGE_SIZE).and_return(expected = JobInstancesModel.new(nil, nil))
 
       get "job_run_history", :uuid => @uuid
 
-      assigns[:agent].should == @agent
-      assigns[:job_instances].should == expected
+      assert_template "job_run_history"
+      assert_template layout: :agent_detail
+      expect(assigns(:agent)).to eq(@agent)
+      expect(assigns(:job_instances)).to eq(expected)
     end
 
     it "should show a later page of job run history" do
       @agent_service.should_receive(:findAgentViewModel).with(@uuid).and_return(@agent)
-      controller.should_receive(:render).with(:layout => "agent_detail")
       controller.should_receive(:job_instance_service).and_return(@job_instance_service = Object.new)
       @job_instance_service.should_receive(:completedJobsOnAgent).with(@uuid, AgentDetailsController::JobHistoryColumns.stage, SortOrder::ASC, 3, AgentDetailsController::PAGE_SIZE).and_return(expected = JobInstancesModel.new(nil, nil))
 
       get "job_run_history", :uuid => @uuid, :page => 3, :column => 'stage', :order => 'ASC'
 
-      assigns[:agent].should == @agent
-      assigns[:job_instances].should == expected
+      assert_template "job_run_history"
+      assert_template layout: :agent_detail
+      expect(assigns(:agent)).to eq(@agent)
+      expect(assigns(:job_instances)).to eq(expected)
     end
   end
 end

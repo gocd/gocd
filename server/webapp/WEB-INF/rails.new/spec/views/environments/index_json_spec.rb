c4a1a13c3ba781970c2471bddc23a49ef996e603
Ali <mdaliejaz@gmail.com>
2014-07-16 15:24:17 +0530

    #130 - Fix Environment index.json and index_json_spec for new Rails.

--- server/webapp/WEB-INF/rails.new/spec/views/environments/index_json_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/views/environments/index_json_spec.rb
@@ -14,45 +14,46 @@
 # limitations under the License.
 ##########################GO-LICENSE-END##################################
 
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
 describe 'environments/index.json.erb' do
   include EnvironmentsHelper
 
   describe "render" do
 
-
     def assert_result
       json = JSON.parse(response.body)
 
-      json["unused_feature"].should == {"html" => "environment", "parent_id" => "unused_feature"}
-      json["environment_environment_panel"].should == {"html" => "pipeline_1", "parent_id" => "ajax_environments", "index" => 0, "type" => "group_of_pipelines"}
-      json["environment_environment_other_panel"].should == {"html" => "pipeline_other_env", "parent_id" => "ajax_environments", "index" => 1, "type" => "group_of_pipelines"}
-      json["model_1"].should == {"html" => "pipeline_1", "parent_id" => "environment_environment_panel", "index" => 0, "type" => "pipeline"}
-      json["model_2"].should == {"html" => "pipeline_2", "parent_id" => "environment_environment_panel", "index" => 1, "type" => "pipeline"}
-      json["other-env"].should == {"html" => "pipeline_other_env", "parent_id" => "environment_environment_other_panel", "index" => 0, "type" => "pipeline"}
+      expect(json["unused_feature"]).to eq({"html" => "environment", "parent_id" => "unused_feature"})
+      expect(json["environment_environment_panel"]).to eq({"html" => "pipeline_1", "parent_id" => "ajax_environments", "index" => 0, "type" => "group_of_pipelines"})
+      expect(json["environment_environment_other_panel"]).to eq({"html" => "pipeline_other_env", "parent_id" => "ajax_environments", "index" => 1, "type" => "group_of_pipelines"})
+      expect(json["model_1"]).to eq({"html" => "pipeline_1", "parent_id" => "environment_environment_panel", "index" => 0, "type" => "pipeline"})
+      expect(json["model_2"]).to eq({"html" => "pipeline_2", "parent_id" => "environment_environment_panel", "index" => 1, "type" => "pipeline"})
+      expect(json["other-env"]).to eq({"html" => "pipeline_other_env", "parent_id" => "environment_environment_other_panel", "index" => 0, "type" => "pipeline"})
     end
 
     it "should create json with 'environments' partial" do
-      assigns[:environments] = [env1 = stub('environment', :name => 'environment', :getPipelineModels => [pipeline_model_1 = Object.new, pipeline_model_2 = Object.new]),
-                                env2 = stub("environment_other", :name => 'environment_other', :getPipelineModels => [pipeline_other_env = Object.new])]
+      assign(:environments, [env1 = double('environment', :name => 'environment', :getPipelineModels => [pipeline_model_1 = Object.new, pipeline_model_2 = Object.new]),
+                             env2 = double("environment_other", :name => 'environment_other', :getPipelineModels => [pipeline_other_env = Object.new])])
 
-      template.should_receive(:render_json).with(:partial => "no_environments.html.erb", :locals => {:scope => {}}).and_return("\"environment\"")
-      template.should_receive(:render_json).with(:partial => "environment.html.erb", :locals => {:scope => {:environment => env1, :omit_pipeline => true}}).and_return("\"pipeline_1\"")
-      template.should_receive(:render_json).with(:partial => "environment.html.erb", :locals => {:scope => {:environment => env2, :omit_pipeline => true}}).and_return("\"pipeline_other_env\"")
+      allow(view).to receive(:render_json).with(:partial => "no_environments.html.erb", :locals => {:scope => {}}).and_return("\"environment\"")
+      allow(view).to receive(:render_json).with(:partial => "environment.html.erb", :locals => {:scope => {:environment => env1, :omit_pipeline => true}}).and_return("\"pipeline_1\"")
+      allow(view).to receive(:render_json).with(:partial => "environment.html.erb", :locals => {:scope => {:environment => env2, :omit_pipeline => true}}).and_return("\"pipeline_other_env\"")
 
-      template.should_receive(:env_dom_id).exactly(3).with("environment").and_return('environment_environment_panel')
-      template.should_receive(:env_dom_id).exactly(2).with("environment_other").and_return('environment_environment_other_panel')
+      allow(view).to receive(:env_dom_id).exactly(3).with("environment").and_return('environment_environment_panel')
+      allow(view).to receive(:env_dom_id).exactly(2).with("environment_other").and_return('environment_environment_other_panel')
 
-      template.should_receive(:render_json).with(:partial => "environment_pipeline.html.erb", :locals => {:scope => {:pipeline_model => pipeline_model_1}}).and_return("\"pipeline_1\"")
-      template.should_receive(:render_json).with(:partial => "environment_pipeline.html.erb", :locals => {:scope => {:pipeline_model => pipeline_model_2}}).and_return("\"pipeline_2\"")
-      template.should_receive(:render_json).with(:partial => "environment_pipeline.html.erb", :locals => {:scope => {:pipeline_model => pipeline_other_env}}).and_return("\"pipeline_other_env\"")
-      template.stub!(:view_cache_key).and_return(view_cache_key = mock('view_cache_key'))
-      view_cache_key.stub!(:forEnvironmentPipelineBox).and_return("key")
+      allow(view).to receive(:render_json).with(:partial => "environment_pipeline.html.erb", :locals => {:scope => {:pipeline_model => pipeline_model_1}}).and_return("\"pipeline_1\"")
+      allow(view).to receive(:render_json).with(:partial => "environment_pipeline.html.erb", :locals => {:scope => {:pipeline_model => pipeline_model_2}}).and_return("\"pipeline_2\"")
+      allow(view).to receive(:render_json).with(:partial => "environment_pipeline.html.erb", :locals => {:scope => {:pipeline_model => pipeline_other_env}}).and_return("\"pipeline_other_env\"")
+      allow(view).to receive(:view_cache_key).and_return(view_cache_key = double('view_cache_key'))
+      view_cache_key.stub(:forEnvironmentPipelineBox).and_return("key")
 
-      template.should_receive(:env_pipeline_dom_id).with(pipeline_model_1).and_return('model_1')
-      template.should_receive(:env_pipeline_dom_id).with(pipeline_model_2).and_return('model_2')
-      template.should_receive(:env_pipeline_dom_id).with(pipeline_other_env).and_return('other-env')
+      allow(view).to receive(:env_pipeline_dom_id).with(pipeline_model_1).and_return('model_1')
+      allow(view).to receive(:env_pipeline_dom_id).with(pipeline_model_2).and_return('model_2')
+      allow(view).to receive(:env_pipeline_dom_id).with(pipeline_other_env).and_return('other-env')
 
-      render 'environments/index.json.erb'
+      render
 
       assert_result
     end
@@ -62,10 +63,10 @@ describe 'environments/index.json.erb' do
     it "should cache pipeline partials of different pipelines separately" do
       environment1 = environment_for_caching(JobState::Building, JobResult::Unknown)
       environment2 = environment_for_caching(JobState::Completed, JobResult::Failed)
-      key_proc = proc {|environment| [ViewCacheKey.new.forEnvironmentPipelineBox(environment.getPipelineModels()[0]), {:subkey => "environment_json_#{env_dom_id(environment.name())}"}]}
+      key_proc = proc { |environment| [ViewCacheKey.new.forEnvironmentPipelineBox(environment.getPipelineModels()[0]), {:subkey => "environment_json_#{env_dom_id(environment.name())}"}] }
       check_fragment_caching(environment1, environment2, key_proc) do |environment|
-        assigns[:environments] = [environment]
-        render "environments/index.json", :locals => {:scope => {}}
+        assign(:environments, [environment])
+        render
       end
     end
 

b0e4a51e6a30d5aef6dce49c56a90326e2d0782d
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2014-07-23 23:18:00 +0530

    Reordering specs for clients_controller. Removed test.sqlite3 from version control

--- spec/controllers/oauth2_provider/clients_controller_spec.rb
+++ spec/controllers/oauth2_provider/clients_controller_spec.rb
@@ -10,26 +10,28 @@ module Oauth2Provider
       end
     end
     
-    it "should return all associated clients" do
-      expected_clients = []
-      (1..5).each do
-        expected_clients << create(Oauth2Provider::Client)
+    describe 'index' do
+      it "should return all associated clients" do
+        expected_clients = []
+        (1..5).each do
+          expected_clients << create(Oauth2Provider::Client)
+        end
+        get :index, { use_route: :oauth_engine }
+        clients = assigns[:oauth_clients]
+        expect(clients.size).to eq(5)
+        expect(clients.map(&:name)).to match_array(expected_clients.map(&:name))
       end
-      get 'index', { use_route: :oauth_engine }
-      clients = assigns[:oauth_clients]
-      expect(clients.size).to eq(5)
-      expect(clients.map(&:name)).to match_array(expected_clients.map(&:name))
-    end
     
-    it "should respond to xml route" do
-      expected_clients = []
-      (1..5).each do
-        expected_clients << create(Oauth2Provider::Client)
+      it "should respond to xml route" do
+        expected_clients = []
+        (1..5).each do
+          expected_clients << create(Oauth2Provider::Client)
+        end
+        get :index, { use_route: :oauth_engine, :format => :xml }
+        output = Hash.from_xml(response.body)
+        actual = output['oauth_clients'].map {|c| c['name']}
+        expect(actual).to match_array(expected_clients.map(&:name))
       end
-      get 'index', { use_route: :oauth_engine, :format => :xml }
-      output = Hash.from_xml(response.body)
-      actual = output['oauth_clients'].map {|c| c['name']}
-      expect(actual).to match_array(expected_clients.map(&:name))
     end
   end
 end

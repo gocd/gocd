d754737347ae63cb17a8c53aadc7cf2766a88c07
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2014-07-28 19:39:52 +0530

    fixing spec

--- spec/controllers/oauth2_provider/user_token_controller_spec.rb
+++ spec/controllers/oauth2_provider/user_token_controller_spec.rb
@@ -30,7 +30,8 @@ module Oauth2Provider
       end
       
       it "should not revoke if token is invalid" do
-        delete :revoke, {use_route: :oauth_engine}.merge({token_id: SecureRandom.hex(32)})
+        another_token = create(Oauth2Provider::Token)
+        delete :revoke, {use_route: :oauth_engine}.merge({token_id: another_token.id})
         expect(response.body).to eq("You are not authorized to perform this action!")
         expect(response.status).to eq(400)
       end
@@ -56,8 +57,7 @@ module Oauth2Provider
       end
       
       it "should not revoke if token is invalid" do
-        another_token = create(Oauth2Provider::Token)
-        delete :revoke_by_admin, {use_route: :oauth_engine}.merge({user_id: @token.user_id, token_id: another_token.id})
+        delete :revoke_by_admin, {use_route: :oauth_engine}.merge({user_id: @token.user_id, token_id: SecureRandom.hex(32)})
         expect(response.body).to eq("You are not authorized to perform this action!")
         expect(response.status).to eq(400)
       end

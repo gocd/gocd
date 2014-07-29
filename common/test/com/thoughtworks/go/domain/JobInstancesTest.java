5bdc0cd595555ea68b2724783cdd18f99d42cdad
Aravind SV <arvind.sv@gmail.com>
2014-07-29 12:28:34 -0400

    #000 - Fix test.
    * Earlier, the test was doing new Date(2008, 10, 12), which gives a date of '12 November, 3908'
      because the constructor adds 1900, just for the heck of it. So, this code change brings that
      behaviour back. Reference: http://docs.oracle.com/javase/6/docs/api/java/util/Date.html#Date(int, int, int)

--- common/test/com/thoughtworks/go/domain/JobInstancesTest.java
+++ common/test/com/thoughtworks/go/domain/JobInstancesTest.java
@@ -181,11 +181,11 @@ public class JobInstancesTest {
 
     @Test
     public void shouldReturnLatestTransitionDate() {
-        Date expectedLatest = date(2008, 10, 12);
+        Date expectedLatest = date(3908, 10, 12);
         Date actualLatest = new JobInstances(
                 completed(completed("job1"), JobResult.Failed, expectedLatest),
-                completed(completed("job1"), JobResult.Failed, date(2008, 10, 11)),
-                completed(completed("job1"), JobResult.Failed, date(2008, 10, 5))).latestTransitionDate();
+                completed(completed("job1"), JobResult.Failed, date(3908, 10, 11)),
+                completed(completed("job1"), JobResult.Failed, date(3908, 10, 5))).latestTransitionDate();
         assertThat(actualLatest,is(expectedLatest));
     }
 

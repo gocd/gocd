68e661a5f5b8444ffc0a07178c59e1f71ac42e7a
Jyoti Singh <jyotisingh7@gmail.com>
2014-07-29 14:53:41 +0530

    #130 - fixing go_cache code and specs
    Basically, activesupport::cache wraps all cache entries in a Entry object instead of caching the values directly. Entry object contains metadata like expiry-date tetc. cache.read and cache.fetch take care of getting the actual value from the entry object. Fixed the tests to reflect that

--- server/webapp/WEB-INF/rails.new/spec/lib/go_cache_store_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/lib/go_cache_store_spec.rb
@@ -28,65 +28,64 @@ describe GoCacheStore do
   end
 
   it "should be the default cache store" do
-      ActionController::Base.cache_store.should be_a GoCacheStore
+      expect(ActionController::Base.cache_store.class).to eq GoCacheStore
   end
 
   it "should write to underlying cruise cache" do
     @store.write("name", "value")
-    @go_cache.get("name").should == "value"
+    expect(@store.fetch("name")).to eq "value"
+    expect(@go_cache.get("name").value).to eq "value"
   end
 
   it "should honor writing under subkey to underlying cruise cache" do
     @store.write("name", "value", :subkey => "sub")
-    @go_cache.get("name", "sub").should == "value"
+    expect(@go_cache.get("name", "sub").value).to eq "value"
   end
 
   it "should read from underlying cruise cache" do
-    @go_cache.put("read_key", "read_value")
-    @store.read("read_key").should == "read_value"
+    @go_cache.put("read_key", ActiveSupport::Cache::Entry.new("read_value"))
+    expect(@store.read("read_key")).to eq "read_value"
   end
 
   it "should honor subkey reading from underlying cruise cache" do
-    @go_cache.put("read_key", "sub", "read_value")
-    @store.read("read_key", :subkey => "sub").should == "read_value"
+    @go_cache.put("read_key", "sub", ActiveSupport::Cache::Entry.new("read_value"))
+    expect(@store.read("read_key", :subkey => "sub")).to eq "read_value"
   end
 
   it "should delete from underlying cruise cache" do
-    @go_cache.put("delete_key", "value")
-    @store.delete("delete_key").should == "value"
-    @store.read("delete_key").should be_nil
-    @go_cache.get("delete_key").should be_nil
+    @go_cache.put("delete_key", ActiveSupport::Cache::Entry.new("value"))
+    expect(@store.delete("delete_key").value).to eq "value"
+    expect(@store.read("delete_key")).to eq nil
+    expect(@go_cache.get("delete_key")).to eq nil
   end
 
   it "should delete subkey from underlying cruise cache" do
-    @go_cache.put("delete_key", "sub", "value")
-    @store.delete("delete_key", :subkey => "sub").should == "value"
-    @store.read("delete_key", :subkey => "sub").should be_nil
-    @go_cache.get("delete_key", "sub").should be_nil
+    @go_cache.put("delete_key", "sub", ActiveSupport::Cache::Entry.new("value"))
+    expect(@store.delete("delete_key", :subkey => "sub").value).to eq "value"
+    expect(@store.read("delete_key", :subkey => "sub")).to eq nil
+    expect(@go_cache.get("delete_key", "sub")).to eq nil
   end
 
   it "should understand if key exists in cruise cache" do
-    @go_cache.put("key", "value")
-    @store.exist?("key").should be_true 
+    @go_cache.put("key", ActiveSupport::Cache::Entry.new("value"))
+    expect(@store.exist?("key")).to be_true 
     @go_cache.remove("key")
-    @store.exist?("key").should be_false
-
-    @store.exist?("nokey").should be_false
+    expect(@store.exist?("key")).to be_false
+    expect(@store.exist?("nokey")).to be_false
   end
 
   it "should understand if key, subkey combination exists in cruise cache" do
-    @go_cache.put("key", "sub", "value")
-    @store.exist?("key", :subkey => "sub").should be_true
+    @go_cache.put("key", "sub", ActiveSupport::Cache::Entry.new("value"))
+    expect(@store.exist?("key", :subkey => "sub")).to be_true
     @go_cache.remove("key", "sub")
-    @store.exist?("key", :subkey => "sub").should be_false
-
-    @store.exist?("nokey", :subkey => "sub").should be_false
+    expect(@store.exist?("key", :subkey => "sub")).to be_false
+    expect(@store.exist?("nokey", :subkey => "sub")).to be_false
   end
 
   it "should clear cruise cache" do
-    @go_cache.put("key", "value")
+    @go_cache.put("key", ActiveSupport::Cache::Entry.new("value"))
     @store.clear
-    @go_cache.get("key").should be_nil
+    expect(@go_cache.get("key")).to eq nil
   end
 end
 

109e506833d732030672bf2b7c9e940bd3af3761
Aravind SV <arvind.sv@gmail.com>
2014-07-30 20:52:21 -0400

    More one-time config needed for gadgets.

--- server/webapp/WEB-INF/rails.new/config/application.rb
+++ server/webapp/WEB-INF/rails.new/config/application.rb
@@ -3,7 +3,11 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "rails"
 require "dynamic_form"
-#require "gadgets"
+
+# For gadgets client plugin
+ENV['ADMIN_OAUTH_URL_PREFIX'] = "admin"
+ENV['LOAD_OAUTH_SILENTLY'] = "yes"
+require "gadgets"
 
 module Go
   class Application < Rails::Application

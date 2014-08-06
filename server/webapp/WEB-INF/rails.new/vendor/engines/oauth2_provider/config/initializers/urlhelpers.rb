b021139d289f9bf4c6a7f943a92166af456a999f
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2014-08-06 10:40:28 +0530

    Removing puts in urlhelpers.rb

--- server/webapp/WEB-INF/rails.new/vendor/engines/oauth2_provider/config/initializers/urlhelpers.rb
+++ server/webapp/WEB-INF/rails.new/vendor/engines/oauth2_provider/config/initializers/urlhelpers.rb
@@ -1,7 +1,6 @@
 module Oauth2Provider
   module ApplicationHelper
     def method_missing method, *args, &block
-      puts "LOOKING FOR ROUTES #{method}"
       if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
         if main_app.respond_to?(method)
           main_app.send(method, *args)

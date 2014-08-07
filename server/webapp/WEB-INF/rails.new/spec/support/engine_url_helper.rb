cc71160f3715c931940222b47ab0133fb328d3e4
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2014-08-07 11:09:05 +0530

    Fixing failing rspec because of oauth2_provider engine
    Because of the inclusion of oauth2_provider engine, all navigation routes had to be prepended with either 'main_app' or 'oauth_engine'. This caused few specs to fail because of the way urls were stubbed before. Changed it by including a EngineUrlHelper which stubs all header links and oauth2_provider links.

---
+++ server/webapp/WEB-INF/rails.new/spec/support/engine_url_helper.rb
@@ -0,0 +1,24 @@
+module EngineUrlHelper
+  def stub_routes_for_main_app main_app
+    allow(main_app).to receive(:root_path).and_return("/")
+    allow(main_app).to receive(:user_listing_path).and_return("/path/for/user/listing")
+    allow(main_app).to receive(:backup_server_path).and_return("admin/backup")
+    allow(main_app).to receive(:pipelines_snippet_path).and_return("admin/pipelines/snippet")
+    allow(main_app).to receive(:pipeline_dashboard_path).and_return("/path/to/pipeline/dashboard")
+    allow(main_app).to receive(:environments_path).and_return("/path/to/environments")
+    allow(main_app).to receive(:agents_path).and_return("/path/to/agents")
+    allow(main_app).to receive(:pipeline_groups_path).and_return("/path/to/pipeline/groups")
+    allow(main_app).to receive(:templates_path).and_return("/path/to/templates")
+    allow(main_app).to receive(:config_view_path).and_return("/path/to/config/view")
+    allow(main_app).to receive(:edit_server_config_path).and_return("/path/to/server/edit")
+    allow(main_app).to receive(:gadgets_oauth_clients_path).and_return("/path/to/gadget")
+    allow(main_app).to receive(:plugins_listing_path).and_return("/path/to/plugins/listing")
+    allow(main_app).to receive(:package_repositories_new_path).and_return("/path/to/package/repo")
+    allow(main_app).to receive(:package_repositories_list_path).and_return("/path/to/package/listing")
+    allow(main_app).to receive(:global_message_path).and_return("/path/to/global/message")
+  end
+
+  def stub_oauth2_provider_engine oauth_engine
+    allow(oauth_engine).to receive(:clients_path).and_return("/path/for/oauth/clients")
+  end
+end

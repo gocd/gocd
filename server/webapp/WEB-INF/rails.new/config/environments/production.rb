4e14f2fb2ffff43ebdda7e1c74a2dd4caa9b8c01
Aravind SV <arvind.sv@gmail.com>
2014-09-29 12:07:00 -0400

    #000 - Fix Rails logging.
    1. Development server no longer logs into /tmp/debug.log. Removed stale
       log4j.xml files from gadgets engines. It will now be in server/go-server.log
       and DEBUG logs are turned on by default.
    
    2. For production, RAILS_LOG_LEVEL environment variable is introduced. It sets the
       log level. However, that decides what Rails sends to Log4J.  Log4J has its own
       level (in log4j.properties) which decides what it appends to the file appender.
       Usually these two need to be in sync. You can set one to a high level and the
       other to a lower logging level and the lower one will will (meaning, WARN will
       be chosen instead of INFO instead of DEBUG, etc).

--- server/webapp/WEB-INF/rails.new/config/environments/production.rb
+++ server/webapp/WEB-INF/rails.new/config/environments/production.rb
@@ -42,7 +42,7 @@ Go::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = (ENV["RAILS_LOG_LEVEL"] || "warn").downcase.to_sym
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]

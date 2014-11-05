4c9e27d1fb052328459a4f93b3689be85b2aeeec
Matthew Boedicker <mboedicker@pivotal.io>
2014-11-05 11:06:29 -0800

    First pass at pipeline build comment javascript.
    Signed-off-by: Frank Kotsianas <fkotsianas@pivotal.io>

---
+++ server/webapp/WEB-INF/rails.new/app/assets/javascripts/pipeline_history_comment.js
@@ -0,0 +1,26 @@
+function initPipelineHistoryComment(jQuery, Modalbox, dashboardPeriodicalExecuter) {
+    var my = {};
+
+    my.showModal = function (pipelineLabel) {
+        var div = jQuery('#comment-form-' + pipelineLabel)[0];
+
+        Modalbox.show(div);
+    };
+
+    my.submit = function (pipelineName, pipelineLabel) {
+        var commentText = jQuery('#comment-input').val();
+
+        jQuery.post(
+                '/go/pipelines/' + pipelineName + '/' + pipelineLabel + '/comment',
+            { comment: commentText },
+            my.onCommentSuccessCloseModalAndRefreshPipelineHistory,
+            'json');
+    };
+
+    my.onCommentSuccessCloseModalAndRefreshPipelineHistory = function () {
+        Modalbox.hide();
+        dashboardPeriodicalExecuter.fireNow();
+    };
+
+    return my;
+}
---
+++ server/webapp/WEB-INF/rails.new/spec/javascripts/pipeline_history_comment_spec.js
@@ -0,0 +1,47 @@
+describe("pipeline history comments", function() {
+    var fakeModalBox = jasmine.createSpyObj('Modalbox', ['hide', 'show']);
+    var fakeDashboardPeriodicalExecuter = jasmine.createSpyObj('DashboardPeriodicalExecuter', [ 'fireNow' ]);
+
+    var pipelineHistoryComment = initPipelineHistoryComment(jQuery, fakeModalBox, fakeDashboardPeriodicalExecuter);
+
+    describe("#showBuildCommentModals", function() {
+        beforeEach(function() {
+            setFixtures('<div id="comment-form-1"></div>');
+        });
+
+        it("shows the modal with the correct build comment form", function () {
+            pipelineHistoryComment.showModal('1');
+            expect(fakeModalBox.show).toHaveBeenCalledWith(jQuery('#comment-form-1')[0]);
+        });
+
+    });
+
+    describe("#submitPipelineComment", function() {
+        beforeEach(function() {
+            setFixtures('<input type="text" id="comment-input" value="This is the comment."/>');
+        });
+
+        it("submits a request with the new modal text", function() {
+            spyOn(jQuery, 'post');
+            pipelineHistoryComment.submit('test-pipeline', '1');
+            expect(jQuery.post).toHaveBeenCalledWith(
+                '/go/pipelines/test-pipeline/1/comment',
+                { comment: 'This is the comment.' },
+                pipelineHistoryComment.onCommentSuccessCloseModalAndRefreshPipelineHistory,
+                'json');
+        });
+    });
+
+    describe("#onCommentSuccessCloseModalAndRefreshPipelineHistory", function() {
+        it("closes the modal box", function() {
+            pipelineHistoryComment.onCommentSuccessCloseModalAndRefreshPipelineHistory();
+            expect(fakeModalBox.hide).toHaveBeenCalled();
+        });
+
+        it("refreshes the pipeline history", function() {
+            pipelineHistoryComment.onCommentSuccessCloseModalAndRefreshPipelineHistory();
+            expect(fakeDashboardPeriodicalExecuter.fireNow).toHaveBeenCalledWith();
+        });
+
+    });
+});

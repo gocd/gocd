355229ba506a324d33a7a7453b4022d6f7999bbe
Praveen <praveen.shivanagoudar@gmail.com>
2014-11-27 22:43:11 +0530

    #727 Fixed failing test

--- server/test/integration/com/thoughtworks/go/server/materials/PackageMaterialUpdaterIntegrationTest.java
+++ server/test/integration/com/thoughtworks/go/server/materials/PackageMaterialUpdaterIntegrationTest.java
@@ -16,34 +16,35 @@
 
 package com.thoughtworks.go.server.materials;
 
-import java.io.File;
-import java.util.List;
-
 import com.thoughtworks.go.config.materials.PackageMaterial;
 import com.thoughtworks.go.domain.MaterialInstance;
 import com.thoughtworks.go.domain.materials.Modification;
 import com.thoughtworks.go.domain.materials.Modifications;
 import com.thoughtworks.go.helper.MaterialsMother;
 import com.thoughtworks.go.helper.ModificationsMother;
-import com.thoughtworks.go.server.dao.DatabaseAccessHelper;
-import com.thoughtworks.go.server.persistence.MaterialRepository;
-import com.thoughtworks.go.server.transaction.TransactionTemplate;
 import com.thoughtworks.go.plugin.access.packagematerial.PackageConfiguration;
 import com.thoughtworks.go.plugin.access.packagematerial.PackageConfigurations;
 import com.thoughtworks.go.plugin.access.packagematerial.PackageMetadataStore;
+import com.thoughtworks.go.server.dao.DatabaseAccessHelper;
+import com.thoughtworks.go.server.persistence.MaterialRepository;
+import com.thoughtworks.go.server.transaction.TransactionTemplate;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 
+import java.io.File;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.doNothing;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -52,15 +53,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
         "classpath:WEB-INF/applicationContext-acegi-security.xml"
 })
 public class PackageMaterialUpdaterIntegrationTest {
-    @Autowired private MaterialRepository materialRepository;
-    @Autowired private ScmMaterialUpdater scmMaterialUpdater;
-    @Autowired private DatabaseAccessHelper dbHelper;
-    @Autowired private TransactionTemplate transactionTemplate;
+    @Autowired
+    private MaterialRepository materialRepository;
+    private ScmMaterialUpdater scmMaterialUpdater;
+    @Autowired
+    private DatabaseAccessHelper dbHelper;
+    @Autowired
+    private TransactionTemplate transactionTemplate;
     private PackageMaterialUpdater packageMaterialUpdater;
 
     @Before
     public void setUp() throws Exception {
         dbHelper.onSetUp();
+        scmMaterialUpdater = Mockito.mock(ScmMaterialUpdater.class);
         packageMaterialUpdater = new PackageMaterialUpdater(materialRepository, scmMaterialUpdater, transactionTemplate);
     }
 
@@ -78,14 +83,14 @@ public class PackageMaterialUpdaterIntegrationTest {
         addMetadata(material, "fieldX", false);
         material.getPackageDefinition().getConfiguration().addNewConfiguration("fieldX", true);
         final List<Modification> modifications = ModificationsMother.multipleModificationList();
-
-		transactionTemplate.execute(new TransactionCallback() {
-			@Override
-			public Object doInTransaction(TransactionStatus transactionStatus) {
-				packageMaterialUpdater.insertLatestOrNewModifications(material, materialInstance, new File(""), new Modifications(modifications));
-				return null;
-			}
-		});
+        doNothing().when(scmMaterialUpdater).insertLatestOrNewModifications(material, materialInstance, new File(""), new Modifications(modifications));
+        transactionTemplate.execute(new TransactionCallback() {
+            @Override
+            public Object doInTransaction(TransactionStatus transactionStatus) {
+                packageMaterialUpdater.insertLatestOrNewModifications(material, materialInstance, new File(""), new Modifications(modifications));
+                return null;
+            }
+        });
 
         MaterialInstance actualInstance = materialRepository.findMaterialInstance(material);
         assertThat(actualInstance.getConfiguration(), is(material.createMaterialInstance().getConfiguration()));

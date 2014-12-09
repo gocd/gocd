d509058b64c742ac2e64b5ff05dd5372d6b6fb8a
Aravind SV <arvind.sv@gmail.com>
2014-12-08 20:14:39 -0500

    #739 - Fix failing rspec spec.
    Had used the wrong namespace in the spec.

--- server/webapp/WEB-INF/rails.new/spec/models/api/feature_toggle_api_model_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/models/api/feature_toggle_api_model_spec.rb
@@ -16,7 +16,7 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-module Admin
+module Api
   describe FeatureToggleAPIModel do
     describe "initialization" do
       it "should populate correct data" do

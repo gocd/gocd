8dd177a96bcd946d30f96806f097c840eea2d36f
Jyoti Singh <jyotisingh7@gmail.com>
2014-12-12 17:45:02 +0530

    #718 - renamed extension for tasks to 'task' from 'task-extension'

--- plugin-infra/go-plugin-access/test/com/thoughtworks/go/plugin/access/pluggabletask/JsonBasedTaskExtensionTest.java
+++ plugin-infra/go-plugin-access/test/com/thoughtworks/go/plugin/access/pluggabletask/JsonBasedTaskExtensionTest.java
@@ -80,7 +80,7 @@ public class JsonBasedTaskExtensionTest {
         TaskConfig taskConfig = mock(TaskConfig.class);
 
         when(response.responseCode()).thenReturn(DefaultGoApiResponse.SUCCESS_RESPONSE_CODE);
-        when(pluginManager.isPluginOfType("task-extension", pluginId)).thenReturn(true);
+        when(pluginManager.isPluginOfType(JsonBasedTaskExtension.TASK_EXTENSION, pluginId)).thenReturn(true);
         when(response.responseBody()).thenReturn("{\"errors\":{\"key\":\"error\"}}");
         when(pluginManager.submitTo(eq(pluginId), any(GoPluginApiRequest.class))).thenReturn(response);
         ValidationResult validationResult = jsonBasedTaskExtension.validate(pluginId, taskConfig);

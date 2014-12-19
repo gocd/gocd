6387a6a92db89b51c0ca4d8e8e1fce048cce269a
Srinivas <srinivas.upadhya@gmail.com>
2014-12-19 09:54:42 +0530

    delete 'rails' containing rails-2 code

--- server/webapp/WEB-INF/rails/vendor/gems/rspec-1.2.8/lib/spec/matchers/method_missing.rb
+++
@@ -1,9 +0,0 @@
-module Spec
-  module Matchers
-    def method_missing(sym, *args, &block) # :nodoc:
-      return Matchers::Be.new(sym, *args) if sym.to_s =~ /^be_/
-      return Matchers::Has.new(sym, *args) if sym.to_s =~ /^have_/
-      super
-    end
-  end
-end
--- server/webapp/WEB-INF/rails/vendor/gems/rspec-rails-1.2.7.1/lib/autotest/discover.rb
+++
@@ -1,5 +0,0 @@
-Autotest.add_discovery do
-  style = []
-  style << "rails" if File.exist? 'config/environment.rb'
-  style
-end

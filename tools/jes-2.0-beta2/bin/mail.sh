000ffd47272457ccc5cd4c83a52357e51b792be2
Srinivas <srinivas.upadhya@gmail.com>
2014-12-19 16:52:13 +0530

    delete tools/jes-2.0-beta2

--- tools/jes-2.0-beta2/bin/mail.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-JAVA_EXEC="java"
-JES_HOME="/usr/local/jes"
-
-$JAVA_EXEC -client -Ddns.server= -Ddns.search= -Dsun.net.spi.nameservice.provider.1=dns,dnsjava -Djava.security.policy=$JES_HOME/jes.policy -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Log4JLogger -Dlog4j.configuration=file:$JES_HOME/conf/log4j.properties -cp $JES_HOME/jes.jar:$JES_HOME/lib/log4j-1.2.15.jar com.ericdaugherty.mail.server.Mail $JES_HOME

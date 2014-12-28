2f722e6317d64fc406b2923b08d0ba47247406a8
Srinivas <srinivas.upadhya@gmail.com>
2014-12-28 20:26:22 +0530

    scm extension point - add scm plugin end point

---
+++ plugin-infra/go-plugin-access/src/com/thoughtworks/go/plugin/access/scm/revision/ModifiedFile.java
@@ -0,0 +1,68 @@
+/*************************GO-LICENSE-START*********************************
+ * Copyright 2014 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *************************GO-LICENSE-END***********************************/
+
+package com.thoughtworks.go.plugin.access.scm.revision;
+
+import java.io.Serializable;
+
+public class ModifiedFile implements Serializable {
+    private String fileName;
+    private ModifiedAction action = ModifiedAction.unknown;
+
+    public ModifiedFile() {
+    }
+
+    public ModifiedFile(String fileName, ModifiedAction action) {
+        this.fileName = fileName;
+        this.action = action;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public ModifiedAction getAction() {
+        return action;
+    }
+
+    public void setAction(ModifiedAction action) {
+        this.action = action;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ModifiedFile that = (ModifiedFile) o;
+
+        if (action != that.action) return false;
+        if (fileName != null ? !fileName.equals(that.fileName) : that.fileName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fileName != null ? fileName.hashCode() : 0;
+        result = 31 * result + (action != null ? action.hashCode() : 0);
+        return result;
+    }
+}

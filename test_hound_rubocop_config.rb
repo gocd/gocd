621f398fc0b3d0468c5ec7b141296b703e0c672d
Aravind SV <arvind.sv@gmail.com>
2015-02-23 11:03:15 -0500

    #000 - Just to test hound and rubocop config. Ignore.

---
+++ test_hound_rubocop_config.rb
@@ -0,0 +1,11 @@
+describe "test hound" do
+  it "should replace . in username with _dot_" do
+    "This line should be less than 80 characters long."
+    2 + 3
+
+    "This line should be more than 80 characters long. Still not there. There we go."
+    4 + 8
+
+    "Here is a long line, so long, to check hound and rubocop. This line should be over 200 characters to get Hound to say it's a long line. Wow 200 is a lot of characters. Really? Haven't reached 200 yet. Now I have"
+  end
+end

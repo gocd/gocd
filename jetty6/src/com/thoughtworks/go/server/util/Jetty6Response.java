243c43a9f8354a1b8c7de5133ef5ee12140c8fd5
Jyoti Singh <jyotisingh7@gmail.com>
2015-03-04 02:47:25 +0530

    #400 - Bringing back jetty6 related code in a separate module jetty6.
    Both jetty6 and jett9 get packaged.
    Before starting go server, JVM arg 'app.server' could be updated to point to one of com.thoughtworks.go.server.Jetty9Server or com.thoughtworks.go.server.Jetty6Server to bring up the appropriate one. It defaults to jetty9.
    Added DevelopmentServerWithJetty6 to ease switching to jetty6 on dev machine.

---
+++ jetty6/src/com/thoughtworks/go/server/util/Jetty6Response.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.go.server.util;
+
+import org.mortbay.jetty.Response;
+
+public class Jetty6Response implements ServletResponse {
+    private javax.servlet.ServletResponse servletResponse;
+
+    public Jetty6Response(javax.servlet.ServletResponse servletResponse) {
+
+        this.servletResponse = servletResponse;
+    }
+
+    @Override
+    public int getStatus() {
+        return ((Response) servletResponse).getStatus();
+    }
+
+    @Override
+    public long getContentCount() {
+        return ((Response) servletResponse).getContentCount();
+    }
+}
--- tasks/jars.rake
+++ tasks/jars.rake
@@ -38,7 +38,8 @@ def server_launcher_dependencies
 end
 def jetty_jars
   jetty9_dependencies = Dir.glob($PROJECT_BASE + '/jetty9/target/lib/*.jar')
-  jetty9_dependencies
+  jetty6_dependencies = Dir.glob($PROJECT_BASE + '/jetty6/target/lib/*.jar')
+  jetty9_dependencies + jetty6_dependencies
 end
 
 def maven_dependency(groupid, artifactid, version, maven_repository = File.expand_path('~') + '/.m2/repository')

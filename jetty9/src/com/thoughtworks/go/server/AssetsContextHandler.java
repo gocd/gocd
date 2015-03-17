72a56f5fa93d12e3281571b219f834aca0a07cda
Jyoti Singh <jyotisingh7@gmail.com>
2015-03-17 14:58:43 +0530

    #400 - Removed unused method from AssetsContextHandler. updated test to use the new implementation

--- jetty9/src/com/thoughtworks/go/server/AssetsContextHandler.java
+++ jetty9/src/com/thoughtworks/go/server/AssetsContextHandler.java
@@ -46,18 +46,6 @@ public class AssetsContextHandler extends ContextHandler {
         handler.setAssetsDir(assetsDir);
     }
 
-    public void handle(String target, javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response, int dispatch) throws IOException, javax.servlet.ServletException {
-        if (shouldNotHandle()) return;
-        Request base_request = (request instanceof Request) ? (Request) request : null;
-        if (target.startsWith(this.getContextPath()) && !base_request.isHandled()) {
-            superDotHandle(target, base_request, request, response);
-        }
-    }
-
-    void superDotHandle(String target, Request base_request, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        super.handle(target, base_request, request, response);
-    }
-
     private boolean shouldNotHandle() {
         return !systemEnvironment.useCompressedJs();
     }
--- jetty9/test/unit/com/thoughtworks/go/server/AssetsContextHandlerTest.java
+++ jetty9/test/unit/com/thoughtworks/go/server/AssetsContextHandlerTest.java
@@ -22,28 +22,39 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.Before;
 import org.junit.Test;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
 public class AssetsContextHandlerTest {
-    @Test
-    public void shouldSetHeadersAndBaseDirectory() throws IOException {
-        SystemEnvironment systemEnvironment = mock(SystemEnvironment.class);
+
+
+    private AssetsContextHandler handler;
+    private SystemEnvironment systemEnvironment;
+
+    @Before
+    public void setUp() throws Exception {
+        systemEnvironment = mock(SystemEnvironment.class);
         when(systemEnvironment.getWebappContextPath()).thenReturn("/go");
         WebAppContext webAppContext = mock(WebAppContext.class);
         when(webAppContext.getInitParameter("rails.root")).thenReturn("/rails.root");
         when(webAppContext.getWebInf()).thenReturn(Resource.newResource("WEB-INF"));
-        AssetsContextHandler handler = new AssetsContextHandler(systemEnvironment);
+        handler = new AssetsContextHandler(systemEnvironment);
 
         handler.init(webAppContext);
+    }
 
+    @Test
+    public void shouldSetHeadersAndBaseDirectory() throws IOException {
         assertThat(handler.getContextPath(), is("/go/assets"));
         assertThat(handler.getHandler() instanceof AssetsContextHandler.AssetsHandler, is(true));
         AssetsContextHandler.AssetsHandler assetsHandler = (AssetsContextHandler.AssetsHandler) handler.getHandler();
@@ -53,53 +64,31 @@ public class AssetsContextHandlerTest {
     }
 
     @Test
-    public void shouldHandleIfTargetMatchesContextPath() throws IOException, ServletException {
-        SystemEnvironment systemEnvironment = mock(SystemEnvironment.class);
+    public void shouldPassOverHandlingToResourceHandler() throws IOException, ServletException {
         when(systemEnvironment.useCompressedJs()).thenReturn(true);
-        when(systemEnvironment.getWebappContextPath()).thenReturn("/go");
-        WebAppContext webAppContext = mock(WebAppContext.class);
-        when(webAppContext.getInitParameter("rails.root")).thenReturn("rails.root");
-        when(webAppContext.getWebInf()).thenReturn(Resource.newResource("WEB-INF"));
-        AssetsContextHandler handler = spy(new AssetsContextHandler(systemEnvironment));
-        handler.init(webAppContext);
+        String target = "/go/assets/junk";
         Request request = mock(Request.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
-        when(request.isHandled()).thenReturn(false);
-        handler.handle("/go/assets/junk", request, response, 1);
-        verify(handler).superDotHandle(eq("/go/assets/junk"), any(Request.class), eq(request), eq(response));
-    }
+        Request baseRequest = mock(Request.class);
+        ResourceHandler resourceHandler = mock(ResourceHandler.class);
+        ReflectionUtil.setField(handler.getHandler(), "resourceHandler", resourceHandler);
 
-    @Test
-    public void shouldNotHandleOnlyIfTargetDoesNotMatcheContextPath() throws IOException, ServletException {
-        SystemEnvironment systemEnvironment = mock(SystemEnvironment.class);
-        when(systemEnvironment.useCompressedJs()).thenReturn(true);
-        when(systemEnvironment.getWebappContextPath()).thenReturn("/go");
-        WebAppContext webAppContext = mock(WebAppContext.class);
-        when(webAppContext.getInitParameter("rails.root")).thenReturn("rails.root");
-        when(webAppContext.getWebInf()).thenReturn(Resource.newResource("WEB-INF"));
-        AssetsContextHandler handler = spy(new AssetsContextHandler(systemEnvironment));
-        handler.init(webAppContext);
-        Request request = mock(Request.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        when(request.isHandled()).thenReturn(false);
-        handler.handle("/junk", request, response, 1);
-        verify(handler, never()).superDotHandle(any(String.class), any(Request.class), any(HttpServletRequest.class), any(HttpServletResponse.class));
+        handler.getHandler().handle(target, baseRequest, request, response);
+        verify(resourceHandler).handle(target, baseRequest, request, response);
     }
 
     @Test
     public void shouldNotHandleForRails4DevelopmentMode() throws IOException, ServletException {
-        SystemEnvironment systemEnvironment = mock(SystemEnvironment.class);
         when(systemEnvironment.useCompressedJs()).thenReturn(false);
-        when(systemEnvironment.getWebappContextPath()).thenReturn("/go");
-        WebAppContext webAppContext = mock(WebAppContext.class);
-        when(webAppContext.getInitParameter("rails.root")).thenReturn("rails.root");
-        when(webAppContext.getWebInf()).thenReturn(Resource.newResource("WEB-INF"));
-        AssetsContextHandler handler = spy(new AssetsContextHandler(systemEnvironment));
-        handler.init(webAppContext);
+
+        String target = "/go/assets/junk";
         Request request = mock(Request.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
-        when(request.isHandled()).thenReturn(false);
-        handler.handle("/go/assets/junk", request, response, 1);
-        verify(handler, never()).superDotHandle(any(String.class), any(Request.class), any(HttpServletRequest.class), any(HttpServletResponse.class));
+        Request baseRequest = mock(Request.class);
+        ResourceHandler resourceHandler = mock(ResourceHandler.class);
+        ReflectionUtil.setField(handler.getHandler(), "resourceHandler", resourceHandler);
+
+        handler.getHandler().handle(target, baseRequest, request, response);
+        verify(resourceHandler, never()).handle(any(String.class), any(Request.class), any(HttpServletRequest.class), any(HttpServletResponse.class));
     }
 }

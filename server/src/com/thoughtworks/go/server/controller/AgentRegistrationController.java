b373c27a8f1ca070e4e062b48b1f29a52db3e5b8
Ali <mdaliejaz@gmail.com>
2015-03-18 10:25:16 +0530

    #825 - Changing Copyright year from 2014 to 2015 of the touched files.

--- server/src/com/thoughtworks/go/server/controller/AgentRegistrationController.java
+++ server/src/com/thoughtworks/go/server/controller/AgentRegistrationController.java
@@ -1,5 +1,5 @@
 /*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+ * Copyright 2015 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

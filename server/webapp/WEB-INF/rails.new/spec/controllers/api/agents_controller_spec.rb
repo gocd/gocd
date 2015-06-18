fadef03f20d051c7fa72ff093d123011a18a6bac
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-06-18 14:14:00 +0530

    fix specs

--- server/webapp/WEB-INF/rails.new/spec/controllers/api/agents_controller_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/controllers/api/agents_controller_spec.rb
@@ -20,7 +20,8 @@ describe Api::AgentsController do
   before do
     controller.stub(:agent_service).and_return(@agent_service = double('agent-service'))
     controller.stub(:job_instance_service).and_return(@job_instance_service = double('job instance service'))
-    controller.stub(:current_user).and_return(@user = Object.new)
+    login_as_user
+    become_admin
   end
 
   describe :index do

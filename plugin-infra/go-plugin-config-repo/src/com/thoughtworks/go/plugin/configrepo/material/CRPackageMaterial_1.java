86de8d5b195ee85d7080377ba4c82a83b1d3c377
Tomasz Setkowski <tom@ai-traders.com>
2015-07-05 21:15:52 +0200

    #1133 fix - CRPackageMaterial_1 equals impl

--- plugin-infra/go-plugin-config-repo/src/com/thoughtworks/go/plugin/configrepo/material/CRPackageMaterial_1.java
+++ plugin-infra/go-plugin-config-repo/src/com/thoughtworks/go/plugin/configrepo/material/CRPackageMaterial_1.java
@@ -24,6 +24,32 @@ public class CRPackageMaterial_1 extends CRMaterial_1 {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CRPackageMaterial_1 that = (CRPackageMaterial_1) o;
+        if(!super.equals(that))
+            return false;
+
+        if (packageId != null ? !packageId.equals(that.packageId) : that.packageId != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (packageId != null ? packageId.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String typeName() {
         return TYPE_NAME;
     }

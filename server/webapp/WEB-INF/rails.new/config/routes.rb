432936bf3ad75cb27e0965331b033d0bc646ee79
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-07-28 16:51:02 +0530

    #1354 - Get rid of users controller and view model

--- server/webapp/WEB-INF/rails.new/config/routes.rb
+++ server/webapp/WEB-INF/rails.new/config/routes.rb
@@ -234,7 +234,6 @@ Go::Application.routes.draw do
 
   namespace :api, as: "" do
     defaults :no_layout => true do
-      delete 'users/:username' => 'users#destroy', constraints: {username: USER_NAME_FORMAT}
       get 'plugins/status' => 'plugins#status'
 
       # state

0f7a8a550be2e7855657e1f053db35f8babb1a5d
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-09-03 14:34:00 +0530

    Move to using uglifier for javascript and css compression, instead of the deprecated yui

--- server/webapp/WEB-INF/rails.new/config/environments/production.rb
+++ server/webapp/WEB-INF/rails.new/config/environments/production.rb
@@ -1,5 +1,3 @@
-require 'yui/compressor'
-
 Go::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -26,8 +24,8 @@ Go::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :yui
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
---
+++ server/webapp/WEB-INF/rails.new/vendor/bundle/jruby/1.9/gems/execjs-2.6.0/lib/execjs/version.rb
@@ -0,0 +1,3 @@
+module ExecJS
+  VERSION = "2.6.0"
+end

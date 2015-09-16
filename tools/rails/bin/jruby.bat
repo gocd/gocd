c132011d740ba1a4d3af067af9e3f9c6e7d0fda6
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-09-16 16:06:17 +0530

    Fix bad windows batch file
    ...originally writen by yours truly, on something that was not a windows machine...

--- tools/rails/bin/jruby.bat
+++ tools/rails/bin/jruby.bat
@@ -1,9 +1,9 @@
-set GO_ROOT=%~dp0\..\..\..\..\
-set JRUBY_BASE=%GO_ROOT\tools\jruby-1.7.11
+set GO_ROOT=%~dp0\..\..\..
+set JRUBY_BASE=%GO_ROOT%\tools\jruby-1.7.11
 set SERVER_ROOT=%GO_ROOT%\server
 set RAILS_ROOT=%SERVER_ROOT%\webapp\WEB-INF\rails.new
 set GEM_HOME=%RAILS_ROOT%\vendor\bundle\jruby\1.9
-set GEM_PATH=%JRUBY_BASE%\lib\ruby\gems\shared:%GEM_HOME%
-set PATH=%JRUBY_BASE\bin:%PATH%
+set GEM_PATH=%JRUBY_BASE%\lib\ruby\gems\shared;%GEM_HOME%
+set PATH=%JRUBY_BASE%\bin;%PATH%
 
 %JRUBY_BASE%\bin\jruby.bat %*

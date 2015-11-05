1fc6444acdb90a9d664b2d8812919130bbd94ff9
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-11-05 15:09:02 +0530

    Test material connection (#1493)
    * Replace the old `GET /go/vcs.json` endpoint and move it to rails instead
    * Improve error handling and reporting and simplify code
    * Change existing usages of that endpoint to use the new endpoint
    * Add support for testing materials configs with params in them.
      Improve performance for testing materials configs with params
      in them by applying params to an empty pipeline instead of
      deep-cloning a huge `PipelineConfig` instance.

--- config/config-api/src/com/thoughtworks/go/config/preprocessor/ClassAttributeCache.java
+++ config/config-api/src/com/thoughtworks/go/config/preprocessor/ClassAttributeCache.java
@@ -1,23 +1,21 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2015 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.go.config.preprocessor;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -56,22 +54,10 @@ public abstract class ClassAttributeCache<K, T> {
         }
     }
 
-    public static class AnnotationPresentCache extends ClassAttributeCache<Map.Entry<AnnotatedElement, Class<? extends Annotation>>, Boolean> {
-        Boolean loadValues(Map.Entry<AnnotatedElement, Class<? extends Annotation>> entry) {
-            return entry.getKey().getAnnotation(entry.getValue()) != null;
-        }
-    }
-
     public static class AssignableCache extends ClassAttributeCache<Map.Entry<Class, Class>, Boolean> {
         Boolean loadValues(Map.Entry<Class, Class> entry) {
             return entry.getKey().isAssignableFrom(entry.getValue());
         }
     }
 
-    public static class AnnotationsCache extends ClassAttributeCache<Map.Entry<AnnotatedElement, Class<? extends Annotation>>, Annotation> {
-
-        Annotation loadValues(Map.Entry<AnnotatedElement, Class<? extends Annotation>> elementToAnnotation) {
-            return elementToAnnotation.getKey().getAnnotation(elementToAnnotation.getValue());
-        }
-    }
 }
--- server/webapp/WEB-INF/rails.new/spec/javascripts/check_connection_spec.js
+++ server/webapp/WEB-INF/rails.new/spec/javascripts/check_connection_spec.js
@@ -1,18 +1,18 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2015 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END**********************************/
+ */
 
 describe("check_connection", function () {
     var orig = WizardPage.checkConnection;
@@ -28,7 +28,7 @@ describe("check_connection", function () {
             "    <input type=\"checkbox\" class=\"passwordNotChanged\" value=\"0\"/>\n" +
             "    <input type=\"checkbox\" class=\"passwordChanged\" checked=\"checked\" value=\"0\"/>\n" +
             "</div>");
-        WizardPage.checkConnection = function (pipeline, url, username, password, materialUrl, materialType, isEncrypted) {
+        WizardPage.checkConnection = function (pipeline, username, password, materialUrl, materialType, isEncrypted) {
             actualPipeline = pipeline;
             actualMaterialUrl = materialUrl;
             actualMaterialType = materialType;

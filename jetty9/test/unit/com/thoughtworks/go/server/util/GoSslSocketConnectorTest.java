455a196aced76dd02b9de74a211e0821b3e581b2
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-12-09 17:44:54 +0530

    Handle `X-Forwarded-Proto` HTTP header properly (#1418)

--- jetty9/test/unit/com/thoughtworks/go/server/util/GoSslSocketConnectorTest.java
+++ jetty9/test/unit/com/thoughtworks/go/server/util/GoSslSocketConnectorTest.java
@@ -1,18 +1,18 @@
-/*************************GO-LICENSE-START*********************************
+/*
  * Copyright 2015 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.go.server.util;
 
@@ -32,6 +32,7 @@ import java.io.File;
 import java.util.Collection;
 import java.util.List;
 
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -101,8 +102,9 @@ public class GoSslSocketConnectorTest {
 
         HttpConnectionFactory httpConnectionFactory = getHttpConnectionFactory(connectionFactories);
         assertThat(httpConnectionFactory.getHttpConfiguration().getOutputBufferSize(), is(100));
-        assertThat(httpConnectionFactory.getHttpConfiguration().getCustomizers().size(), is(1));
-        assertThat(httpConnectionFactory.getHttpConfiguration().getCustomizers().get(0) instanceof SecureRequestCustomizer, is(true));
+        assertThat(httpConnectionFactory.getHttpConfiguration().getCustomizers().size(), is(2));
+        assertThat(httpConnectionFactory.getHttpConfiguration().getCustomizers().get(0), instanceOf(SecureRequestCustomizer.class));
+        assertThat(httpConnectionFactory.getHttpConfiguration().getCustomizers().get(1), instanceOf(ForwardedRequestCustomizer.class));
     }
 
     @Test

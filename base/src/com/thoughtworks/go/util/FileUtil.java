f713f43d8d7df95f73eff99dc51731fac69ff000
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-12-23 17:21:14 +0530

    Fix a file handle leak (#1743)
    This was caused by a combination of #1707 (which added a InputStream.close() at the wrong method)
    and #1709 (which removed the InputStream.close()) with the assumption that #1707 did the right thing.

--- base/src/com/thoughtworks/go/util/FileUtil.java
+++ base/src/com/thoughtworks/go/util/FileUtil.java
@@ -1,45 +1,32 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2015 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.go.util;
-
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Stack;
-import java.util.StringTokenizer;
-import java.util.UUID;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
-import static com.thoughtworks.go.util.ExceptionUtils.bomb;
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.URI;
+import java.util.*;
+
 import static java.lang.System.getProperty;
 
 public class FileUtil {
@@ -77,22 +64,21 @@ public class FileUtil {
     }
 
     public static String readToEnd(File file) throws IOException {
-        FileInputStream input = new FileInputStream(file);
-        try {
-            return readToEnd(input);
-        } finally {
-            IOUtils.closeQuietly(input);
-        }
+        return readToEnd(new FileInputStream(file));
     }
 
     public static String readToEnd(InputStream input) throws IOException {
-        @SuppressWarnings("unchecked") List<String> list = IOUtils.readLines(input);
-        StringBuilder builder = new StringBuilder();
-        for (String line : list) {
-            builder.append(line);
-            builder.append(lineSeparator());
+        try {
+            @SuppressWarnings("unchecked") List<String> list = IOUtils.readLines(input);
+            StringBuilder builder = new StringBuilder();
+            for (String line : list) {
+                builder.append(line);
+                builder.append(lineSeparator());
+            }
+            return builder.toString().trim();
+        } finally {
+            IOUtils.closeQuietly(input);
         }
-        return builder.toString().trim();
     }
 
     public static boolean isHidden(File file) {

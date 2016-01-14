5cc48ad1a381fcd14a8f6428a2d9a5eb1c4192ad
Ivan Moore <ivan@teamoptimization.com>
2016-01-14 11:44:09 +0000

    Ivan/Connor - added "python style" slices for materials

--- config/config-api/src/com/thoughtworks/go/domain/label/PipelineLabel.java
+++ config/config-api/src/com/thoughtworks/go/domain/label/PipelineLabel.java
@@ -19,6 +19,7 @@ package com.thoughtworks.go.domain.label;
 import com.thoughtworks.go.config.CaseInsensitiveString;
 import com.thoughtworks.go.util.StringUtil;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.IntRange;
 
 import java.io.Serializable;
 import java.util.Map;
@@ -42,7 +43,7 @@ public class PipelineLabel implements Serializable {
         return label;
     }
 
-    public static final Pattern PATTERN = Pattern.compile("(?i)\\$\\{([a-zA-Z0-9_\\-\\.!~'#:]+)(\\[:(\\d+)\\])?\\}");
+    public static final Pattern PATTERN = Pattern.compile("(?i)\\$\\{(?<identifier>[a-zA-Z0-9_\\-\\.!~'#:]+)(\\[(?<startIndex>\\-?\\d+)?(?<sliceColon>:)?(?<endIndex>\\-?\\d+)?\\])?\\}");
 
     private String replaceRevisionsInLabel(Map<CaseInsensitiveString, String> materialRevisions) {
         final Matcher matcher = PATTERN.matcher(this.label);
@@ -55,8 +56,33 @@ public class PipelineLabel implements Serializable {
         return buffer.toString();
     }
 
+    private String slice(String input, Integer start, Integer end) {
+        if (start == null) {
+            start = 0;
+        } else if (start >= input.length()) {
+            return "";
+        } else {
+            start = (start + input.length()) % input.length();
+        }
+
+        if (end == null || end >= input.length()) {
+            end = input.length();
+        } else {
+            end = (end + input.length()) % input.length();
+        }
+
+        return input.substring(start, end);
+    }
+
+    private Integer stringToIntOrNull(String string) {
+        if (string == null) {
+            return null;
+        }
+        return Integer.parseInt(string);
+    }
+
     private String lookupMaterialRevision(Matcher matcher,  Map<CaseInsensitiveString, String> materialRevisions) {
-        final CaseInsensitiveString material = new CaseInsensitiveString(matcher.group(1));
+        final CaseInsensitiveString material = new CaseInsensitiveString(matcher.group("identifier"));
 
         if (!materialRevisions.containsKey(material)) {
             //throw new IllegalStateException("cannot find material '" + material + "'");
@@ -64,14 +90,16 @@ public class PipelineLabel implements Serializable {
         }
 
         String revision = materialRevisions.get(material);
-        final String truncationLengthLiteral = matcher.group(3);
-        if (truncationLengthLiteral != null) {
-            int truncationLength = Integer.parseInt(truncationLengthLiteral);
+        Integer sliceStartIndex = stringToIntOrNull(matcher.group("startIndex"));
+        final String sliceColon = matcher.group("sliceColon");
+        Integer sliceEndIndex = stringToIntOrNull(matcher.group("endIndex"));
 
-            if (revision.length() > truncationLength) {
-                revision = revision.substring(0, truncationLength);
-            }
+        if (sliceStartIndex != null && sliceColon == null) {
+            revision = slice(revision, sliceStartIndex, sliceStartIndex + 1);
+        } else if (sliceColon != null) {
+            revision = slice(revision, sliceStartIndex, sliceEndIndex);
         }
+
         return revision;
     }
 

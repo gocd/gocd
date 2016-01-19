4bfd7d7eb5417a0afbc92ea65ec3c786966e9d90
Tomasz Setkowski <tom@ai-traders.com>
2016-01-19 09:27:33 +0000

    #1133 sync artifact with API

--- plugin-infra/go-plugin-config-repo/src/com/thoughtworks/go/plugin/configrepo/CRArtifact_1.java
+++ plugin-infra/go-plugin-config-repo/src/com/thoughtworks/go/plugin/configrepo/CRArtifact_1.java
@@ -5,16 +5,29 @@ import com.thoughtworks.go.util.StringUtil;
 public class CRArtifact_1 extends CRBase {
     private String source;
     private String destination;
+    private String type;
 
     public CRArtifact_1(){}
-    public CRArtifact_1(String src, String dest) {
+    public CRArtifact_1(String src, String dest,String type) {
         this.source = src;
         this.destination = dest;
+        this.type = type;
     }
 
     @Override
     public void getErrors(ErrorCollection errors) {
         validateSource(errors);
+        validateType(errors);
+    }
+
+    private void validateType(ErrorCollection errors) {
+        if (StringUtil.isBlank(type)) {
+            errors.add(this, "Artifact type is not set");
+        }
+        else if(!type.contentEquals("build") && !type.contentEquals("test"))
+        {
+            errors.add(this, "Artifact type must be 'build' or 'test'");
+        }
     }
 
     private void validateSource(ErrorCollection errors) {
@@ -47,6 +60,9 @@ public class CRArtifact_1 extends CRBase {
         if (destination != null ? !destination.equals(other.destination) : other.destination != null) {
             return false;
         }
+        if (type != null ? !type.equals(other.type) : other.type != null) {
+            return false;
+        }
         return !(source != null ? !source.equals(other.source) : other.source != null) ;
 
     }
@@ -55,6 +71,15 @@ public class CRArtifact_1 extends CRBase {
         int result = 0;
         result = 31 * result + (source != null ? source.hashCode() : 0);
         result = 31 * result + (destination != null ? destination.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
         return result;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
--- plugin-infra/go-plugin-config-repo/test/com/thoughtworks/go/plugin/configrepo/CRArtifact_1Test.java
+++ plugin-infra/go-plugin-config-repo/test/com/thoughtworks/go/plugin/configrepo/CRArtifact_1Test.java
@@ -1,17 +1,24 @@
 package com.thoughtworks.go.plugin.configrepo;
 
+import org.junit.Test;
+
 import java.util.Map;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CRArtifact_1Test extends CRBaseTest<CRArtifact_1> {
 
     private final CRArtifact_1 artifact;
     private final CRArtifact_1 invalidNoSource;
+    private final CRArtifact_1 invalidBadType;
 
     public CRArtifact_1Test()
     {
-        artifact = new CRArtifact_1("src","dest");
-        invalidNoSource = new CRArtifact_1(null,"dest");
-
+        artifact = new CRArtifact_1("src","dest","build");
+        invalidNoSource = new CRArtifact_1(null,"dest","test");
+        invalidBadType =  new CRArtifact_1("src","dest","bla");
     }
 
     @Override
@@ -22,5 +29,25 @@ public class CRArtifact_1Test extends CRBaseTest<CRArtifact_1> {
     @Override
     public void addBadExamples(Map<String, CRArtifact_1> examples) {
         examples.put("invalidNoSource",invalidNoSource);
+        examples.put("invalidBadType",invalidBadType);
+    }
+
+
+    @Test
+    public void shouldDeserializeFromAPILikeObject()
+    {
+        String json = "{\n" +
+                "      \"source\": \"test\",\n" +
+                "      \"destination\": \"res1\",\n" +
+                "      \"type\": \"test\"\n" +
+                "    }";
+        CRArtifact_1 deserializedValue = gson.fromJson(json,CRArtifact_1.class);
+
+        assertThat(deserializedValue.getSource(),is("test"));
+        assertThat(deserializedValue.getDestination(),is("res1"));
+        assertThat(deserializedValue.getType(),is("test"));
+
+        ErrorCollection errors = deserializedValue.getErrors();
+        assertTrue(errors.isEmpty());
     }
 }

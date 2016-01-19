30d8f42008f7d137642469c6b2ccb6ba21cb8f37
Tomasz Setkowski <tom@ai-traders.com>
2016-01-19 09:35:28 +0000

    #1133 added tab test to deserialize from API-like json

--- plugin-infra/go-plugin-config-repo/test/com/thoughtworks/go/plugin/configrepo/CRTab_1Test.java
+++ plugin-infra/go-plugin-config-repo/test/com/thoughtworks/go/plugin/configrepo/CRTab_1Test.java
@@ -1,6 +1,12 @@
 package com.thoughtworks.go.plugin.configrepo;
 
+import org.junit.Test;
+
 import java.util.Map;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CRTab_1Test extends CRBaseTest<CRTab_1> {
 
@@ -25,5 +31,22 @@ public class CRTab_1Test extends CRBaseTest<CRTab_1> {
     public void addBadExamples(Map<String, CRTab_1> examples) {
         examples.put("invalidTabNoName",invalidTabNoName);
         examples.put("invalidTabNoPath",invalidTabNoPath);
+    }
+
+
+    @Test
+    public void shouldDeserializeFromAPILikeObject()
+    {
+        String json = "{\n" +
+                "      \"name\": \"cobertura\",\n" +
+                "      \"path\": \"target/site/cobertura/index.html\"\n" +
+                "    }";
+        CRTab_1 deserializedValue = gson.fromJson(json,CRTab_1.class);
+
+        assertThat(deserializedValue.getName(),is("cobertura"));
+        assertThat(deserializedValue.getPath(),is("target/site/cobertura/index.html"));
+
+        ErrorCollection errors = deserializedValue.getErrors();
+        assertTrue(errors.isEmpty());
     }
 }

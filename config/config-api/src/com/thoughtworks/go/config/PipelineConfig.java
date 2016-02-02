58102abfcce668bf5e2ba4d82441361cc9f38def
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2016-02-02 14:20:24 +0530

    Remove unused annotation attribute.

--- config/config-api/src/com/thoughtworks/go/config/PipelineConfig.java
+++ config/config-api/src/com/thoughtworks/go/config/PipelineConfig.java
@@ -56,7 +56,7 @@ import static org.apache.commons.collections.CollectionUtils.select;
  * @understands how a cruise pipeline is configured by the user
  */
 @ConfigTag("pipeline")
-@ConfigCollection(value = StageConfig.class, asFieldName = "Stages")
+@ConfigCollection(StageConfig.class)
 public class PipelineConfig extends BaseCollection<StageConfig> implements ParamScope, ParamsAttributeAware,
         Validatable, EnvironmentVariableScope, ConfigOriginTraceable {
     private static final Cloner CLONER = new Cloner();

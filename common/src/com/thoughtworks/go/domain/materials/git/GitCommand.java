d7bc84d5cf95303a59fd6d43eca62e434c73af07
wpc <alex.hal9000@gmail.com>
2016-02-02 18:22:58 -0800

    #313 shallow clone: better console output when unshallow is happening

--- common/src/com/thoughtworks/go/domain/materials/git/GitCommand.java
+++ common/src/com/thoughtworks/go/domain/materials/git/GitCommand.java
@@ -292,7 +292,7 @@ public class GitCommand extends SCMCommand {
     // Special depth 2147483647 (Integer.MAX_VALUE) are treated as infinite -- fully unshallow
     // https://git-scm.com/docs/git-fetch-pack
     public void unshallow(ProcessOutputStreamConsumer outputStreamConsumer, Integer depth) {
-        outputStreamConsumer.stdOutput("[GIT] Unshallowing");
+        outputStreamConsumer.stdOutput(String.format("[GIT] Unshallowing repository with depth %d", depth));
         CommandLine gitFetch = git()
                 .withArgs("fetch", "origin")
                 .withArg(String.format("--depth=%d", depth))

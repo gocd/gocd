6fc2d60d923f53b6c01b47d969b9ed78999fd75a
Jyoti Singh <jyotisingh7@gmail.com>
2016-03-16 00:46:38 +0530

    UnhandledRequestTypeException was incorrectly marked as deprecated, reverting it

--- plugin-infra/go-plugin-api/src/com/thoughtworks/go/plugin/api/exceptions/UnhandledRequestTypeException.java
+++ plugin-infra/go-plugin-api/src/com/thoughtworks/go/plugin/api/exceptions/UnhandledRequestTypeException.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.go.plugin.api.exceptions;
 
-@Deprecated
 public class UnhandledRequestTypeException extends Exception {
     public UnhandledRequestTypeException(String requestType) {
         super("This is an invalid request type :" + requestType);

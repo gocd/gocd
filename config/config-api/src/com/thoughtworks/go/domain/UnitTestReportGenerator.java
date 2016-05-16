762a158d690b2a93ffa61dd39ef6803025a3ea3a
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2016-05-16 17:28:15 +0530

    Close input streams to prevent file leaks

--- config/config-api/src/com/thoughtworks/go/domain/UnitTestReportGenerator.java
+++ config/config-api/src/com/thoughtworks/go/domain/UnitTestReportGenerator.java
@@ -1,18 +1,18 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2016 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.go.domain;
 
@@ -52,9 +52,8 @@ public class UnitTestReportGenerator implements TestReportGenerator {
             mergedResource = mergeAllTestResultToSingleFile(allTestFiles);
             transformedHtml = new FileOutputStream(mergedResults);
 
-            try {
+            try(InputStream xslt = getClass().getResourceAsStream("unittests.xsl")) {
                 mergedFileStream = new FileInputStream(mergedResource);
-                InputStream xslt = getClass().getResourceAsStream("unittests.xsl");
                 Source xmlSource = new StreamSource(mergedFileStream);
                 Source xsltSource = new StreamSource(xslt);
                 TransformerFactory transFact = TransformerFactory.newInstance();
--- server/src/com/thoughtworks/studios/shine/semweb/grddl/XSLTTransformerRegistry.java
+++ server/src/com/thoughtworks/studios/shine/semweb/grddl/XSLTTransformerRegistry.java
@@ -1,29 +1,31 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2016 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.studios.shine.semweb.grddl;
 
-import java.util.Collection;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.io.InputStream;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.stream.StreamSource;
+
+import static com.thoughtworks.go.util.ExceptionUtils.bomb;
 
 /**
  * IMPORTANT: This is completely thread unsafe
@@ -32,16 +34,22 @@ public class XSLTTransformerRegistry {
     private final Map<String, Transformer> transformerMap;
 
     public XSLTTransformerRegistry() {
-        transformerMap = new HashMap<String, Transformer>();
-        register("cruise/pipeline-graph-grddl.xsl");
-        register("cruise/stage-graph-grddl.xsl");
-        register("cruise/job-grddl.xsl");
-        register("xunit/ant-junit-grddl.xsl");
-        register("xunit/nunit-to-junit.xsl");
+        transformerMap = new HashMap<>();
+        try {
+            register("cruise/pipeline-graph-grddl.xsl");
+            register("cruise/stage-graph-grddl.xsl");
+            register("cruise/job-grddl.xsl");
+            register("xunit/ant-junit-grddl.xsl");
+            register("xunit/nunit-to-junit.xsl");
+        } catch (IOException e) {
+            throw bomb(e);
+        }
     }
 
-    private void register(String xsltPath) {
-        transformerMap.put(xsltPath, transformerForXSLStream(getClass().getClassLoader().getResourceAsStream(xsltPath)));
+    private void register(String xsltPath) throws IOException {
+        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(xsltPath)) {
+            transformerMap.put(xsltPath, transformerForXSLStream(resourceAsStream));
+        }
     }
 
     public Transformer getTransformer(String xsltPath) {

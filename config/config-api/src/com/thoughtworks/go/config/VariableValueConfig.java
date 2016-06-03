9de838c1571c029a18fbe3509cc2bf9be763d950
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2016-06-03 19:09:17 +0530

    Specified validation error when encrypted value is given when secure is false.

--- config/config-api/src/com/thoughtworks/go/config/VariableValueConfig.java
+++ config/config-api/src/com/thoughtworks/go/config/VariableValueConfig.java
@@ -1,18 +1,18 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2016 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.go.config;
 
@@ -48,11 +48,8 @@ public class VariableValueConfig implements Serializable {
 
         VariableValueConfig that = (VariableValueConfig) o;
 
-        if (!value.equals(that.value)) {
-            return false;
-        }
+        return value != null ? value.equals(that.value) : that.value == null;
 
-        return true;
     }
 
     @Override
--- config/config-api/test/com/thoughtworks/go/config/domain/VariableValueConfigTest.java
+++ config/config-api/test/com/thoughtworks/go/config/domain/VariableValueConfigTest.java
@@ -1,18 +1,18 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2016 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.go.config.domain;
 
@@ -29,5 +29,10 @@ public class VariableValueConfigTest {
     @Test
     public void shouldBeEqualIfValuesAreEqual(){
         assertThat(new VariableValueConfig("abcd"), is(new VariableValueConfig("abcd")));
+    }
+
+    @Test
+    public void shouldReturnTrueIfBothValuesAreNull() {
+        assertThat(new VariableValueConfig(null), is(new VariableValueConfig(null)));
     }
 }

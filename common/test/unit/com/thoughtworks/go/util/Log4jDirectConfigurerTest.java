c9c9f375667c167fd9e47cc3d03298e1a05f5d61
Zabil Cheriya Maliackal <zabil@users.noreply.github.com>
2016-07-12 16:14:16 +0530

    Use junit temporary folders to test log4j file creation (#2425)

--- common/test/unit/com/thoughtworks/go/util/Log4jDirectConfigurerTest.java
+++ common/test/unit/com/thoughtworks/go/util/Log4jDirectConfigurerTest.java
@@ -1,5 +1,5 @@
 /*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+ * Copyright 2016 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,45 +24,45 @@ import static org.hamcrest.core.Is.is;
 import org.junit.After;
 import static org.junit.Assert.assertThat;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class Log4jDirectConfigurerTest {
-    private File file;
-    private File log4jExist;
-
-
-    @Before
-    public void setup() {
-        file = new File("anotherLog4jFile.properties");
-        log4jExist = new File("log4j-exist.properties");
-    }
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     @After
     public void teardown() {
-        FileUtils.deleteQuietly(file);
-        FileUtils.deleteQuietly(log4jExist);
+        temporaryFolder.delete();
     }
 
     @Test
     public void shouldCreateNewFileWhenLog4jDoesNotExist() throws Exception {
+        File file = new File(temporaryFolder.newFolder(), "anotherLog4jFile.properties");
+
         assertThat(file.exists(), is(false));
-        Log4jDirectConfigurer log4jDirectConfigurer = new Log4jDirectConfigurer(file.getName());
+
+        Log4jDirectConfigurer log4jDirectConfigurer = new Log4jDirectConfigurer(file.getAbsolutePath());
         log4jDirectConfigurer.afterPropertiesSet();
+
         assertThat(file.exists(), is(true));
     }
 
     @Test
     public void shouldNotModifyTheExistingLog4jFile() throws Exception {
-        createLog4J();
-        Log4jDirectConfigurer log4jDirectConfigurer = new Log4jDirectConfigurer(log4jExist.getName());
+        File file = new File(temporaryFolder.newFolder(), "log4j-exist.properties");
+        FileUtils.writeStringToFile(file, DEFAULT_LOG4J);
+
+        assertThat(file.exists(), is(true));
+
+        Log4jDirectConfigurer log4jDirectConfigurer = new Log4jDirectConfigurer(file.getAbsolutePath());
         log4jDirectConfigurer.afterPropertiesSet();
-        assertThat(FileUtils.readFileToString(log4jExist), is(DEFAULT_LOG4J));
+
+        assertThat(FileUtils.readFileToString(file), is(DEFAULT_LOG4J));
     }
 
     private static String DEFAULT_LOG4J = "log4j.rootLogger=INFO, FileAppender\n"
             + "log4j.logger.org.apache.velocity=ERROR\n";
 
-    private void createLog4J() throws IOException {
-        FileUtils.writeStringToFile(log4jExist, DEFAULT_LOG4J);
-    }
 }

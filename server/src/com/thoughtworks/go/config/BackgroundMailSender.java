d05746b57a1b28593ddcf667cbad0cea4dd65e30
Aravind SV <arvind.sv@gmail.com>
2016-07-26 09:32:59 +0530

    Added a friendlier error message on failing to send test email.

--- server/src/com/thoughtworks/go/config/BackgroundMailSender.java
+++ server/src/com/thoughtworks/go/config/BackgroundMailSender.java
@@ -1,18 +1,18 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2016 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.go.config;
 
@@ -20,8 +20,11 @@ import com.thoughtworks.go.domain.materials.ValidationBean;
 import com.thoughtworks.go.server.messaging.SendEmailMessage;
 import com.thoughtworks.go.util.GoConstants;
 import com.thoughtworks.go.util.SystemEnvironment;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class BackgroundMailSender implements GoMailSender {
+    private static final Log LOGGER = LogFactory.getLog(BackgroundMailSender.class);
 
     private static final Integer CRUISE_MAIL_SENDER_TIMEOUT = Integer.parseInt(
             SystemEnvironment.getProperty("cruise.mail.sender.timeout", String.valueOf(GoConstants.DEFAULT_TIMEOUT)));
@@ -29,8 +32,6 @@ public class BackgroundMailSender implements GoMailSender {
     private ValidationBean validation;
     private GoMailSender mailSender;
     private int timeout;
-    private static final String ERROR_MESSAGE = "Timeout when sending email."
-            + " Make sure your host, port, username, password and tls configuration are correct";
 
     public BackgroundMailSender(GoMailSender sender) {
         this(sender, CRUISE_MAIL_SENDER_TIMEOUT);
@@ -60,13 +61,12 @@ public class BackgroundMailSender implements GoMailSender {
             thread.join(timeout);
             if (thread.isAlive()) {
                 thread.interrupt();
-                return ValidationBean.notValid(
-                        ERROR_MESSAGE);
+                return ValidationBean.notValid(ERROR_MESSAGE);
             }
             return validation;
         } catch (InterruptedException e) {
-            return ValidationBean.notValid(
-                    ERROR_MESSAGE);
+            LOGGER.error("Timed out when sending an email. Please check email configuration.");
+            return ValidationBean.notValid(ERROR_MESSAGE);
         }
     }
 

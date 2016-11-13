b4d34e4062aed94dad13af2c00b817c6ef68ce47
Varsha Varadarajan <varshavaradarajan@users.noreply.github.com>
2016-11-13 10:54:02 +0530

    Disable material notifiers to avoid flakiness for material update. (#2857)

--- server/test/integration/com/thoughtworks/go/server/service/AutoBuildIntegrationTest.java
+++ server/test/integration/com/thoughtworks/go/server/service/AutoBuildIntegrationTest.java
@@ -1,18 +1,18 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2016 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.go.server.service;
 
@@ -26,6 +26,7 @@ import com.thoughtworks.go.server.cache.GoCache;
 import com.thoughtworks.go.server.dao.DatabaseAccessHelper;
 import com.thoughtworks.go.server.dao.PipelineDao;
 import com.thoughtworks.go.server.domain.PipelineTimeline;
+import com.thoughtworks.go.server.materials.DependencyMaterialUpdateNotifier;
 import com.thoughtworks.go.server.materials.MaterialChecker;
 import com.thoughtworks.go.server.persistence.MaterialRepository;
 import com.thoughtworks.go.server.transaction.TransactionTemplate;
@@ -64,6 +65,7 @@ public class AutoBuildIntegrationTest {
     @Autowired private MaterialChecker materialChecker;
     @Autowired private PipelineTimeline pipelineTimeline;
     @Autowired private ServerHealthService serverHealthService;
+    @Autowired private DependencyMaterialUpdateNotifier notifier;
 
     private GoConfigFileHelper configHelper = new GoConfigFileHelper();
     private ScheduleTestUtil scheduleUtil;
@@ -76,10 +78,12 @@ public class AutoBuildIntegrationTest {
 
         dbHelper.onSetUp();
         scheduleUtil = new ScheduleTestUtil(transactionTemplate, materialRepository, dbHelper, configHelper);
+        notifier.disableUpdates();
     }
 
     @After
     public void teardown() throws Exception {
+        notifier.enableUpdates();
         dbHelper.onTearDown();
         configHelper.onTearDown();
     }

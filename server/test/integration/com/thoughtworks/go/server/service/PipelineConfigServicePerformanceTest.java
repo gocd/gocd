172988f9771acf4d554e2e0e8e040695bc5bfc5f
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2016-12-07 15:01:56 +0530

    Use the data dir to extract the TFS SDK (#2941)

--- server/test/integration/com/thoughtworks/go/server/service/PipelineConfigServicePerformanceTest.java
+++ server/test/integration/com/thoughtworks/go/server/service/PipelineConfigServicePerformanceTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,6 +30,7 @@ import org.apache.log4j.*;
 import org.apache.log4j.spi.Filter;
 import org.apache.log4j.spi.LoggingEvent;
 import org.junit.*;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -97,6 +98,9 @@ public class PipelineConfigServicePerformanceTest {
     @Autowired
     private EntityHashingService entityHashingService;
 
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
     private GoConfigFileHelper configHelper;
     private final int numberOfRequests = 100;
     private HttpLocalizedOperationResult result;
@@ -117,7 +121,7 @@ public class PipelineConfigServicePerformanceTest {
         configHelper.usingCruiseConfigDao(goConfigDao).initializeConfigFile();
         configHelper.onSetUp();
         goConfigService.forceNotifyListeners();
-        File dumpDir = FileUtil.createTempFolder("perf-pipelineapi-test");
+        File dumpDir = tempFolder.newFolder("perf-pipelineapi-test");
         FileUtil.deleteDirectoryNoisily(dumpDir);
         dumpDir.mkdirs();
         result = new HttpLocalizedOperationResult();

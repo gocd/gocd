da7f85301e62e950873439ea2c416cca8ea66ac6
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2017-01-03 13:19:24 +0530

    Refactor to use one controller to check connection for repository and package.

--- server/webapp/WEB-INF/rails.new/app/controllers/api_v1/admin/internal/package_repository_check_connection_controller.rb
+++ server/webapp/WEB-INF/rails.new/app/controllers/api_v1/admin/internal/package_repository_check_connection_controller.rb
@@ -17,14 +17,26 @@
 module ApiV1
   module Admin
     module Internal
-      class RepositoryCheckConnectionController < ::ApiV1::BaseController
+      class PackageRepositoryCheckConnectionController < ::ApiV1::BaseController
         before_action :check_admin_user_or_group_admin_user_and_401
 
-        def check_connection
+        def repository_check_connection
           package_repository = PackageRepository.new
           result = HttpLocalizedOperationResult.new
-          ApiV1::Config::Materials::CheckConnectionRepresenter.new(package_repository).from_hash(params[:repository_check_connection])
+          ApiV1::Config::Materials::CheckConnectionRepresenter.new(package_repository).from_hash(params[:package_repository_check_connection])
           package_repository_service.checkConnection(package_repository, result)
+          render_http_operation_result(result)
+        end
+
+        def package_check_connection
+          package_definition = PackageDefinition.new
+          repository = package_repository_service.getPackageRepository(params[:repo_id])
+          raise RecordNotFound if repository.blank?
+
+          package_definition.setRepository(repository)
+          ApiV1::Config::Materials::CheckConnectionRepresenter.new(package_definition).from_hash(params[:package_repository_check_connection])
+          result = HttpLocalizedOperationResult.new
+          package_definition_service.checkConnection(package_definition, result)
           render_http_operation_result(result)
         end
 

06921dec8a32699e8ee42e99159fa46e0565a37e
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2017-05-09 16:14:07 +0530

    Display duration in human readable format (#3472)

--- server/webapp/WEB-INF/rails.new/spec/javascripts/pipeline_plan_spec.js
+++ server/webapp/WEB-INF/rails.new/spec/javascripts/pipeline_plan_spec.js
@@ -1,18 +1,18 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2017 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END**********************************/
+ */
 
 describe("pipeline_plan", function () {
     beforeEach(function () {
@@ -158,12 +158,12 @@ describe("pipeline_plan", function () {
             "                                {/if}\n" +
             "                                {if build.last_build_duration && build.last_build_duration != ''}\n" +
             "                                    {if parseInt(build.current_build_duration) > parseInt(build.last_build_duration)}\n" +
-            "                                        Longer by: ${%cruiseTimeConverter.fromSecondsToHHMMSS(parseInt(build.current_build_duration) - parseInt(build.last_build_duration))%}\n" +
+            "                                        Longer by: ${%moment.duration(parseInt(build.current_build_duration) - parseInt(build.last_build_duration), 's').humanizeForGoCD()%}\n" +
             "                                    {else}\n" +
-            "                                        Elapsed: ${%cruiseTimeConverter.fromSecondsToHHMMSS(parseInt(build.current_build_duration))%}, ETA: ${%cruiseTimeConverter.fromSecondsToHHMMSS(parseInt(build.last_build_duration) - parseInt(build.current_build_duration))%}\n" +
+            "                                        Elapsed: ${%moment.duration(parseInt(build.current_build_duration), 's').humanizeForGoCD() %}, ETA: ${%moment.duration(parseInt(build.last_build_duration) - parseInt(build.current_build_duration), 's').humanizeForGoCD() %}\n" +
             "                                    {/if}\n" +
             "                                {else}\n" +
-            "                                    Elapsed: ${%cruiseTimeConverter.fromSecondsToHHMMSS(parseInt(build.current_build_duration))%}\n" +
+            "                                    Elapsed: ${%moment.duration(parseInt(build.current_build_duration), 's').humanizeForGoCD() %}\n" +
             "                                {/if}\n" +
             "                              </div>\n" +
             "                            {/if}\n" +

7fd5f18116cfa19b71533954ed68ca954d78b375
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2017-05-15 15:15:56 +0530

    Set the creation time when jobs in a stage are rerun.

--- common/test/unit/com/thoughtworks/go/domain/StageTest.java
+++ common/test/unit/com/thoughtworks/go/domain/StageTest.java
@@ -1,18 +1,18 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2017 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.go.domain;
 
@@ -20,6 +20,7 @@ import com.thoughtworks.go.helper.StageMother;
 import com.thoughtworks.go.util.TimeProvider;
 import com.thoughtworks.go.utils.Timeout;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeUtils;
 import org.joda.time.Duration;
 import org.junit.Before;
 import org.junit.Test;
@@ -267,5 +268,27 @@ public class StageTest {
 
         stage = new Stage("foo-stage", new JobInstances(), "admin", "manual", new TimeProvider());
         assertThat(stage.getConfigVersion(), is(nullValue()));
+    }
+
+    @Test
+    public void shouldSetTheCurrentTimeAsCreationTimeForRerunOfJobs() {
+        Stage stage = new Stage("foo-stage", new JobInstances(), "admin", "manual", false, false, "git-sha", new TimeProvider());
+        Timestamp createdTimeOfRun1 = stage.getCreatedTime();
+        long minuteAfter = DateTimeUtils.currentTimeMillis() + 60000;
+        freezeTime(minuteAfter);
+        stage.prepareForRerunOf(new DefaultSchedulingContext("admin"), "git-sha");
+        resetTime();
+        Timestamp createdTimeOfRun2 = stage.getCreatedTime();
+
+        assertNotEquals(createdTimeOfRun1, createdTimeOfRun2);
+        assertEquals(createdTimeOfRun2, new Timestamp(minuteAfter));
+    }
+
+    private void freezeTime(Long millis) {
+        DateTimeUtils.setCurrentMillisFixed(millis);
+    }
+
+    private void resetTime() {
+        DateTimeUtils.setCurrentMillisSystem();
     }
 }

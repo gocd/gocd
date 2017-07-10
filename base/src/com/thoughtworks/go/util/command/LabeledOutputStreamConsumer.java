ca40b30989a80cf8cb6cdd1baa4ba393a0f3fcc6
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2017-07-10 17:20:51 +0530

    Remove spurious comments

--- base/src/com/thoughtworks/go/util/command/LabeledOutputStreamConsumer.java
+++ base/src/com/thoughtworks/go/util/command/LabeledOutputStreamConsumer.java
@@ -16,9 +16,6 @@
 
 package com.thoughtworks.go.util.command;
 
-/**
- * Created by marqueslee on 2/17/17.
- */
 public class LabeledOutputStreamConsumer implements ConsoleOutputStreamConsumer {
     private String tag;
     private String errorTag;

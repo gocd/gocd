2ca2b6bda52030953e2750258b889848fb1e385e
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2017-10-26 15:32:20 +0530

    Do not show settings icon for plugins that don't support it. * Add a spinner while the plugin settings is being loaded.

--- server/webapp/WEB-INF/rails.new/spec/webpack/views/plugins/plugins_widget_spec.js
+++ server/webapp/WEB-INF/rails.new/spec/webpack/views/plugins/plugins_widget_spec.js
@@ -32,202 +32,301 @@ describe("PluginsWidget", () => {
   });
   afterEach(window.destroyDomElementForTest);
 
-  const pluginSettingJSON = {
-    "plugin_id":  "github.oauth.login",
-    "configuration": [
-      {
-        "key":   "server_base_url",
-        "value": "https://localhost:8154/go"
-      },
-      {
-        "key":   "consumer_key",
-        "value": "foo"
-      }
-    ]
-  };
-
-  const githubAuthPluginInfoJSON = {
-    "id": "github.oauth.login",
-    "type": "authentication",
-    "status": {
+  const configRepoPluginInfoJSON = {
+    "id":                   "json.config.plugin",
+    "type":                 "configrepo",
+    "status":               {
       "state": "active"
     },
-    "plugin_file_location": "/server/plugins/external/github-oauth-login-2.4.jar",
-    "bundled_plugin": false,
-    "about": {
-      "name": "GitHub OAuth Login",
-      "version": "2.4",
-      "target_go_version": "16.2.1",
-      "description": "GitHub OAuth Login",
-      "target_operating_systems": [
-
-      ],
-      "vendor": {
+    "plugin_file_location": "/server/plugins/external/json.config.plugin-0.2.1.jar",
+    "bundled_plugin":       true,
+    "about":                {
+      "name":                     "JSON Config Plugin",
+      "version":                  "0.2",
+      "target_go_version":        "16.6.0",
+      "description":              "JSON Config Plugin",
+      "target_operating_systems": [],
+      "vendor":                   {
         "name": "GoCD Contributors",
-        "url": "https://github.com/gocd-contrib/gocd-oauth-login"
+        "url":  "https://github.com/tomzo/json-config-plugin"
       }
     },
-    "extension_info": {
+    "extension_info":       {
       "plugin_settings": {
         "configurations": [
           {
-            "key":      "server_base_url",
+            "key":      "pattern",
             "metadata": {
               "secure":   false,
-              "required": true
-            }
-          },
-          {
-            "key":      "consumer_key",
-            "metadata": {
-              "secure":   false,
-              "required": true
+              "required": false
             }
           }
         ],
-        "view": {
+        "view":           {
           "template": "plugin settings view"
         }
       }
     }
   };
-  const githubScmPluginInfoJSON = {
-    "id": "github.pr",
-    "type": "scm",
-    "status": {
-      "state": "active"
-    },
-    "plugin_file_location": "/server/plugins/external/github-pr-poller-1.3.3.jar",
-    "bundled_plugin": false,
-    "about": {
-      "name": "Github Pull Requests Builder",
-      "version": "1.3.3",
-      "target_go_version": "15.1.0",
-      "description": "Plugin that polls a GitHub repository for pull requests and triggers a build for each of them",
-      "target_operating_systems": [
 
-      ],
-      "vendor": {
-        "name": "Ashwanth Kumar",
-        "url": "https://github.com/ashwanthkumar/gocd-build-github-pull-requests"
-      }
-    },
-    "extension_info": {
-      "display_name": "GitHub",
-      "scm_settings": {
-        "configurations": [
-          {
-            "key": "url",
-            "metadata": {
-              "secure": false,
-              "required": true,
-              "part_of_identity": true
+  describe('functionality', () => {
+    const pluginSettingJSON = {
+      "plugin_id":     "github.oauth.login",
+      "configuration": [
+        {
+          "key":   "server_base_url",
+          "value": "https://localhost:8154/go"
+        },
+        {
+          "key":   "consumer_key",
+          "value": "foo"
+        }
+      ]
+    };
+
+    const githubAuthPluginInfoJSON = {
+      "id":                   "github.oauth.login",
+      "type":                 "authentication",
+      "status":               {
+        "state": "active"
+      },
+      "plugin_file_location": "/server/plugins/external/github-oauth-login-2.4.jar",
+      "bundled_plugin":       false,
+      "about":                {
+        "name":                     "GitHub OAuth Login",
+        "version":                  "2.4",
+        "target_go_version":        "16.2.1",
+        "description":              "GitHub OAuth Login",
+        "target_operating_systems": [],
+        "vendor":                   {
+          "name": "GoCD Contributors",
+          "url":  "https://github.com/gocd-contrib/gocd-oauth-login"
+        }
+      },
+      "extension_info":       {
+        "plugin_settings": {
+          "configurations": [
+            {
+              "key":      "server_base_url",
+              "metadata": {
+                "secure":   false,
+                "required": true
+              }
+            },
+            {
+              "key":      "consumer_key",
+              "metadata": {
+                "secure":   false,
+                "required": true
+              }
             }
+          ],
+          "view":           {
+            "template": "plugin settings view"
           }
-        ],
-        "view": {
-          "template": "scm settings view"
         }
       }
-    }
-  };
-
-  const yumPluginInfoJSON = {
-    "id": "yum",
-    "type": null,
-    "status": {
-      "state": "invalid",
-      "messages": ["Yum plugin not supported"]
-    },
-    "plugin_file_location": "/server/plugins/bundled/gocd-yum-repository-poller-plugin.jar",
-    "bundled_plugin": true,
-    "about": {
-      "name": "Yum Plugin",
-      "version": "2.0.3",
-      "target_go_version": "15.2.0",
-      "description": "Plugin that polls a yum repository",
-      "target_operating_systems": [
-        "Linux"
-      ],
-      "vendor": {
-        "name": "ThoughtWorks Go Team",
-        "url": "https://www.thoughtworks.com"
+    };
+    const githubScmPluginInfoJSON  = {
+      "id":                   "github.pr",
+      "type":                 "scm",
+      "status":               {
+        "state": "active"
+      },
+      "plugin_file_location": "/server/plugins/external/github-pr-poller-1.3.3.jar",
+      "bundled_plugin":       false,
+      "about":                {
+        "name":                     "Github Pull Requests Builder",
+        "version":                  "1.3.3",
+        "target_go_version":        "15.1.0",
+        "description":              "Plugin that polls a GitHub repository for pull requests and triggers a build for each of them",
+        "target_operating_systems": [],
+        "vendor":                   {
+          "name": "Ashwanth Kumar",
+          "url":  "https://github.com/ashwanthkumar/gocd-build-github-pull-requests"
+        }
+      },
+      "extension_info":       {
+        "display_name": "GitHub",
+        "scm_settings": {
+          "configurations": [
+            {
+              "key":      "url",
+              "metadata": {
+                "secure":           false,
+                "required":         true,
+                "part_of_identity": true
+              }
+            }
+          ],
+          "view":           {
+            "template": "scm settings view"
+          }
+        }
       }
-    }
-  };
+    };
 
-  const allPluginInfosJSON = [githubAuthPluginInfoJSON, githubScmPluginInfoJSON, yumPluginInfoJSON];
-  const pluginInfos     = Stream(PluginInfos.fromJSON([]));
-  const isUserAnAdmin      = Stream('true' === 'true');
-
-  beforeEach(() => {
-    jasmine.Ajax.install();
-    pluginInfos(PluginInfos.fromJSON(allPluginInfosJSON));
-
-    m.mount(root, {
-      view() {
-        return m(PluginsWidget, {
-          pluginInfos,
-          isUserAnAdmin
-        });
+    const yumPluginInfoJSON = {
+      "id":                   "yum",
+      "type":                 null,
+      "status":               {
+        "state":    "invalid",
+        "messages": ["Yum plugin not supported"]
+      },
+      "plugin_file_location": "/server/plugins/bundled/gocd-yum-repository-poller-plugin.jar",
+      "bundled_plugin":       true,
+      "about":                {
+        "name":                     "Yum Plugin",
+        "version":                  "2.0.3",
+        "target_go_version":        "15.2.0",
+        "description":              "Plugin that polls a yum repository",
+        "target_operating_systems": [
+          "Linux"
+        ],
+        "vendor":                   {
+          "name": "ThoughtWorks Go Team",
+          "url":  "https://www.thoughtworks.com"
+        }
       }
-    });
-    m.redraw(true);
-  });
+    };
 
-  afterEach(() => {
-    jasmine.Ajax.uninstall();
+    const allPluginInfosJSON = [githubAuthPluginInfoJSON, githubScmPluginInfoJSON, yumPluginInfoJSON];
+    const pluginInfos        = Stream(PluginInfos.fromJSON([]));
+    const isUserAnAdmin      = Stream('true' === 'true');
 
-    m.mount(root, null);
-    m.redraw();
+    beforeEach(() => {
+      jasmine.Ajax.install();
+      pluginInfos(PluginInfos.fromJSON(allPluginInfosJSON));
 
-    expect($('.new-modal-container .reveal')).not.toExist('Did you forget to close the modal before the test?');
-  });
-
-  describe("list all plugins", () => {
-    it("should list active and bad plugins alike", () => {
-      expect($root.find('.plugin .plugin-name')).toContainText(githubAuthPluginInfoJSON.about.name);
-      expect($root.find('.plugin .plugin-name')).toContainText(githubScmPluginInfoJSON.about.name);
-      expect($root.find('.plugin .plugin-name')).toContainText(yumPluginInfoJSON.about.name);
-
-      expect($root.find('.plugin .plugin-version .value')).toContainText(githubAuthPluginInfoJSON.about.version);
-      expect($root.find('.plugin .plugin-version .value')).toContainText(githubScmPluginInfoJSON.about.version);
-      expect($root.find('.plugin .plugin-version .value')).toContainText(yumPluginInfoJSON.about.version);
-
-      expect($root.find('.plugin .plugin-id')).toContainText(githubAuthPluginInfoJSON.id);
-      expect($root.find('.plugin .plugin-id')).toContainText(githubScmPluginInfoJSON.id);
-      expect($root.find('.plugin .plugin-id')).toContainText(yumPluginInfoJSON.id);
-    });
-
-    it("should show the additional information about loaded plugin", () => {
-      expect($root.find('.plugin .plugin-config-read-only')).toContainText(githubAuthPluginInfoJSON.about.description);
-      expect($root.find('.plugin .plugin-config-read-only')).toContainText(githubAuthPluginInfoJSON.about.vendor.name);
-      expect($root.find('.plugin .plugin-config-read-only')).toContainText(githubAuthPluginInfoJSON.about.target_operating_systems);
-      expect($root.find('.plugin .plugin-config-read-only')).toContainText(githubAuthPluginInfoJSON.plugin_file_location);
-      expect($root.find('.plugin .plugin-config-read-only')).toContainText(githubAuthPluginInfoJSON.about.target_go_version);
-    });
-
-    it("should show the reason for why a plugin is not loaded", () => {
-      expect($root.find('.plugin')).toContainText("Yum plugin not supported");
-    });
-
-  });
-
-  describe("add/edit plugin settings", () => {
-    afterEach(Modal.destroyAll);
-    it("should popup a new modal to allow editing plugin settings", () => {
-      jasmine.Ajax.stubRequest(`/go/api/admin/plugin_settings/${pluginSettingJSON.plugin_id}`, undefined, 'GET').andReturn({
-        responseText:    JSON.stringify(pluginSettingJSON),
-        responseHeaders: {
-          'ETag': '"foo"'
-        },
-        status:          200
+      m.mount(root, {
+        view() {
+          return m(PluginsWidget, {
+            pluginInfos,
+            isUserAnAdmin
+          });
+        }
       });
-      expect($root.find('.reveal:visible')).not.toBeInDOM();
+      m.redraw(true);
+    });
 
-      simulateEvent.simulate($root.find('.edit-plugin').get(0), 'click');
+    afterEach(() => {
+      jasmine.Ajax.uninstall();
+
+      m.mount(root, null);
       m.redraw();
-      expect($('.reveal:visible')).toBeInDOM();
+
+      expect($('.new-modal-container .reveal')).not.toExist('Did you forget to close the modal before the test?');
+    });
+
+    describe("list all plugins", () => {
+      it("should list active and bad plugins alike", () => {
+        expect($root.find('.plugin .plugin-name')).toContainText(githubAuthPluginInfoJSON.about.name);
+        expect($root.find('.plugin .plugin-name')).toContainText(githubScmPluginInfoJSON.about.name);
+        expect($root.find('.plugin .plugin-name')).toContainText(yumPluginInfoJSON.about.name);
+        expect($root.find('.plugin-actions')).toContainElement('a.edit-plugin');
+        expect($root.find('.plugin .plugin-version .value')).toContainText(githubAuthPluginInfoJSON.about.version);
+        expect($root.find('.plugin .plugin-version .value')).toContainText(githubScmPluginInfoJSON.about.version);
+        expect($root.find('.plugin .plugin-version .value')).toContainText(yumPluginInfoJSON.about.version);
+
+        expect($root.find('.plugin .plugin-id')).toContainText(githubAuthPluginInfoJSON.id);
+        expect($root.find('.plugin .plugin-id')).toContainText(githubScmPluginInfoJSON.id);
+        expect($root.find('.plugin .plugin-id')).toContainText(yumPluginInfoJSON.id);
+      });
+
+      it("should show the additional information about loaded plugin", () => {
+        expect($root.find('.plugin .plugin-config-read-only')).toContainText(githubAuthPluginInfoJSON.about.description);
+        expect($root.find('.plugin .plugin-config-read-only')).toContainText(githubAuthPluginInfoJSON.about.vendor.name);
+        expect($root.find('.plugin .plugin-config-read-only')).toContainText(githubAuthPluginInfoJSON.about.target_operating_systems);
+        expect($root.find('.plugin .plugin-config-read-only')).toContainText(githubAuthPluginInfoJSON.plugin_file_location);
+        expect($root.find('.plugin .plugin-config-read-only')).toContainText(githubAuthPluginInfoJSON.about.target_go_version);
+      });
+
+      it("should show the reason for why a plugin is not loaded", () => {
+        expect($root.find('.plugin')).toContainText("Yum plugin not supported");
+      });
+
+    });
+
+    describe("add/edit plugin settings", () => {
+      afterEach(Modal.destroyAll);
+      it("should popup a new modal to allow editing plugin settings", () => {
+        jasmine.Ajax.stubRequest(`/go/api/admin/plugin_settings/${pluginSettingJSON.plugin_id}`, undefined, 'GET').andReturn({
+          responseText:    JSON.stringify(pluginSettingJSON),
+          responseHeaders: {
+            'ETag': '"foo"'
+          },
+          status:          200
+        });
+        expect($root.find('.reveal:visible')).not.toBeInDOM();
+
+        simulateEvent.simulate($root.find('.edit-plugin').get(0), 'click');
+        m.redraw();
+        expect($('.reveal:visible')).toBeInDOM();
+      });
+    });
+  });
+
+  describe('for an admin', () => {
+    const allPluginInfosJSON = [configRepoPluginInfoJSON];
+    const pluginInfos        = Stream(PluginInfos.fromJSON([]));
+    const isUserAnAdmin      = Stream('true' === 'true');
+
+
+    beforeEach(() => {
+      pluginInfos(PluginInfos.fromJSON(allPluginInfosJSON));
+
+      m.mount(root, {
+        view() {
+          return m(PluginsWidget, {
+            pluginInfos,
+            isUserAnAdmin
+          });
+        }
+      });
+      m.redraw(true);
+    });
+
+    afterEach(() => {
+      m.mount(root, null);
+      m.redraw();
+    });
+
+    it("should show settings icon for admin", () => {
+      expect($root.find('.plugin .plugin-name')).toContainText(configRepoPluginInfoJSON.about.name);
+      expect($root.find('.plugin-actions')).toContainElement('a.edit-plugin');
+    });
+
+
+  });
+
+  describe('for a non-admin', () => {
+    const allPluginInfosJSON = [configRepoPluginInfoJSON];
+    const pluginInfos        = Stream(PluginInfos.fromJSON([]));
+    const isUserAnAdmin      = Stream('false' === 'true');
+
+
+    beforeEach(() => {
+      pluginInfos(PluginInfos.fromJSON(allPluginInfosJSON));
+
+      m.mount(root, {
+        view() {
+          return m(PluginsWidget, {
+            pluginInfos,
+            isUserAnAdmin
+          });
+        }
+      });
+      m.redraw(true);
+    });
+
+    afterEach(() => {
+      m.mount(root, null);
+      m.redraw();
+    });
+
+    it("should show the disabled settings icon for admin", () => {
+      expect($root.find('.plugin .plugin-name')).toContainText(configRepoPluginInfoJSON.about.name);
+      expect($root.find('.plugin-actions')).toContainElement('a.edit-plugin.disabled');
     });
   });
 });

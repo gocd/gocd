f197d81645b7f9855cf851f071007c5f0b383350
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2017-10-31 12:48:52 +0530

    Added missing license header

--- server/webapp/WEB-INF/rails.new/spec/javascripts/support/jasmine_with_selenium_runner.rb
+++ server/webapp/WEB-INF/rails.new/spec/javascripts/support/jasmine_with_selenium_runner.rb
@@ -1,3 +1,18 @@
+##########################GO-LICENSE-START################################
+# Copyright 2017 ThoughtWorks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################GO-LICENSE-END##################################
 # Heavily inspired by https://github.com/jasmine/jasmine_selenium_runner.
 # Couldn't use jasmine_selenium_runner directly as it depends on sauce-connect which internally depends on cmdparse.
 # cmdparse has a dependency on ruby 2.0+ :(

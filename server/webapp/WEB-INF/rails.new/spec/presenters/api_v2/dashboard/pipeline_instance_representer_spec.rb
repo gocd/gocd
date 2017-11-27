fe79b62c697764d407f1b4a8ce021bab13aa254b
Mahesh Panchaksharaiah <maheshp@thoughtworks.com>
2017-11-27 17:50:12 +0530

    Dashboard API including build_cause as part of pipeline instance json

--- server/webapp/WEB-INF/rails.new/spec/presenters/api_v2/dashboard/pipeline_instance_representer_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/presenters/api_v2/dashboard/pipeline_instance_representer_spec.rb
@@ -23,10 +23,8 @@ describe ApiV2::Dashboard::PipelineInstanceRepresenter do
     presenter = ApiV2::Dashboard::PipelineInstanceRepresenter.new(pipeline_instance_model_empty('p1', 's1'))
 
     actual_json = presenter.to_hash(url_builder: UrlBuilder.new)
-    expect(actual_json).to have_links(:self, :doc, :build_cause_url, :compare_url, :history_url, :vsm_url)
+    expect(actual_json).to have_links(:self, :compare_url, :history_url, :vsm_url)
     expect(actual_json).to have_link(:self).with_url('http://test.host/api/pipelines/p1/instance/0')
-    expect(actual_json).to have_link(:doc).with_url('https://api.go.cd/current/#get-pipeline-instance')
-    expect(actual_json).to have_link(:build_cause_url).with_url('http://test.host/pipelines/p1/0/build_cause')
     expect(actual_json).to have_link(:compare_url).with_url('http://test.host/compare/p1/-1/with/0')
     expect(actual_json).to have_link(:history_url).with_url('http://test.host/api/pipelines/p1/history')
     expect(actual_json).to have_link(:vsm_url).with_url('http://test.host/pipelines/value_stream_map/p1/0')
@@ -34,7 +32,6 @@ describe ApiV2::Dashboard::PipelineInstanceRepresenter do
   end
 
   it 'renders all pipeline instance with hal representation' do
-
     instance = pipeline_instance_model({:name => "p1", :label => "g1", :counter => 5, :stages => [{:name => "cruise", :counter => "10", :approved_by => "Anonymous"}]})
     date = instance.getScheduledDate()
     presenter = ApiV2::Dashboard::PipelineInstanceRepresenter.new(instance)
@@ -42,7 +39,76 @@ describe ApiV2::Dashboard::PipelineInstanceRepresenter do
     actual_json = presenter.to_hash(url_builder: UrlBuilder.new)
     actual_json.delete(:_links)
     actual_json.delete(:_embedded)
-    expect(actual_json).to eq({:label => "g1", :scheduled_at => date, :triggered_by => "Anonymous"})
+    expect(actual_json).to eq({:label => 'g1', :scheduled_at => date, :triggered_by => 'Triggered by Anonymous',
+                               :build_cause => {:approver => 'anonymous', :is_forced => true, :trigger_message=>"Forced by anonymous",
+                                                :material_revisions=>[] }})
+  end
+
+  it 'renders all pipeline instance with build_cause' do
+    instance = pipeline_instance_model({:name => "p1", :label => "g1", :counter => 5, :stages => [{:name => "cruise", :counter => "10", :approved_by => "Anonymous"}]})
+    material_revisions = ModificationsMother.createHgMaterialRevisions()
+    pipeline_dependency = ModificationsMother.dependencyMaterialRevision("up1", 1, "label", "first", 1, Time.now)
+    material_revisions.addRevision(pipeline_dependency)
+
+    instance.setBuildCause(BuildCause.createWithModifications(material_revisions, 'Anonymous'))
+
+    date = instance.getScheduledDate()
+    presenter = ApiV2::Dashboard::PipelineInstanceRepresenter.new(instance)
+
+    actual_json = presenter.to_hash(url_builder: UrlBuilder.new)
+    actual_json.delete(:_links)
+    actual_json.delete(:_embedded)
+
+    expected_build_cause = {:approver => 'Anonymous', :is_forced => false, :trigger_message => 'modified by user2',
+                            :material_revisions => [
+                              {:material_type => 'Mercurial',
+                               :material_name => 'hg-url',
+                               :changed => false,
+                               :modifications => [
+                                 {
+                                   :_links => {
+                                     :vsm => {
+                                       :href => 'http://test.host/materials/value_stream_map/4290e91721d0a0be34955725cfd754113588d9c27c39f9bd1a97c15e55832515/9fdcf27f16eadc362733328dd481d8a2c29915e1'
+                                     }},
+                                   :user_name => 'user2',
+                                   :email_address => 'email2',
+                                   :revision => '9fdcf27f16eadc362733328dd481d8a2c29915e1',
+                                   :modified_time => material_revisions.first.getModifications().first.getModifiedTime(),
+                                   :comment => 'comment2'
+                                 },
+                                 {
+                                   :_links => {
+                                     :vsm => {
+                                       :href => 'http://test.host/materials/value_stream_map/4290e91721d0a0be34955725cfd754113588d9c27c39f9bd1a97c15e55832515/eef77acd79809fc14ed82b79a312648d4a2801c6'}
+                                   },
+                                   :user_name => 'user1',
+                                   :email_address => 'email1',
+                                   :revision => 'eef77acd79809fc14ed82b79a312648d4a2801c6',
+                                   :modified_time => material_revisions.first.getModifications().last.getModifiedTime(),
+                                   :comment => 'comment1'
+                                 }]
+                              },
+                              {:material_type => "Pipeline",
+                               :material_name => "up1",
+                               :changed => false,
+                               :modifications => [
+                                 {:_links => {
+                                   :vsm => {
+                                     :href => 'http://test.host/pipelines/value_stream_map/up1/1'
+                                   },
+                                   :stage_details_url=> {
+                                       :href=>'http://test.host/pipelines/up1/1/first/1'
+                                   }
+                                 },
+                                  :revision => 'up1/1/first/1',
+                                  :modified_time => material_revisions.getMaterialRevision(1).getModifications().first.getModifiedTime(),
+                                  :pipeline_label => 'label'
+                                 }
+                               ]}
+                            ]
+    }
+
+    expect(actual_json).to eq({:label => 'g1', :scheduled_at => date, :triggered_by => 'Triggered by Anonymous', :build_cause => expected_build_cause})
   end
 
 end

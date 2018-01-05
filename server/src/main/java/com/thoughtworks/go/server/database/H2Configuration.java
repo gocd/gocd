4c8eed1ba406f214b50580ef51232113ee204b71
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2018-01-05 15:16:05 +0530

    Delete a class that does not warrant existance
    See #4141

--- server/src/main/java/com/thoughtworks/go/server/database/H2Configuration.java
+++ server/src/main/java/com/thoughtworks/go/server/database/H2Configuration.java
@@ -16,24 +16,17 @@
 
 package com.thoughtworks.go.server.database;
 
-import com.thoughtworks.go.configuration.DatabaseConfiguration;
 import com.thoughtworks.go.util.SystemEnvironment;
-
-import java.util.Properties;
 
 /*
     Responsible for loading H2 Configuration.
  */
-public class H2Configuration extends DatabaseConfiguration {
+public class H2Configuration  {
+
+    private SystemEnvironment systemEnvironment;
 
     public H2Configuration(SystemEnvironment systemEnvironment) {
-        super(systemEnvironment);
-    }
-
-    @Override
-    public Properties loadConfiguration() {
-        // Nothing to load as configuration is fetched by system environment
-        return null;
+        this.systemEnvironment = systemEnvironment;
     }
 
     public String getHost() {

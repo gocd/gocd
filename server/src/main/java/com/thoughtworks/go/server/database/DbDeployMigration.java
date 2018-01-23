a1907874491c9a8a5298cc754d27dcdce82eb9f2
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2018-01-23 18:46:58 +0530

    Improve logging and messaging around migrations

--- server/src/main/java/com/thoughtworks/go/server/database/DbDeployMigration.java
+++ server/src/main/java/com/thoughtworks/go/server/database/DbDeployMigration.java
@@ -20,12 +20,15 @@ import com.thoughtworks.go.util.SystemEnvironment;
 import net.sf.dbdeploy.InMemory;
 import net.sf.dbdeploy.database.syntax.HsqlDbmsSyntax;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.io.File;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.thoughtworks.go.util.ExceptionUtils.bomb;
 import static com.thoughtworks.go.util.ExceptionUtils.bombUnless;
@@ -47,21 +50,37 @@ public class DbDeployMigration implements Migration {
 
     private void upgradeWithDbDeploy() {
         LOG.info("Upgrading database at {}. This might take a while depending on the size of the database.", dataSource);
+
+        List<String> messages = Arrays.asList(
+                StringUtils.repeat("*", "", 72),
+                "WARNING: Shutting down your server at this point will lead to a database corruption. Please wait until the database upgrade completes.",
+                StringUtils.repeat("*", "", 72)
+        );
+        for (String message : messages) {
+            System.err.println(message);
+            LOG.info(message);
+        }
+
         File upgradePath = env.getDBDeltasPath();
         bombUnless(upgradePath.exists(), "Database upgrade scripts do not exist in directory " + upgradePath.getAbsolutePath());
         InMemory dbDeploy = new InMemory(dataSource, new HsqlDbmsSyntax(), upgradePath, "DDL");
         try {
             String migrationSql = dbDeploy.migrationSql();
             new JdbcTemplate(dataSource).execute(migrationSql);
+
+            System.err.println("Database upgrade completed successfully.");
+            LOG.info("Database upgrade completed.");
+
         } catch (Exception e) {
             String message = "Unable to create database upgrade script for database " + dataSource.getUrl() + ". The problem was: " + e.getMessage();
             if (e.getCause() != null) {
                 message += ". The cause was: " + e.getCause().getMessage();
             }
             LOG.error(message, e);
+            System.err.println(message);
+            e.printStackTrace(System.err);
             throw bomb(message, e);
         }
-        LOG.info("Database upgraded");
     }
 
 

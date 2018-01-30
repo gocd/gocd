7411250d016ebfe3b4ef070c6df85edd7ead1dd2
Isabelle Carter <isabellenataliecarter@gmail.com>
2018-01-30 10:58:27 -0800

    Adding filter to analytics controller to allow only admins

--- server/webapp/WEB-INF/rails.new/app/controllers/analytics_controller.rb
+++ server/webapp/WEB-INF/rails.new/app/controllers/analytics_controller.rb
@@ -19,7 +19,7 @@ class AnalyticsController < ApplicationController
 
   layout 'single_page_app', only: [:index]
 
-  before_action :check_user_and_401
+  before_action :check_admin_user_and_401
   before_action :check_user_can_see_pipeline, only: [:pipeline]
 
   def index

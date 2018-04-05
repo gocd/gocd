7f38370685042d3c18f95734dbe980fc43ec56d2
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2018-04-05 14:42:04 +0530

    Render bad request if the representer throws an exception while serializing or deserializing unsupported properties for resources.(#4608)

--- server/webapp/WEB-INF/rails.new/app/presenters/api_v3/base_representer.rb
+++ server/webapp/WEB-INF/rails.new/app/presenters/api_v3/base_representer.rb
@@ -98,12 +98,26 @@ module ApiV3
 
     end
 
+    def message
+      "There is something wrong with your request. This would usually happen if - \n* There is a API version mismatch. Version 3 of the API does not how to handle your specific request. Are you sure you're using an API version?\n* You submitted an incorrect data-type for one or more elements.\nPlease check the go server logs for more information."
+    end
+
     def to_hash(*options)
-      super.deep_symbolize_keys unless @represented.nil?
+      begin
+        super.deep_symbolize_keys
+      rescue Exception => e
+        Rails.logger.error(e)
+        raise ApiV3::BadRequest, message
+      end
     end
 
     def from_hash(data, options={})
-      super(with_default_values(data), options)
+      begin
+        super(with_default_values(data), options)
+      rescue Exception => e
+        Rails.logger.error(e)
+        raise ApiV3::BadRequest, message
+      end
     end
 
     private

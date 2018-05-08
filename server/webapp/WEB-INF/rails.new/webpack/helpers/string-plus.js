08a13950a12ddec02158c2d50112be7f6bacf2e4
Marques Lee <marques.lee@thoughtworks.com>
2018-05-08 14:00:50 -0700

    suppress lint for ternary operator nesting; this line isn't that hard to figure out.

--- server/webapp/WEB-INF/rails.new/webpack/helpers/string-plus.js
+++ server/webapp/WEB-INF/rails.new/webpack/helpers/string-plus.js
@@ -103,7 +103,7 @@ const mixins = {
     a = a.toLocaleLowerCase();
     b = b.toLocaleLowerCase();
 
-    return (a < b) ? -1 : (a > b) ? 1 : 0;
+    return (a < b) ? -1 : (a > b) ? 1 : 0; // eslint-disable-line no-nested-ternary
   }
 };
 

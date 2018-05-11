5c4a0a8f0a12ee84ff05ce8f9041b7e0d3402137
Aravind SV <mail@arvindsv.com>
2018-05-11 08:01:06 -0400

    Revert "Revert "Adding following entries in authorize filter chain""
    This reverts commit 260fe6677ccbc71a4ca175c8f191c23ea0bf8e88.

--- server/src/main/java/com/thoughtworks/go/server/newsecurity/filterchains/RememberLastRequestUrlFilterChain.java
+++ server/src/main/java/com/thoughtworks/go/server/newsecurity/filterchains/RememberLastRequestUrlFilterChain.java
@@ -34,6 +34,7 @@ public class RememberLastRequestUrlFilterChain extends FilterChainProxy {
                 .addFilterChain("/plugin/*/login")
                 .addFilterChain("/assets/**")
                 .addFilterChain("/server/messages.json")
+                .addFilterChain("/pipelines.json")
                 .addFilterChain("/**", new RememberLastRequestUrlFilter())
                 .build()
         );

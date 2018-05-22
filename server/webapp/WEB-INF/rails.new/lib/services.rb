59ecbf156d4d54509193984aaaa4497d3d76b52b
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2018-05-22 16:22:52 +0530

    Remove webpack-rails gem
    Use the java `WebpackAssetsService` instead.
    
    The rails <-> webpack integration provided by gems is not suitable for
    our needs and needs quite some hacky workarounds. So it's best to drop
    this integration before attempting a rails upgrade.

--- server/webapp/WEB-INF/rails.new/lib/services.rb
+++ server/webapp/WEB-INF/rails.new/lib/services.rb
@@ -104,6 +104,7 @@ module Services
     :version_info_service,
     :xml_api_service,
     :elastic_agent_plugin_service,
+    :webpack_assets_service
   )
 
   service_with_alias_name(:go_config_service_for_url, "goConfigService")

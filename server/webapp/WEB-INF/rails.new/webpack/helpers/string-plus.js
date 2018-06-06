4b953d3c4dfcc20b489f195d869793cf4dc8cab4
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2018-06-06 16:02:46 +0530

    Remove function left behind from previous commit

--- server/webapp/WEB-INF/rails.new/webpack/helpers/string-plus.js
+++ server/webapp/WEB-INF/rails.new/webpack/helpers/string-plus.js
@@ -81,16 +81,6 @@ const mixins = {
     return value;
   },
 
-  uuid: function guid() {
-    function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-    }
-
-    return `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
-  },
-
   terminateWithPeriod(str) {
     if (s.endsWith(str, '.')) {
       return str;

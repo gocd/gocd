d3efe516f9c8f9f803e6acc7e4920e2c1c1365eb
kierarad <kieraradman@gmail.com>
2018-08-28 16:24:44 -0700

    Style stacked modals

--- server/webapp/WEB-INF/rails.new/webpack/views/dashboard/personalization_editor.js
+++ server/webapp/WEB-INF/rails.new/webpack/views/dashboard/personalization_editor.js
@@ -52,7 +52,48 @@ function personalizeEditor(opts, personalization, model) {
     });
   }
 
-  new Modal({
+  function deleteView() {
+    new Modal({
+      title: "Delete View",
+      size: "delete-view",
+      body: () => {
+        return <span>
+          Do you want to delete <span class="view-name">{existing}</span> view?
+        </span>;
+      },
+      buttons: [{
+        text: "Yes",
+        onclick: () => {
+          personalization().removeFilter(existing, model.etag()).done((data) => {
+            model.currentView("Default");
+            model.names(personalization().names());
+            model.checksum(data.contentHash);
+
+            Modal.close();
+            model.onchange();
+          }).fail((xhr) => {
+            const reason = JSON.parse(xhr.responseText).message;
+            that.replace({
+              title: "Delete View",
+              size: "delete-view",
+              body: () => {
+                return <span class="server-error-response">
+                  <i class="icon_alert"></i>
+                  <span class="reason">
+                    Failed to delete view <span class="view-name">{name}</span>: {reason}
+                  </span>
+                </span>;
+              },
+              buttons: [{text: "Close"}]
+            });
+          }).always(() => {
+            m.redraw();
+          });
+        }}, {text: "Cancel", class: "btn-link"}, {text: "open another", onclick: () => {new Modal()}}]
+    });
+  }
+
+  this.modal = new Modal({
     title: existing ? `Edit ${opts.name}`: "Create new view",
     size: "personalize-editor",
     body: () => {

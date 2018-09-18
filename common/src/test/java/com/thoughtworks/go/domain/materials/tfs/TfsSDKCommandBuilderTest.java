f9d2c682d8f8c0309fbd84ecf44e024accb1944b
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2018-09-18 15:50:21 +0530

    Fix failing test on java 10/11. Fixes #5174
    See https://bugs.openjdk.java.net/browse/JDK-8202758

--- common/src/test/java/com/thoughtworks/go/domain/materials/tfs/TfsSDKCommandBuilderTest.java
+++ common/src/test/java/com/thoughtworks/go/domain/materials/tfs/TfsSDKCommandBuilderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 ThoughtWorks, Inc.
+ * Copyright 2018 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 
 package com.thoughtworks.go.domain.materials.tfs;
 
-import com.thoughtworks.go.util.NestedJarClassLoader;
 import com.thoughtworks.go.util.command.UrlArgument;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,8 +23,9 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.*;
 
 public class TfsSDKCommandBuilderTest {
     private TfsSDKCommandBuilder builder;
@@ -33,23 +33,34 @@ public class TfsSDKCommandBuilderTest {
     private final String USERNAME = "userName";
     private final String PASSWORD = "password";
     private final String computedWorkspaceName = "boo-yaa-goo-moo-foo";
-    private NestedJarClassLoader mockSdkLoader;
+    private ClassLoader mockSdkLoader;
+    private boolean invoked;
+    private String className;
 
     @Before
     public void setup() throws IOException, URISyntaxException {
-        mockSdkLoader = mock(NestedJarClassLoader.class);
+        // we intentionally don't use mockito to mock this
+        // see https://bugs.openjdk.java.net/browse/JDK-8202758
+        mockSdkLoader = new ClassLoader() {
+            @Override
+            public Class<?> loadClass(String name) throws ClassNotFoundException {
+                invoked = true;
+                className = name;
+                throw new RuntimeException();
+            }
+        };
         builder = new TfsSDKCommandBuilder(mockSdkLoader);
     }
 
     @Test
     public void shouldLoadTheCorrectImplementationOfSDKCommandViaTheNestedClassLoader() throws Exception {
-        when(mockSdkLoader.loadClass("com.thoughtworks.go.tfssdk.TfsSDKCommandTCLAdapter")).thenThrow(new RuntimeException());
         try {
             builder.buildTFSSDKCommand(null,new UrlArgument("url"), DOMAIN, USERNAME, PASSWORD, computedWorkspaceName, "$/project");
             fail("should have failed to load class as we are not wiring any dependencies");
         } catch (Exception e) {
             //Do not worry about load class failing. We're only asserting that load class is invoked with the right FQN for TFSSDKCommand
         }
-        verify(mockSdkLoader, times(1)).loadClass("com.thoughtworks.go.tfssdk.TfsSDKCommandTCLAdapter");
+        assertThat(invoked, is(true));
+        assertThat(className, is("com.thoughtworks.go.tfssdk.TfsSDKCommandTCLAdapter"));
     }
 }

d2ab677ed38d1adc6ff941478d3e6ef6b9750c92
Marques Lee <marques.lee@thoughtworks.com>
2018-10-22 17:41:39 -0700

    Introduce ConfigRepo API V2
    Differences from V1
    
      - no create, update, or delete
      - only implements `all()` and `get()` equivalents
      - responds with config-repo structure AND last parsed result as a single JSON entity
      - written to support config-repo SPA
      - introduce a new set of Java representers for materials and config-repos
        - these could be used in the future to support porting the V1 API from Rails to Spark

---
+++ api/api-config-repos-v2/src/main/java/com/thoughtworks/go/apiv2/configrepos/representers/ConfigRepoRepresenterV2.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.thoughtworks.go.apiv2.configrepos.representers;
+
+import com.thoughtworks.go.api.base.OutputWriter;
+import com.thoughtworks.go.api.representers.ConfigurationPropertyRepresenter;
+import com.thoughtworks.go.config.PartialConfigParseResult;
+import com.thoughtworks.go.config.remote.ConfigRepoConfig;
+
+import static com.thoughtworks.go.spark.Routes.ConfigRepos.*;
+
+public class ConfigRepoRepresenterV2 {
+    public static void toJSON(OutputWriter json, ConfigRepoConfig repo, PartialConfigParseResult result) {
+        attachLinks(json, repo);
+        json.add("id", repo.getId());
+        json.add("plugin_id", repo.getConfigProviderPluginName());
+        json.addChild("material", w -> MaterialRepresenter.toJSON(w, repo.getMaterialConfig()));
+        attachConfigurations(json, repo);
+
+        json.addChild("last_parse", w -> PartialConfigParseResultRepresenter.toJSON(w, result));
+    }
+
+    private static void attachLinks(OutputWriter json, ConfigRepoConfig repo) {
+        json.addLinks(links -> {
+            links.addLink("self", id(repo.getId()));
+            links.addAbsoluteLink("doc", DOC);
+            links.addLink("find", find());
+        });
+    }
+
+    private static void attachConfigurations(OutputWriter json, ConfigRepoConfig repo) {
+        json.addChildList("configuration", w -> ConfigurationPropertyRepresenter.toJSON(w, repo.getConfiguration()));
+    }
+}
---
+++ api/api-config-repos-v2/src/test/groovy/com/thoughtworks/go/apiv2/configrepos/representers/ConfigRepoRepresenterV2Test.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2018 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.thoughtworks.go.apiv2.configrepos.representers
+
+import com.thoughtworks.go.config.PartialConfigParseResult
+import com.thoughtworks.go.config.materials.mercurial.HgMaterialConfig
+import com.thoughtworks.go.config.remote.ConfigRepoConfig
+import com.thoughtworks.go.domain.config.Configuration
+import com.thoughtworks.go.spark.Routes
+import org.junit.jupiter.api.Test
+
+import static com.thoughtworks.go.api.base.JsonUtils.toObjectString
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson
+
+class ConfigRepoRepresenterV2Test {
+  private static final String TEST_PLUGIN_ID = "test.configrepo.plugin"
+  private static final String TEST_REPO_URL = "https://fakeurl.com"
+
+  @Test
+  void toJSON() {
+    String json = toObjectString({ w ->
+      ConfigRepoRepresenterV2.toJSON(w, repo("foo"), new PartialConfigParseResult("123", new RuntimeException("boom!")))
+    })
+
+    String self = "http://test.host/go${Routes.ConfigRepos.id("foo")}"
+    String find = "http://test.host/go${Routes.ConfigRepos.find()}"
+
+    assertThatJson(json).isEqualTo([
+      _links       : [
+        self: [href: self],
+        doc : [href: Routes.ConfigRepos.DOC],
+        find: [href: find],
+      ],
+
+      id           : "foo",
+      plugin_id    : TEST_PLUGIN_ID,
+      material     : [
+        type      : "hg",
+        attributes: [
+          name       : null,
+          url        : TEST_REPO_URL,
+          auto_update: true
+        ]
+      ],
+      configuration: [
+        [key: "foo", value: "bar"],
+        [key: "baz", value: "quu"]
+      ],
+
+      last_parse   : [
+        revision: "123",
+        success : false,
+        error   : "boom!"
+      ]
+    ])
+  }
+
+  static ConfigRepoConfig repo(String id) {
+    Configuration c = new Configuration()
+    c.addNewConfigurationWithValue("foo", "bar", false)
+    c.addNewConfigurationWithValue("baz", "quu", false)
+
+    HgMaterialConfig materialConfig = new HgMaterialConfig(TEST_REPO_URL, "")
+    ConfigRepoConfig repo = new ConfigRepoConfig(materialConfig, TEST_PLUGIN_ID, id)
+    repo.setConfiguration(c)
+
+    return repo
+  }
+}

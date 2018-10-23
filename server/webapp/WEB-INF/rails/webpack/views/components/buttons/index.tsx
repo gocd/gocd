103bccc71ae2b708d14fc8ed124055b2cd7d991c
GaneshSPatil <ganeshpl@thoughtworks.com>
2018-10-23 11:35:03 +0530

    Implement buttons component (#5232) * Create Primary, Secondary and Reset button of regular and small size

---
+++ server/webapp/WEB-INF/rails/webpack/views/components/buttons/index.tsx
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2018 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import {MithrilComponent} from "../../../jsx/mithril-component";
+import * as m from 'mithril';
+
+const styles     = require('./index.scss');
+const classnames = require('classnames/bind').bind(styles);
+
+export interface Attrs {
+  small?: boolean
+}
+
+class Button extends MithrilComponent<Attrs> {
+  private readonly type: string;
+
+  protected constructor(type: string) {
+    super();
+    this.type = type;
+  }
+
+  view(vnode: m.Vnode<Attrs>) {
+    const isSmall = vnode.attrs.small;
+
+    return (
+      <button {...vnode.attrs}
+              className={classnames(styles.button, {[styles.btnSmall]: isSmall}, styles[this.type])}>
+        {vnode.children}
+      </button>
+    );
+  }
+}
+
+export class Primary extends Button {
+  constructor() {
+    super('btn-primary');
+  }
+}
+
+export class Secondary extends Button {
+  constructor() {
+    super('btn-secondary');
+  }
+}
+
+export class Reset extends Button {
+  constructor() {
+    super('btn-reset');
+  }
+}

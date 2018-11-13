f2d37869d7d18e3b77447760b57882d2a55880c4
GaneshSPatil <ganeshpl@thoughtworks.com>
2018-11-13 11:58:20 +0530

    Version Updater In Typescript

---
+++ server/webapp/WEB-INF/rails/spec/webpack/models/shared/version_updater_spec.ts
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2018 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {VersionUpdater} from "models/shared/version_updater";
+
+require("jasmine-ajax");
+describe("VersionUpdater", () => {
+
+  describe("update", () => {
+    beforeEach(() => {
+      localStorage.clear();
+    });
+
+    it("should fetch the stale version info", () => {
+      jasmine.Ajax.withMock(() => {
+        jasmine.Ajax.stubRequest("/go/api/version_infos/stale", undefined, "GET").andReturn({
+          responseText: JSON.stringify({}),
+          status: 200
+        });
+
+        jasmine.Ajax.stubRequest("/go/api/version_infos/latest_version", undefined, "GET").andReturn({
+          responseText: JSON.stringify({}),
+          status: 200
+        });
+
+        const thirtyOneMinutesBack = new Date(Date.now() - 31 * 60 * 1000);
+
+        localStorage.setItem("versionCheckInfo", JSON.stringify({last_updated_at: thirtyOneMinutesBack}));
+
+        VersionUpdater.update();
+
+        const request = jasmine.Ajax.requests.at(0);
+
+        expect(request.method).toBe("GET");
+        expect(request.url).toBe("/go/api/version_infos/stale");
+        expect(request.requestHeaders["Content-Type"]).toContain("application/json");
+        expect(request.requestHeaders.Accept).toContain("application/vnd.go.cd.v1+json");
+
+        expect(JSON.parse(localStorage.getItem("system_notifications") as string)).toBeNull();
+      });
+    });
+
+    it("should skip updates if update tried in last half hour", () => {
+      jasmine.Ajax.withMock(() => {
+        const twentyNineMinutesBack = new Date(Date.now() - 29 * 60 * 1000);
+
+        localStorage.setItem("versionCheckInfo", JSON.stringify({last_updated_at: twentyNineMinutesBack}));
+
+        VersionUpdater.update();
+
+        expect(jasmine.Ajax.requests.count()).toBe(0);
+      });
+    });
+
+    it("should skip updates in absence of stale version info and update local storage with last update time", () => {
+      jasmine.Ajax.withMock(() => {
+        jasmine.Ajax.stubRequest("/go/api/version_infos/stale", undefined, "GET").andReturn({
+          responseText: JSON.stringify({}),
+          status: 200
+        });
+
+        jasmine.Ajax.stubRequest("/go/api/version_infos/latest_version", undefined, "GET").andReturn({
+          responseText: JSON.stringify({latest_version: "18.7.0-1234"}),
+          status: 200
+        });
+
+        const myDate = jasmine.createSpyObj("Date", ["getTime"]);
+
+        // @ts-ignore
+        spyOn(window, "Date").and.returnValue(myDate);
+
+        myDate.getTime.and.callFake(() => 123);
+
+        VersionUpdater.update();
+
+        expect(jasmine.Ajax.requests.count()).toBe(2);
+        expect(localStorage.getItem("versionCheckInfo")).toEqual("{\"last_updated_at\":123}");
+      });
+    });
+
+    it("should fetch latest version info if can update", () => {
+      jasmine.Ajax.withMock(() => {
+        jasmine.Ajax.stubRequest("/go/api/version_infos/stale", undefined, "GET").andReturn({
+          responseText: JSON.stringify({update_server_url: "update.server.url"}),
+          status: 200
+        });
+
+        jasmine.Ajax.stubRequest("update.server.url", undefined, "GET").andReturn({
+          responseText: JSON.stringify({}),
+          status: 200
+        });
+
+        VersionUpdater.update();
+
+        const request = jasmine.Ajax.requests.at(1);
+
+        expect(request.method).toBe("GET");
+        expect(request.url).toBe("update.server.url");
+        expect(request.requestHeaders.Accept).toContain("application/vnd.update.go.cd.v1+json");
+      });
+    });
+
+    it("should post the latest version info to server", () => {
+      jasmine.Ajax.withMock(() => {
+        jasmine.Ajax.stubRequest("/go/api/version_infos/stale", undefined, "GET").andReturn({
+          responseText: JSON.stringify({update_server_url: "update.server.url"}),
+          responseHeaders: {
+            "Content-Type": `application/vnd.go.cd.v1+json`,
+          },
+          status: 200
+        });
+
+        jasmine.Ajax.stubRequest("update.server.url", undefined, "GET").andReturn({
+          responseText: JSON.stringify({foo: "bar"}),
+          responseHeaders: {
+            "Content-Type": "application/json"
+          },
+          status: 200
+        });
+
+        jasmine.Ajax.stubRequest("/go/api/version_infos/go_server", undefined, "PATCH").andReturn({
+          responseText: JSON.stringify({}),
+          responseHeaders: {
+            "Content-Type": `application/vnd.go.cd.v1+json`
+          },
+          status: 200
+        });
+
+        const myDate = jasmine.createSpyObj("Date", ["getTime"]);
+
+        // @ts-ignore
+        spyOn(window, "Date").and.returnValue(myDate);
+
+        myDate.getTime.and.callFake(() => 123);
+
+        VersionUpdater.update();
+
+        const request = jasmine.Ajax.requests.at(2);
+
+        expect(request.method).toBe("PATCH");
+        expect(request.url).toBe("/go/api/version_infos/go_server");
+        expect(JSON.parse(request.params)).toEqual({foo: "bar"});
+        expect(request.requestHeaders["Content-Type"]).toContain("application/json");
+        expect(request.requestHeaders.Accept).toContain("application/vnd.go.cd.v1+json");
+        expect(localStorage.getItem("versionCheckInfo")).toEqual("{\"last_updated_at\":123}");
+      });
+    });
+
+    it("should post update check message in local storage if an update is available", () => {
+      jasmine.Ajax.withMock(() => {
+        jasmine.Ajax.stubRequest("/go/api/version_infos/stale", undefined, "GET").andReturn({
+          responseText: JSON.stringify({update_server_url: "update.server.url"}),
+          status: 200
+        });
+
+        jasmine.Ajax.stubRequest("update.server.url", undefined, "GET").andReturn({
+          responseText: JSON.stringify({}),
+          status: 200
+        });
+
+        jasmine.Ajax.stubRequest("/go/api/version_infos/go_server", undefined, "PATCH").andReturn({
+          responseText: JSON.stringify({}),
+          responseHeaders: {
+            "Content-Type": `application/vnd.go.cd.v1+json`
+          },
+          status: 200
+        });
+
+        jasmine.Ajax.stubRequest("/go/api/version_infos/latest_version", undefined, "GET").andReturn({
+          responseText: JSON.stringify({latest_version: "18.7.0-1234"}),
+          status: 200
+        });
+
+        VersionUpdater.update();
+
+        const request = jasmine.Ajax.requests.at(3);
+        expect(request.method).toBe("GET");
+        expect(request.url).toBe("/go/api/version_infos/latest_version");
+        expect(request.requestHeaders.Accept).toContain("application/vnd.go.cd.v1+json");
+        const notifications = JSON.parse(localStorage.getItem("system_notifications") as string);
+        expect(notifications.length).toBe(1);
+        expect(notifications[0].message).toBe("A new version of GoCD - 18.7.0-1234 is available.");
+        expect(notifications[0].type).toBe("UpdateCheck");
+        expect(notifications[0].link).toBe("https://www.gocd.org/download/");
+        expect(notifications[0].linkText).toBe("Learn more ...");
+        expect(notifications[0].read).toBe(false);
+        expect(notifications[0].id).not.toBeUndefined();
+      });
+    });
+  });
+});
---
+++ server/webapp/WEB-INF/rails/webpack/models/shared/version_updater.ts
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2018 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Routes from "gen/ts-routes";
+import * as $ from "jquery";
+import * as _ from "lodash";
+import {SystemNotifications} from "models/notifications/system_notifications";
+
+const mrequest = require("helpers/mrequest");
+
+interface VersionCheckInfo {
+  last_updated_at: number;
+}
+
+interface StaleVersionInfo {
+  component_name: string;
+  update_server_url: string;
+  installed_version: string;
+  latest_version: string | null;
+}
+
+interface LatestVersionFromUpdateServer {
+  message: string;
+  message_signature: string;
+  signing_public_key: string;
+  signing_public_key_signature: string;
+}
+
+interface LatestVersion {
+  latest_version?: string;
+}
+
+export class VersionUpdater {
+  static update() {
+    if (VersionUpdater.canUpdateVersion()) {
+      VersionUpdater.fetchStaleVersionInfo().then((data: StaleVersionInfo | {}) => {
+        _.isEmpty(data) ? VersionUpdater.markUpdateDoneAndNotify() : VersionUpdater.fetchLatestVersion(data as StaleVersionInfo);
+      });
+    }
+  }
+
+  private static fetchStaleVersionInfo() {
+    return $.ajax({
+      method: "GET",
+      url: Routes.apiv1StaleVersionInfoPath(),
+      beforeSend: mrequest.xhrConfig.forVersion("v1")
+    });
+  }
+
+  private static canUpdateVersion(): boolean {
+    const versionCheckInfoFromLocalStorage = localStorage.getItem("versionCheckInfo");
+
+    if (_.isEmpty(versionCheckInfoFromLocalStorage)) {
+      return true;
+    }
+
+    const versionCheckInfo: VersionCheckInfo = JSON.parse(versionCheckInfoFromLocalStorage as string);
+
+    const lastUpdateAt: Date = new Date(versionCheckInfo.last_updated_at);
+    const halfHourAgo: Date  = new Date(_.now() - 30 * 60 * 1000);
+
+    return halfHourAgo > lastUpdateAt;
+  }
+
+  private static markUpdateDoneAndNotify() {
+    VersionUpdater.markUpdateDone();
+    VersionUpdater.get().then((latestVersionNumber: string) => {
+      if (latestVersionNumber !== undefined) {
+        SystemNotifications.notifyNewMessage("UpdateCheck", `A new version of GoCD - ${latestVersionNumber} is available.`, "https://www.gocd.org/download/", "Learn more ...");
+      }
+    });
+  }
+
+  private static fetchLatestVersion(versionInfo: StaleVersionInfo) {
+    $.ajax({
+      method: "GET",
+      url: versionInfo.update_server_url,
+      beforeSend(xhr) {
+        xhr.setRequestHeader("Accept", "application/vnd.update.go.cd.v1+json");
+      },
+    }).then(VersionUpdater.updateLatestVersion);
+  }
+
+  private static markUpdateDone() {
+    const versionCheckInfo = JSON.stringify({last_updated_at: new Date().getTime()});
+    localStorage.setItem("versionCheckInfo", versionCheckInfo);
+  }
+
+  private static updateLatestVersion(data: LatestVersionFromUpdateServer) {
+    $.ajax({
+      method: "PATCH",
+      beforeSend: mrequest.xhrConfig.forVersion("v1"),
+      url: Routes.apiv1UpdateServerVersionInfoPath(),
+      data: JSON.stringify(data)
+    }).then(VersionUpdater.markUpdateDoneAndNotify);
+  }
+
+  private static get() {
+    return $.Deferred(function() {
+      const deferred = this;
+
+      const jqXHR = $.ajax({
+        method: "GET",
+        url: Routes.apiv1LatestVersionInfoPath(),
+        timeout: mrequest.timeout,
+        beforeSend: mrequest.xhrConfig.forVersion("v1"),
+        contentType: false
+      });
+
+      const didFulfill = (data: LatestVersion) => {
+        deferred.resolve(data.latest_version);
+      };
+
+      const didReject = () => {
+        deferred.reject(null);
+      };
+
+      jqXHR.then(didFulfill, didReject);
+    }).promise();
+  }
+}

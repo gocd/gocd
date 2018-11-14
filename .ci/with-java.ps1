d3a45368ff9e1639ec33b1c3b9b310c74a48fe6a
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2018-11-14 09:36:36 +0530

    Fixup java installation

--- .ci/with-java.ps1
+++ .ci/with-java.ps1
@@ -1,16 +1,16 @@
 $mod=($env:GO_PIPELINE_COUNTER % 3)
 
 function use_jdk() {
-    jabba install "$($args[0])=$($args[1])"
+    (jabba use "$($args[0])") -or (jabba install "$($args[0])=$($args[1])")
     jabba use "$($args[0])"
 }
 
 if ($mod -eq "0") {
     Write-Host "Using system JVM"
 } elseif ($mod -eq "1") {
-    use_jdk "oracle@1.10.0-2" "tgz+https://nexus.gocd.io/repository/s3-mirrors/local/jdk/openjdk-10.0.2_windows-x64_bin.tar.gz"
+    use_jdk "oracle@1.10" "tgz+https://nexus.gocd.io/repository/s3-mirrors/local/jdk/openjdk-10.0.2_windows-x64_bin.tar.gz"
 } elseif ($mod -eq "2") {
-    use_jdk "oracle@1.11.0-28" "zip+https://nexus.gocd.io/repository/s3-mirrors/local/jdk/openjdk-11_28_windows-x64_bin.zip"
+    use_jdk "oracle@1.11" "zip+https://nexus.gocd.io/repository/s3-mirrors/local/jdk/openjdk-11_28_windows-x64_bin.zip"
 }
 
 $command = "$($args[0])"

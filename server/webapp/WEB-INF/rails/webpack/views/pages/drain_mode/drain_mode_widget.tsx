a7a42d6b6f2901b62b5bf9d8a5c436c997ec8331
GaneshSPatil <ganeshpl@thoughtworks.com>
2018-12-12 15:49:54 +0530

    Extract out running jobs and running mdus into separate component

--- server/webapp/WEB-INF/rails/webpack/views/pages/drain_mode/drain_mode_widget.tsx
+++ server/webapp/WEB-INF/rails/webpack/views/pages/drain_mode/drain_mode_widget.tsx
@@ -15,15 +15,13 @@
  */
 
 import {MithrilViewComponent} from "jsx/mithril-component";
-import {Materials, ScmMaterialAttributes} from "models/drain_mode/material";
-import {DrainModeInfo, Job, StageLocator} from "models/drain_mode/types";
-import {CollapsiblePanel} from "views/components/collapsible_panel";
+import {DrainModeInfo, StageLocator} from "models/drain_mode/types";
 import {Switch} from "views/components/forms/input_fields";
-import {KeyValuePair} from "views/components/key_value_pair";
-import {Table} from "views/components/table";
 import {bind} from "classnames/bind";
 import * as m from "mithril";
-import * as Buttons from "views/components/buttons";
+import {DisabledSubsystemsWidget} from "views/pages/drain_mode/disabled_susbsystems_widget";
+import {JobInfoWidget} from "views/pages/drain_mode/running_jobs_widget.tsx";
+import {MDUInfoWidget} from "views/pages/drain_mode/running_mdus_widget";
 import * as styles from "./index.scss";
 
 const classnames = bind(styles);
@@ -80,104 +78,5 @@ export class DrainModeInfoWidget extends MithrilViewComponent<InfoAttrs> {
                      onCancelStage={vnode.attrs.onCancelStage}/>,
       <MDUInfoWidget materials={vnode.attrs.drainModeInfo.runningSystem.mdu}/>
     ];
-  }
-}
-
-interface JobInfoAttrs {
-  jobs?: Map<string, Job[]>;
-  onCancelStage: (stageLocator: StageLocator, e: Event) => void;
-}
-
-export class JobInfoWidget extends MithrilViewComponent<JobInfoAttrs> {
-
-  view(vnode: m.Vnode<JobInfoAttrs>): m.Children {
-    const runningStages: m.Children = [];
-    if (!vnode.attrs.jobs || vnode.attrs.jobs.size === 0) {
-      runningStages.push(<em>No running stages.</em>);
-    } else {
-      vnode.attrs.jobs.forEach((jobs, key) => {
-        const stageLocator = StageLocator.fromStageLocatorString(key);
-        runningStages.push(
-          <CollapsiblePanel header={<KeyValuePair inline={true} data={stageLocator.asMap()}/>}
-                            actions={JobInfoWidget.stageCancelButton(stageLocator, vnode)}>
-            <Table headers={["Job", "State", "Agent"]} data={JobInfoWidget.dataForTable(jobs)}/>
-          </CollapsiblePanel>
-        );
-      });
-    }
-
-    return (
-      <div>
-        <h3 className={styles.runningSystemHeader}>Running stages</h3>
-        <div>{runningStages}</div>
-      </div>
-    );
-  }
-
-  private static dataForTable(jobs: Job[]): m.Child[][] {
-    return jobs.reduce((groups: any[], job) => {
-      const jobName = <a href={`/go/tab/build/detail/${job.locator()}`}>{job.jobName}</a>;
-      groups.push([jobName, job.scheduledDate, JobInfoWidget.agentLink(job)]);
-      return groups;
-    }, []);
-  }
-
-  private static agentLink(job: Job): m.Children {
-    if (job.agentUUID) {
-      return <Buttons.Primary onclick={this.goTo.bind(this, `/go/agents/${job.agentUUID}`)}
-                              data-test-id="agent-link"
-                              small={true}>Agent</Buttons.Primary>;
-    }
-    return <em>(Not assigned)</em>;
-  }
-
-  private static stageCancelButton(stageLocator: StageLocator, vnode: m.Vnode<JobInfoAttrs>) {
-    return (<Buttons.Primary onclick={vnode.attrs.onCancelStage.bind(vnode.attrs, stageLocator)}
-                             data-test-id="job-link"
-                             small={true}>Cancel stage</Buttons.Primary>);
-  }
-
-  private static goTo(href: string, event: Event): void {
-    event.stopPropagation();
-    window.location.href = href;
-  }
-}
-
-class MDUInfoAttrs {
-  materials?: Materials;
-}
-
-export class MDUInfoWidget extends MithrilViewComponent<MDUInfoAttrs> {
-  view(vnode: m.Vnode<MDUInfoAttrs>): m.Children {
-    let inProgressMaterials;
-
-    if (!vnode.attrs.materials || vnode.attrs.materials.count() === 0) {
-      inProgressMaterials = <em>No material update is in progress.</em>;
-    } else {
-      inProgressMaterials = vnode.attrs.materials.allScmMaterials().map((material) => {
-        const attributes = material.attributes() as ScmMaterialAttributes;
-        const nameOrUrl  = attributes.name() ? attributes.name() : attributes.url();
-        const headerMap  = new Map([
-                                     ["Type", material.type()],
-                                     ["Name", nameOrUrl],
-                                     ["Auto Update", attributes.name()],
-                                     ["Started At", material.mduStartTime().toString()]
-                                   ]);
-
-        return (
-          <CollapsiblePanel
-            header={<KeyValuePair inline={true} data={headerMap}/>}>
-            <KeyValuePair data={material.attributesAsMap()}/>
-          </CollapsiblePanel>
-        );
-      });
-    }
-
-    return (
-      <div>
-        <h3 className={styles.runningSystemHeader}>Running MDUs</h3>
-        {inProgressMaterials}
-      </div>
-    );
   }
 }

9b6cbc292f86ce626264d7d6f8f3475c103d2832
GaneshSPatil <ganeshpl@thoughtworks.com>
2019-01-08 15:59:13 +0530

    Schedule material update for config repo materials upon creation or update (#5650)
    * Add a entityConfigChangeListener for config repo for scheduling
      material update for config repo materials.

---
+++ server/src/test-fast/java/com/thoughtworks/go/server/service/ConfigRepoServiceTest.java
@@ -0,0 +1,72 @@
+package com.thoughtworks.go.server.service;
+
+import com.thoughtworks.go.config.BasicCruiseConfig;
+import com.thoughtworks.go.config.materials.git.GitMaterialConfig;
+import com.thoughtworks.go.config.remote.ConfigRepoConfig;
+import com.thoughtworks.go.domain.materials.MaterialConfig;
+import com.thoughtworks.go.listener.EntityConfigChangedListener;
+import com.thoughtworks.go.plugin.access.configrepo.ConfigRepoExtension;
+import com.thoughtworks.go.server.materials.MaterialUpdateService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+class ConfigRepoServiceTest {
+    @Mock
+    private GoConfigService goConfigService;
+    @Mock
+    private SecurityService securityService;
+    @Mock
+    private EntityHashingService entityHashingService;
+    @Mock
+    private ConfigRepoExtension configRepoExtension;
+    @Mock
+    private MaterialUpdateService materialUpdateService;
+    @Mock
+    private MaterialConfigConverter converter;
+
+    private String repoId;
+    private String pluginId;
+    private ConfigRepoConfig configRepo;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+
+        this.repoId = "repo-1";
+        this.pluginId = "json-config-repo-plugin";
+        MaterialConfig repoMaterial = new GitMaterialConfig("https://foo.git", "master");
+        this.configRepo = new ConfigRepoConfig(repoMaterial, pluginId, repoId);
+    }
+
+    @Test
+    void shouldRegisterListener() {
+        new ConfigRepoService(goConfigService, securityService, entityHashingService, configRepoExtension, materialUpdateService, converter);
+        verify(goConfigService).register(any(EntityConfigChangedListener.class));
+    }
+
+    @Test
+    void shouldScheduleAMaterialUpdateOnConfigRepoChange() {
+        BasicCruiseConfig cruiseConfig = new BasicCruiseConfig();
+        cruiseConfig.getConfigRepos().add(configRepo);
+        when(goConfigService.getConfigForEditing()).thenReturn(cruiseConfig);
+
+        new ConfigRepoService(goConfigService, securityService, entityHashingService, configRepoExtension, materialUpdateService, converter);
+
+        ArgumentCaptor<EntityConfigChangedListener> listenerCaptor = ArgumentCaptor.forClass(EntityConfigChangedListener.class);
+
+        verify(goConfigService).register(listenerCaptor.capture());
+        EntityConfigChangedListener listner = listenerCaptor.getValue();
+
+        verify(materialUpdateService, never()).updateMaterial(any());
+
+        listner.onEntityConfigChange(configRepo);
+
+        verify(materialUpdateService, times(1)).updateMaterial(any());
+    }
+}

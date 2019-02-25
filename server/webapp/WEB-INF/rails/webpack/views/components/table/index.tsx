c04e1e60b398fe3b8a621cda274e92904252831c
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2019-02-25 10:17:12 +0530

    Initial version of the table component.

--- server/webapp/WEB-INF/rails/webpack/views/components/table/index.tsx
+++ server/webapp/WEB-INF/rails/webpack/views/components/table/index.tsx
@@ -14,43 +14,84 @@
  * limitations under the License.
  */
 
-import {MithrilViewComponent} from "jsx/mithril-component";
+import {MithrilComponent, MithrilViewComponent} from "jsx/mithril-component";
 import * as _ from "lodash";
 import * as m from "mithril";
+import {Stream} from "mithril/stream";
+import * as stream from "mithril/stream";
 import * as s from "underscore.string";
 import * as styles from "./index.scss";
 
+export abstract class Comparator<T> {
+  protected readonly data: Stream<T[]>;
+
+  protected constructor(data: Stream<T[]>) {
+    this.data = data;
+  }
+
+  abstract compare(element1: T, element2: T): number;
+
+  readonly sort = (reverse: boolean) => this.data(this.data().sort(this.getFn.bind(this, reverse)));
+
+  private getFn(direction: boolean, element1: T, element2: T) {
+    const result = this.compare(element1, element2);
+    return direction ? result : result * -1;
+  }
+}
+
 interface Attrs {
-  headers: m.Child[] | any;
+  headers: any;
   data: m.Child[][];
   "data-test-id"?: string;
 }
 
-export class Table extends MithrilViewComponent<Attrs> {
-  view(vnode: m.Vnode<Attrs>) {
+interface HeaderAttrs {
+  name: string;
+  comparator?: Comparator<any>;
+  width?: string;
+}
 
-    return (<table className={styles.table} data-test-id={vnode.attrs["data-test-id"] || "table"}>
-        <thead data-test-id="table-header">
-        <tr data-test-id="table-header-row">
-          {vnode.attrs.headers.map((header: any) => (<th>{Table.renderedValue(header)}</th>))}
-        </tr>
-        </thead>
-        <tbody data-test-id="table-body">
-        {
-          vnode.attrs.data.map((rows) => {
-            return (
-              <tr data-test-id="table-row">
-                {rows.map((row) => <td>{Table.renderedValue(row)}</td>)}
-              </tr>
-            );
-          })
-        }
-        </tbody>
-      </table>
-    );
+interface DefaultAttrs {
+  [key: string]: string | boolean;
+}
+
+interface HeaderState {
+  sortDirectionToggle: Stream<boolean>;
+}
+
+export class TableHeader extends MithrilComponent<HeaderAttrs, HeaderState> {
+  oninit(vnode: m.Vnode<HeaderAttrs, HeaderState>): any {
+    vnode.state.sortDirectionToggle = stream(false);
   }
 
-  private static renderedValue(value: m.Children) {
+  view(vnode: m.Vnode<HeaderAttrs, HeaderState>): m.Children | void | null {
+    const attributes: DefaultAttrs = {};
+    if (vnode.attrs.width) {
+      attributes.width = vnode.attrs.width;
+    }
+
+    return <th {...attributes}>
+      {Table.renderedValue(vnode.attrs.name)}
+      {TableHeader.sortButton(vnode)}
+    </th>;
+  }
+
+  private static sortButton(vnode: m.Vnode<HeaderAttrs, HeaderState>) {
+    if (vnode.attrs.comparator) {
+      return <span onclick={() => TableHeader.doSort(vnode)} className={styles.sortButton}>
+          <i class="fas fa-sort"/>
+        </span>;
+    }
+  }
+
+  private static doSort(vnode: m.Vnode<HeaderAttrs, HeaderState>) {
+    vnode.state.sortDirectionToggle(!vnode.state.sortDirectionToggle());
+    vnode.attrs.comparator!.sort(vnode.state.sortDirectionToggle());
+  }
+}
+
+export class Table extends MithrilViewComponent<Attrs> {
+  static renderedValue(value: m.Children) {
     // check booleans, because they're weird in JS :-/
     if (_.isBoolean(value) || _.isNumber(value)) {
       // toString() because `false` values will not be rendered
@@ -68,7 +109,36 @@ export class Table extends MithrilViewComponent<Attrs> {
     return value;
   }
 
+  view(vnode: m.Vnode<Attrs>) {
+    return (<table className={styles.table} data-test-id={vnode.attrs["data-test-id"] || "table"}>
+        <thead data-test-id="table-header">
+        <tr data-test-id="table-header-row">
+          {vnode.attrs.headers.map(Table.wrapInTHTagIfRequired)}
+        </tr>
+        </thead>
+        <tbody data-test-id="table-body">
+        {
+          vnode.attrs.data.map((rows) => {
+            return (
+              <tr data-test-id="table-row">
+                {rows.map((row) => <td>{Table.renderedValue(row)}</td>)}
+              </tr>
+            );
+          })
+        }
+        </tbody>
+      </table>
+    );
+  }
+
   private static unspecifiedValue() {
     return ("");
+  }
+
+  private static wrapInTHTagIfRequired(header: any) {
+    if (header.tag && header.tag.name === "TableHeader") {
+      return header;
+    }
+    return <th>{Table.renderedValue(header)}</th>;
   }
 }
--- server/webapp/WEB-INF/rails/webpack/views/components/table/spec/index_spec.tsx
+++ server/webapp/WEB-INF/rails/webpack/views/components/table/spec/index_spec.tsx
@@ -15,39 +15,164 @@
  */
 
 import * as m from "mithril";
-import {TestHelper} from "views/pages/artifact_stores/spec/test_helper";
-import {Table} from "../index";
+import * as stream from "mithril/stream";
+import * as simulateEvent from "simulate-event";
+import {Comparator, Table, TableHeader} from "../index";
+import * as styles from "../index.scss";
 
-describe("Table component", () => {
-  const helper = new TestHelper();
-  afterEach(helper.unmount.bind(helper));
+describe("TableComponent", () => {
+  let $root: any, root: Element;
 
-  const headers = ["Col1", "Col2", "Col3"];
-  const data    = [[1, 2, ""], [true, "two", null]];
+  beforeEach(() => {
+    // @ts-ignore
+    [$root, root] = window.createDomElementForTest();
+
+  });
+
+  afterEach(unmount);
+
+  const headers = ["Col1", "Col2", <lable>Col3</lable>];
+  const data    = [[1, 2, "something"], [true, "two", null]];
+
+  // @ts-ignore
+  afterEach(window.destroyDomElementForTest);
 
   it("should render table component", () => {
-    mount();
-    expect(helper.findByDataTestId("table")).toBeVisible();
-    expect(helper.findByDataTestId("table-header-row").length).toEqual(1);
-    expect(helper.findByDataTestId("table-row").length).toEqual(2);
+    mount(headers, data);
+    expect(findByDataTestId("table")).toBeVisible();
+    expect(findByDataTestId("table-header-row").length).toEqual(1);
+    expect(findByDataTestId("table-row").length).toEqual(2);
   });
 
   it("should render headers and data", () => {
-    mount();
-    expect(helper.findByDataTestId("table-header-row")).toContainText("Col1");
-    expect(helper.findByDataTestId("table-header-row")).toContainText("Col2");
-    expect(helper.findByDataTestId("table-header-row")).toContainText("Col3");
+    mount(headers, data);
+    expect(findByDataTestId("table-header-row")).toContainText("Col1");
+    expect(findByDataTestId("table-header-row")).toContainText("Col2");
+    expect(findByDataTestId("table-header-row")).toContainText("Col3");
 
-    expect(helper.findByDataTestId("table-row").get(0)).toContainText("1");
-    expect(helper.findByDataTestId("table-row").get(0)).toContainText("2");
-    expect(helper.findByDataTestId("table-row").get(0)).toContainText("");
+    expect(findByDataTestId("table-row").get(0)).toContainText("1");
+    expect(findByDataTestId("table-row").get(0)).toContainText("2");
+    expect(findByDataTestId("table-row").get(0)).toContainText("");
 
-    expect(helper.findByDataTestId("table-row").get(1)).toContainText("true");
-    expect(helper.findByDataTestId("table-row").get(1)).toContainText("two");
-    expect(helper.findByDataTestId("table-row").get(1)).toContainText("");
+    expect(findByDataTestId("table-row").get(1)).toContainText("true");
+    expect(findByDataTestId("table-row").get(1)).toContainText("two");
+    expect(findByDataTestId("table-row").get(1)).toContainText("");
   });
 
-  function mount() {
-    helper.mount(() => <Table headers={headers} data={data}/>);
+  describe("ColumnWidth", () => {
+    it("should not apply column width if not specified", () => {
+      mount([<TableHeader name={"Name"}/>, <TableHeader name={"Release Date"}/>], [[]]);
+
+      expect(findByTag("th")).not.toHaveAttr("width");
+    });
+
+    it("should apply column width in pixel", () => {
+      mount([<TableHeader name={"Name"} width="100px"/>, <TableHeader name={"Release Date"}/>], [[]]);
+
+      expect(findByTag("th")).toHaveAttr("width", "100px");
+    });
+
+    it("should apply column width in percentage", () => {
+      mount([<TableHeader name={"Name"} width="20%"/>, <TableHeader name={"Release Date"}/>], [[]]);
+
+      expect(findByTag("th")).toHaveAttr("width", "20%");
+    });
+  });
+
+  describe("Sort", () => {
+    const initialOrderOfData = "AZMCXNBYL";
+    const ascendingOrder     = "AZMBYLCXN";
+    const descendingOrder    = "CXNBYLAZM";
+
+    it("should have sort button for first column", () => {
+      const testData = getTestData();
+      mount(headersWithSortCapability(testData), testData());
+
+      expect(findByClass(styles.sortButton)).toHaveLength(1);
+      expect(findByClass(styles.sortButton).parent()).toHaveText("Col-1");
+    });
+
+    it("should sort data based on comparator", () => {
+      const testData = getTestData();
+      mount(headersWithSortCapability(testData), testData());
+
+      expect(findByDataTestId("table-row")).toHaveText(initialOrderOfData);
+
+      simulateEvent.simulate(findByClass(styles.sortButton).get(0), "click");
+      m.redraw();
+
+      expect(findByDataTestId("table-row")).toHaveText(ascendingOrder);
+    });
+
+    it("should reverse the sorted data if user click's twice on the sort button", () => {
+      const testData = getTestData();
+      mount(headersWithSortCapability(testData), testData());
+
+      expect(findByDataTestId("table-row")).toHaveText(initialOrderOfData);
+
+      simulateEvent.simulate(findByClass(styles.sortButton).get(0), "click");
+      m.redraw();
+
+      expect(findByDataTestId("table-row")).toHaveText(ascendingOrder);
+
+      simulateEvent.simulate(findByClass(styles.sortButton).get(0), "click");
+      m.redraw();
+
+      expect(findByDataTestId("table-row")).toHaveText(descendingOrder);
+    });
+  });
+
+  function mount(headers: any, data: any) {
+    m.mount(root, {
+      view() {
+        return (<Table headers={headers} data={data}/>);
+      }
+    });
+
+    // @ts-ignore
+    m.redraw(true);
+  }
+
+  function unmount() {
+    m.mount(root, null);
+    m.redraw();
+  }
+
+  function findByDataTestId(id: string) {
+    return $root.find(`[data-test-id='${id}']`);
+  }
+
+  function findByClass(className: string) {
+    return $root.find(`.${className}`);
+  }
+
+  function findByTag(tag: string) {
+    return $root.find(tag);
   }
 });
+
+function getTestData() {
+  return stream(
+    [
+      ["A", "Z", "M"],
+      ["C", "X", "N"],
+      ["B", "Y", "L"],
+    ]);
+}
+
+class TestComparator extends Comparator<string[]> {
+  constructor(testData: any) {
+    super(testData);
+  }
+
+  compare(element1: string[], element2: string[]): number {
+    return element1[0] < element2[0] ? -1 : element1[0] > element2[0] ? 1 : 0;
+  }
+}
+
+function headersWithSortCapability(testData: any) {
+  return [
+    <TableHeader name={"Col-1"} comparator={new TestComparator(testData)} width="10%"/>,
+    <TableHeader name={"Col-2"}/>,
+    <TableHeader name={"Col-3"}/>];
+}

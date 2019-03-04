b3bdfbc01e3f18d4e33df97aeda94b327d638184
GaneshSPatil <ganeshpl@thoughtworks.com>
2019-03-04 14:23:50 +0530

    Copy paste elastic agent extension from v4 to v5

---
+++ plugin-infra/go-plugin-access/src/main/java/com/thoughtworks/go/plugin/access/elastic/v5/CapabilitiesConverterV5.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.thoughtworks.go.plugin.access.elastic.v5;
+
+import com.thoughtworks.go.plugin.access.elastic.DataConverter;
+import com.thoughtworks.go.plugin.domain.elastic.Capabilities;
+
+class CapabilitiesConverterV5 implements DataConverter<Capabilities, CapabilitiesDTO> {
+    @Override
+    public Capabilities fromDTO(CapabilitiesDTO capabilitiesDTO) {
+        return new Capabilities(capabilitiesDTO.supportsStatusReport(), capabilitiesDTO.supportsAgentStatusReport());
+    }
+
+    @Override
+    public CapabilitiesDTO toDTO(Capabilities object) {
+        throw unsupportedOperationException(object.getClass().getName(), CapabilitiesDTO.class.getName());
+    }
+}

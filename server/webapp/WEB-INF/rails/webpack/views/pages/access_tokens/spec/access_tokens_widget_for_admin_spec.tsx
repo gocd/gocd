619898193d2e6152dc032d24561bdb676c14f09f
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2019-03-04 11:59:28 +0530

    Fixed specs for admin access token widget

--- server/webapp/WEB-INF/rails/webpack/views/pages/access_tokens/spec/access_tokens_widget_for_admin_spec.tsx
+++ server/webapp/WEB-INF/rails/webpack/views/pages/access_tokens/spec/access_tokens_widget_for_admin_spec.tsx
@@ -22,63 +22,56 @@ import {
   formatTimeInformation, getLastUsedInformation
 } from "views/pages/access_tokens/access_tokens_widget";
 import {AccessTokenTestData} from "views/pages/access_tokens/spec/test_data";
+import {TestHelper} from "views/pages/artifact_stores/spec/test_helper";
 
 describe("AccessTokensWidgetForAdminSpec", () => {
-  let $root: any, root: any;
+  const helper          = new TestHelper();
   const onRevoke        = jasmine.createSpy("onRevoke");
   const allAccessTokens = AccessTokens.fromJSON(AccessTokenTestData.all([AccessTokenTestData.validAccessToken()]));
   const validToken      = AccessToken.fromJSON(AccessTokenTestData.validAccessToken());
 
-  beforeEach(() => {
-    // @ts-ignore
-    [$root, root] = window.createDomElementForTest();
-  });
-
   function mount(accessTokens: Stream<AccessTokens>) {
-    m.mount(root, {
-      view() {
-        return <AccessTokensWidgetForAdmin accessTokens={accessTokens} onRevoke={onRevoke} searchText={stream()}/>;
-      }
-    });
-    m.redraw();
+    helper.mount(() => <AccessTokensWidgetForAdmin accessTokens={accessTokens} onRevoke={onRevoke}
+                                                   searchText={stream()}/>);
   }
 
   it("should be able to render info when no access tokens have been created", () => {
     mount(stream(new AccessTokens()));
 
-    expect(findByDataTestId("access_token_info")).toBeInDOM();
-    expect(findByDataTestId("access_token_info").text())
+    expect(helper.findByDataTestId("access_token_info")).toBeInDOM();
+    expect(helper.findByDataTestId("access_token_info").text())
       .toContain(
         "Click on \"Generate Token\" to create new personal access token.A Generated token can be used to access the GoCD API.");
 
-    expect(findByDataTestId("tab-header-0")).not.toBeInDOM();
-    expect(findByDataTestId("tab-header-1")).not.toBeInDOM();
+    expect(helper.findByDataTestId("tab-header-0")).not.toBeInDOM();
+    expect(helper.findByDataTestId("tab-header-1")).not.toBeInDOM();
   });
 
   it("should be able to render two tabs when access tokens are present", () => {
     mount(stream(allAccessTokens));
 
-    expect(findByDataTestId("access-token-info")).not.toBeInDOM();
-    expect(findByDataTestId("tab-header-0")).toBeInDOM();
-    expect(findByDataTestId("tab-header-0")).toHaveText("Active Tokens");
-    expect(findByDataTestId("tab-header-1")).toBeInDOM();
-    expect(findByDataTestId("tab-header-0").text()).toContain("Active Token");
+    expect(helper.findByDataTestId("access-token-info")).not.toBeInDOM();
+    expect(helper.findByDataTestId("tab-header-0")).toBeInDOM();
+    expect(helper.findByDataTestId("tab-header-0")).toHaveText("Active Tokens");
+    expect(helper.findByDataTestId("tab-header-1")).toBeInDOM();
+    expect(helper.findByDataTestId("tab-header-0").text()).toContain("Active Token");
   });
 
   it("should be able to render active access tokens data", () => {
     mount(stream(new AccessTokens(stream(validToken))));
 
-    const activeTokensTab = findByDataTestId("tab-header-0");
+    const activeTokensTab = helper.findByDataTestId("tab-header-0");
     expect(activeTokensTab).toBeInDOM();
     expect(activeTokensTab).toHaveText("Active Tokens");
-    const activeTokenTableHeaders = findIn(findByDataTestId("tab-content-0"), "table-header-row").find("th");
+    const activeTokenTableHeaders = helper.findIn(helper.findByDataTestId("tab-content-0"), "table-header-row")
+                                          .find("th");
     expect(activeTokenTableHeaders.eq(0)).toHaveText("Created By");
     expect(activeTokenTableHeaders.eq(1)).toHaveText("Description");
     expect(activeTokenTableHeaders.eq(2)).toHaveText("Created At");
     expect(activeTokenTableHeaders.eq(3)).toHaveText("Last Used");
     expect(activeTokenTableHeaders.eq(4)).toHaveText("Revoke");
 
-    const activeTokenTableRows = findIn(findByDataTestId("tab-content-0"), "table-row");
+    const activeTokenTableRows = helper.findIn(helper.findByDataTestId("tab-content-0"), "table-row");
     expect(activeTokenTableRows.length).toEqual(1);
     expect(activeTokenTableRows).toContainText(validToken.description());
     expect(activeTokenTableRows)
@@ -86,10 +79,10 @@ describe("AccessTokensWidgetForAdminSpec", () => {
     expect(activeTokenTableRows)
       .toContainText(getLastUsedInformation(validToken));
 
-    const revokedTokensTab = findByDataTestId("tab-header-1");
+    const revokedTokensTab = helper.findByDataTestId("tab-header-1");
     expect(revokedTokensTab).toHaveText("Revoked Tokens");
-    expect(findIn(findByDataTestId("tab-content-1"), "table-header-row")).not.toBeInDOM();
-    expect(findByDataTestId("tab-content-1")).toHaveText("You don't have any revoked tokens.");
+    expect(helper.findIn(helper.findByDataTestId("tab-content-1"), "table-header-row")).not.toBeInDOM();
+    expect(helper.findByDataTestId("tab-content-1")).toHaveText("You don't have any revoked tokens.");
   });
 
   it("should be able to render revoked access tokens data", () => {
@@ -98,7 +91,7 @@ describe("AccessTokensWidgetForAdminSpec", () => {
 
     mount(stream(allAccessTokens));
 
-    const revokedAccessTokensColumns = findIn(findByDataTestId("tab-content-1"), "table-row").find("td");
+    const revokedAccessTokensColumns = helper.findIn(helper.findByDataTestId("tab-content-1"), "table-row").find("td");
 
     expect(revokedAccessTokensColumns.length).toEqual(6);
     expect(revokedAccessTokensColumns.eq(0)).toHaveText(revokedToken.username());
@@ -110,20 +103,6 @@ describe("AccessTokensWidgetForAdminSpec", () => {
     expect(revokedAccessTokensColumns.eq(5)).toHaveText(revokedToken.revokeCause());
   });
 
-  afterEach(unmount);
-  // @ts-ignore
-  afterEach(window.destroyDomElementForTest);
+  afterEach(helper.unmount.bind(helper));
 
-  function unmount() {
-    m.mount(root, null);
-    m.redraw();
-  }
-
-  function findByDataTestId(id: string) {
-    return $root.find(`[data-test-id='${id}']`);
-  }
-
-  function findIn(elem: any, dataTestId: string) {
-    return elem.find(`[data-test-id='${dataTestId}']`);
-  }
 });

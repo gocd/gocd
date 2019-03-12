6383fe600ae72262e6d9eb77ca399e7d1aa686ee
GaneshSPatil <ganeshpl@thoughtworks.com>
2019-03-12 14:45:50 +0530

    Clear out local storage when reading of system notifications fails
    * Because of issue #5968, the system notification local storage data will
    be in a double escaped form.
    * Fix mentioned as part of commit d8e4a8a13e7b0c5f48cc0bee6305d603dec4488f
    will avoid causing similar situations.
    * For the users having badly escaped data, system notifications will not be
    shown on the header. Hence, Clear the current local storage. On the next
    page refresh, each notification handler will repopulate the local storage with
    appropriate notification message.

--- server/webapp/WEB-INF/rails/webpack/models/notifications/system_notifications.ts
+++ server/webapp/WEB-INF/rails/webpack/models/notifications/system_notifications.ts
@@ -77,8 +77,15 @@ export class SystemNotifications {
 
   static all() {
     return new Promise<SystemNotifications>((resolve) => {
-      const notifications: Notification[] = JSON.parse(localStorage.getItem("system_notifications") || "[]");
-      resolve(SystemNotifications.fromJSON(notifications));
+      try {
+        const notifications: Notification[] = JSON.parse(localStorage.getItem("system_notifications") || "[]");
+        const systemNotifications           = SystemNotifications.fromJSON(notifications);
+        resolve(systemNotifications);
+      } catch (e) {
+        //Badly escaped data causing system notification to fail while reading.
+        //clear the current local storage. on next page refresh, each notification handler will populate the new notification message.
+        localStorage.setItem("system_notifications", "[]");
+      }
     });
   }
 

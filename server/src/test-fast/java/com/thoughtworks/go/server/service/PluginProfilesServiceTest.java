e62c9616f05aa70067344de4a127fc28b73b72fb
GaneshSPatil <ganeshpl@thoughtworks.com>
2019-04-05 12:14:28 +0530

    Validate Elastic Agent Plugin exists for create and update elastic profiles API (#6087)

---
+++ server/src/test-fast/java/com/thoughtworks/go/server/service/PluginProfilesServiceTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.thoughtworks.go.server.service;
+
+import com.thoughtworks.go.config.PluginProfiles;
+import com.thoughtworks.go.config.elastic.ClusterProfile;
+import com.thoughtworks.go.config.exceptions.RecordNotFoundException;
+import com.thoughtworks.go.config.update.AddClusterProfileCommand;
+import com.thoughtworks.go.plugin.infra.GoPluginFrameworkException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+class PluginProfilesServiceTest {
+
+    @Mock
+    private GoConfigService goConfigService;
+
+    @Mock
+    private EntityHashingService hashingService;
+
+    @Mock
+    private AddClusterProfileCommand command;
+
+    private PluginProfilesService service;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+        service = new PluginProfilesService(goConfigService, hashingService) {
+            @Override
+            protected PluginProfiles getPluginProfiles() {
+                return null;
+            }
+        };
+    }
+
+    @Test
+    void shouldAddExceptionsThrownByPluginInfraErrorOnPluginProfile() {
+        ClusterProfile pluginProfile = new ClusterProfile();
+        String errorMessage = "Plugin is missing!";
+        Mockito.when(command.validateUsingExtension(any(), anyMap())).thenThrow(new GoPluginFrameworkException(errorMessage));
+
+        assertThat(pluginProfile.hasErrors()).isFalse();
+
+        service.validatePluginProperties(command, pluginProfile);
+
+        assertThat(pluginProfile.hasErrors()).isTrue();
+        assertThat(pluginProfile.errors().get("pluginId")).isEqualTo(Arrays.asList(errorMessage));
+    }
+
+    @Test
+    void shouldAddNotFoundExceptionsThrownOnPluginProfile() {
+        ClusterProfile pluginProfile = new ClusterProfile("foo", "plugin-id");
+        String errorMessage = "Plugin with id `plugin-id` is not found.";
+        Mockito.when(command.validateUsingExtension(any(), anyMap())).thenThrow(new RecordNotFoundException("Boom!"));
+
+        assertThat(pluginProfile.hasErrors()).isFalse();
+
+        service.validatePluginProperties(command, pluginProfile);
+
+        assertThat(pluginProfile.hasErrors()).isTrue();
+        assertThat(pluginProfile.errors().get("pluginId")).isEqualTo(Arrays.asList(errorMessage));
+    }
+}

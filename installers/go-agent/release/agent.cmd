b54c342ceb115e04fa4b5acde8f55a7c51308ff6
Kritika Singh <kritika.singh1994@gmail.com>
2019-04-08 15:12:44 +0530

    Cleaned up the windows agent and server bash startup file. Removed the debug, log and yourkit (for agent) setup as it is not utilized. Removed the fallback for GO_SERVER and GO_SERVER_PORT

--- installers/go-agent/release/agent.cmd
+++ installers/go-agent/release/agent.cmd
@@ -1,6 +1,6 @@
 @echo off
 REM #########################################################################
-REM Copyright 2015 ThoughtWorks, Inc.
+REM Copyright 2019 ThoughtWorks, Inc.
 REM
 REM Licensed under the Apache License, Version 2.0 (the "License");
 REM you may not use this file except in compliance with the License.
@@ -21,38 +21,12 @@ if not defined STOP_BEFORE_STARTUP set STOP_BEFORE_STARTUP=Y
 if not defined AGENT_DIR set AGENT_DIR=%cd%
 if not defined AGENT_MEM set AGENT_MEM=128m
 if not defined AGENT_MAX_MEM set AGENT_MAX_MEM=256m
-set GOCD_LEGACY_MODE=
-if defined GO_SERVER set GOCD_LEGACY_MODE=true
-if defined GO_SERVER_PORT set GOCD_LEGACY_MODE=true
 
-if defined GOCD_LEGACY_MODE (
-  echo "The environment variable GO_SERVER and GO_SERVER_PORT has been deprecated in favor of GO_SERVER_URL. Please set GO_SERVER_URL instead to a https url (https://example.com:8154/go)"
-  set GO_LEGACY_MODE=
-)
-
-if not defined GO_SERVER_URL (
-  if defined GO_SERVER (
-    set GO_SERVER_URL=https://%GO_SERVER%:8154/go
-  ) else (
-    set GO_SERVER_URL=https://127.0.0.1:8154/go
-  )
-)
+if not defined GO_SERVER_URL set GO_SERVER_URL=https://127.0.0.1:8154/go
 
 if not defined GO_AGENT_SYSTEM_PROPERTIES set GO_AGENT_SYSTEM_PROPERTIES=
 
-if defined JVM_DEBUG (
-  set JVM_DEBUG=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5006
-) else (
-  set JVM_DEBUG=
-)
-
-if defined GC_LOG (
-  set GC_LOG=-verbose:gc -Xloggc:go-agent-gc.log -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -XX:+PrintGCDetails -XX:+PrintGC
-) else (
-  set GC_LOG=
-)
-
-set AGENT_STARTUP_ARGS=-Xms%AGENT_MEM% -Xmx%AGENT_MAX_MEM% %JVM_DEBUG% %GC_LOG% %GO_AGENT_SYSTEM_PROPERTIES% -Dcruise.console.publish.interval=10
+set AGENT_STARTUP_ARGS=-Xms%AGENT_MEM% -Xmx%AGENT_MAX_MEM% %GO_AGENT_SYSTEM_PROPERTIES% -Dcruise.console.publish.interval=10
 
 set JAVA_CMD=%GO_AGENT_JAVA_HOME%\bin\java.exe
 set JAVA_HOME=%GO_AGENT_JAVA_HOME%

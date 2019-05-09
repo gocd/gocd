bcb057b716b4364b18be2e2fe483b6661a9cbcd3
Marques Lee <marques.lee@thoughtworks.com>
2019-05-08 17:30:54 -0700

    Forcefully run the new pipeline on save

--- server/webapp/WEB-INF/rails/webpack/views/pages/pipelines/actions.tsx
+++ server/webapp/WEB-INF/rails/webpack/views/pages/pipelines/actions.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import * as Routes from "gen/ts-routes";
 import {MithrilViewComponent} from "jsx/mithril-component";
 import * as m from "mithril";
@@ -7,9 +23,28 @@ import * as css from "./components.scss";
 
 interface Attrs {
   pipelineConfig: PipelineConfig;
+  loc?: LocationHandler;
+}
+
+export interface LocationHandler {
+  go: (url: string) => void;
+}
+
+class WindowLocation implements LocationHandler {
+  go(url: string) {
+    window.location.href = url;
+  }
 }
 
 export class PipelineActions extends MithrilViewComponent<Attrs> {
+  private location: LocationHandler = new WindowLocation();
+
+  oninit(vnode: m.Vnode<Attrs, {}>) {
+    if (vnode.attrs.loc) {
+      this.location = vnode.attrs.loc;
+    }
+  }
+
   view(vnode: m.Vnode<Attrs>): m.Children | void | null {
     return (
       <footer class={css.actions}>
@@ -25,7 +60,7 @@ export class PipelineActions extends MithrilViewComponent<Attrs> {
 
   onCancel(event: Event): void {
     event.stopPropagation();
-    window.location.href = "/go/pipelines";
+    this.location.go("/go/pipelines");
   }
 
   onSave(shouldPause: boolean, pipelineConfig: PipelineConfig, event: Event): void {
@@ -37,10 +72,12 @@ export class PipelineActions extends MithrilViewComponent<Attrs> {
         response.getOrThrow();
         if (shouldPause) {
           pipelineConfig.pause().then(() => {
-            window.location.href = Routes.pipelineEditPath("pipelines", pipelineConfig.name(), "general");
+            this.location.go(Routes.pipelineEditPath("pipelines", pipelineConfig.name(), "general"));
           });
         } else  {
-          window.location.href = `/go/pipelines?new_pipeline_name=${pipelineConfig.name()}`;
+          pipelineConfig.run().then(() => {
+            this.location.go(`/go/pipelines?new_pipeline_name=${pipelineConfig.name()}`);
+          });
         }
       }).catch((reason) => {
         this.setError(reason);

b65aecf6cb0fdd5a55e65532239823e395819f91
Kritika Singh <kritika.singh1994@gmail.com>
2019-05-30 15:53:07 +0530

    Added specs for environment crud and model

---
+++ server/webapp/WEB-INF/rails/webpack/models/environments/spec/environment_crud_spec.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {EnvironmentCRUD} from "models/environments/environment_crud";
+import {TestData} from "models/environments/spec/test_data";
+import {EnvironmentsJSON} from "models/environments/types";
+
+describe("EnvironmentCRUD", () => {
+  beforeEach(() => jasmine.Ajax.install());
+  afterEach(() => jasmine.Ajax.uninstall());
+
+  it("should make get request", () => {
+    jasmine.Ajax.stubRequest("/go/api/admin/environments").andReturn(listEnvironmentResponse());
+
+    EnvironmentCRUD.all();
+
+    const request = jasmine.Ajax.requests.mostRecent();
+    expect(request.url).toEqual("/go/api/admin/environments");
+    expect(request.method).toEqual("GET");
+    expect(request.data()).toEqual(toJSON({} as EnvironmentsJSON));
+    expect(request.requestHeaders.Accept).toEqual("application/vnd.go.cd.v2+json");
+  });
+});
+
+function toJSON(object: any) {
+  return JSON.parse(JSON.stringify(object));
+}
+
+function listEnvironmentResponse() {
+  return {
+    status: 200,
+    responseHeaders: {
+      "Content-Type": "application/vnd.go.cd.v2+json; charset=utf-8"
+    },
+    responseText: JSON.stringify(TestData.environmentList(TestData.newEnvironment()))
+  };
+}
---
+++ server/webapp/WEB-INF/rails/webpack/models/environments/spec/types_spec.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {TestData} from "models/environments/spec/test_data";
+import {Environments} from "models/environments/types";
+
+describe("EnvironmentModel", () => {
+
+  it("should deserialize json to Environments", () => {
+    const env1             = TestData.newEnvironment("env1", [], ["pipeline1"]);
+    const env2             = TestData.newEnvironment("env2", ["agent1uuid"]);
+    const environmentsJSON = TestData.environmentList(env1, env2);
+    const environments     = Environments.fromJSON(environmentsJSON);
+
+    expect(environments.length).toEqual(2);
+
+    const environment1 = environments[0];
+
+    expect(environment1.name()).toBe(env1.name);
+    expect(environment1.agents().length).toBe(0);
+    expect(environment1.pipelines().length).toBe(1);
+    expect(environment1.pipelines()[0].name()).toBe("pipeline1");
+    expect(environment1.environmentVariables().length).toBe(0);
+
+    const environment2 = environments[1];
+
+    expect(environment2.name()).toBe(env2.name);
+    expect(environment2.agents().length).toBe(1);
+    expect(environment2.agents()[0].uuid()).toBe("agent1uuid");
+    expect(environment2.pipelines().length).toBe(0);
+    expect(environment2.environmentVariables().length).toBe(0);
+
+  });
+});

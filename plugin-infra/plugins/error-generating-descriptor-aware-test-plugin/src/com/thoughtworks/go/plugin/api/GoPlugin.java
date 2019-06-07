1f287c5b17a3ac96bdff52758f7635971253d434
Aravind SV <mail@arvindsv.com>
2019-06-07 16:37:35 -0400

    Fix mistake in test
    Was using an old activator and was succeeding for the wrong reasons. There was some code in the activator which
    registered an extension type called CANNOT_FIND_EXTENSION_TYPE, if the extension type could not be found. This code
    was changed, but the old activator continued to be used. So, the test passed. But, with new activator code, it failed
    to register the extension.

--- plugin-infra/plugins/error-generating-descriptor-aware-test-plugin/src/com/thoughtworks/go/plugin/api/GoPlugin.java
+++ plugin-infra/plugins/error-generating-descriptor-aware-test-plugin/src/com/thoughtworks/go/plugin/api/GoPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 ThoughtWorks, Inc.
+ * Copyright 2019 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,4 +17,5 @@
 package com.thoughtworks.go.plugin.api;
 
 public interface GoPlugin {
+    GoPluginIdentifier pluginIdentifier();
 }
---
+++ plugin-infra/plugins/error-generating-descriptor-aware-test-plugin/src/com/thoughtworks/go/plugin/api/GoPluginIdentifier.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.util.List;
+
+public class GoPluginIdentifier {
+
+    private String extension;
+
+    private List<String> supportedExtensionVersions;
+
+    /**
+     * Constructs GoPluginIdentifier with extension and list of supported extension versions
+     * @param extension Name of extension
+     * @param supportedExtensionVersions List of supported extension versions
+     */
+    public GoPluginIdentifier(String extension, List<String> supportedExtensionVersions) {
+        this.extension = extension;
+        this.supportedExtensionVersions = supportedExtensionVersions;
+    }
+
+    /**
+     * Gets extension name
+     * @return Extension name
+     */
+    public String getExtension() {
+        return extension;
+    }
+
+    /**
+     * Gets list of supported extension versions
+     * @return List of supported extension versions
+     */
+    public List<String> getSupportedExtensionVersions() {
+        return supportedExtensionVersions;
+    }
+}

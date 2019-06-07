5f487eace35595a7e537d56620402d832e002268
Aravind SV <mail@arvindsv.com>
2019-06-07 12:37:32 -0400

    Handle invalidation properly
    * If a plugin is marked invalid, all sibling plugins in the bundle are marked invalid.
    * If a bundle is marked invalid, all plugins in the bundle are marked invalid.

---
+++ plugin-infra/go-plugin-infra/src/test/java/com/thoughtworks/go/plugin/infra/plugininfo/GoPluginBundleDescriptorTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.thoughtworks.go.plugin.infra.plugininfo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static java.util.Collections.singletonList;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+class GoPluginBundleDescriptorTest {
+    @Test
+    void bundleIsConsideredInvalidIfAtleastOnePluginInItIsInvalid() {
+        final GoPluginDescriptor pluginDescriptor1 = GoPluginDescriptor.usingId("plugin.1", null, null, false);
+        final GoPluginDescriptor pluginDescriptor2 = GoPluginDescriptor.usingId("plugin.2", null, null, false);
+
+        final GoPluginBundleDescriptor bundleDescriptor = new GoPluginBundleDescriptor(pluginDescriptor1, pluginDescriptor2);
+
+        assertThat(bundleDescriptor.isInvalid(), is(false));
+
+        pluginDescriptor1.markAsInvalid(singletonList("Ouch!"), null);
+
+        assertThat(bundleDescriptor.isInvalid(), is(true));
+    }
+
+    @Test
+    void shouldAggregateStatusMessagesFromAcrossPluginsInABundle() {
+        final GoPluginDescriptor pluginDescriptor1 = GoPluginDescriptor.usingId("plugin.1", null, null, false);
+        final GoPluginDescriptor pluginDescriptor2 = GoPluginDescriptor.usingId("plugin.2", null, null, false);
+
+        final GoPluginBundleDescriptor bundleDescriptor = new GoPluginBundleDescriptor(pluginDescriptor1, pluginDescriptor2);
+
+        pluginDescriptor1.markAsInvalid(singletonList("Ouch!"), null);
+        pluginDescriptor2.markAsInvalid(singletonList("Second one is bad too!"), null);
+
+        /* Overwrites previous message. */
+        assertThat(bundleDescriptor.getMessages(), is(Arrays.asList("Second one is bad too!", "Second one is bad too!")));
+    }
+}

79063b7d31e68849a7c5f0c3942d233a7b36d521
Bhupendra <bdpiparva@gmail.com>
2019-06-21 12:21:53 +0530

    Revert "Updated rules validation on MDU (#6555)"
    This reverts commit 19897e67ffe1757e7a1368a29bd1af927aeb90d9.

--- server/src/main/java/com/thoughtworks/go/server/service/RulesService.java
+++ server/src/main/java/com/thoughtworks/go/server/service/RulesService.java
@@ -20,16 +20,12 @@ import com.thoughtworks.go.config.*;
 import com.thoughtworks.go.config.materials.ScmMaterial;
 import com.thoughtworks.go.domain.JobIdentifier;
 import com.thoughtworks.go.remote.work.BuildAssignment;
-import com.thoughtworks.go.server.exceptions.RulesViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.thoughtworks.go.server.exceptions.RulesViolationException.throwCannotRefer;
 import static com.thoughtworks.go.server.exceptions.RulesViolationException.throwSecretConfigNotFound;
@@ -48,42 +44,13 @@ public class RulesService {
     public boolean validateSecretConfigReferences(ScmMaterial scmMaterial) {
         List<CaseInsensitiveString> pipelines = goConfigService.pipelinesWithMaterial(scmMaterial.getFingerprint());
         SecretParams secretParams = scmMaterial.getSecretParams();
+        pipelines.forEach(pipeline -> {
+            PipelineConfigs group = goConfigService.findGroupByPipeline(pipeline);
+            String errorMessagePrefix = format("Material with url: '%s' in Pipeline: '%s' and Pipeline Group:", scmMaterial.getUriForDisplay(), pipeline);
+            validateSecretConfigReferences(secretParams, group.getClass(), group.getGroup(), errorMessagePrefix);
+        });
 
-        List<String> missingSecretConfigs = secretParams.stream()
-                .filter(secretParam -> goConfigService.cruiseConfig().getSecretConfigs().find(secretParam.getSecretConfigId()) == null)
-                .map(SecretParam::getSecretConfigId)
-                .collect(Collectors.toList());
-
-        if (!missingSecretConfigs.isEmpty()) {
-            throwSecretConfigNotFound("ScmMaterial", scmMaterial.getUriForDisplay(), String.join(", ", missingSecretConfigs));
-        }
-
-        HashMap<CaseInsensitiveString, List<String>> pipelinesWithViolatedSecretConfigIds = new HashMap<>();
-        secretParams.stream()
-                .map(secretParam -> goConfigService.cruiseConfig().getSecretConfigs().find(secretParam.getSecretConfigId()))
-                .forEach(secretConfig -> {
-                    pipelines.forEach(pipelineName -> {
-                        PipelineConfigs group = goConfigService.findGroupByPipeline(pipelineName);
-                        if (!secretConfig.canRefer(group.getClass(), group.getGroup())) {
-                            if (!pipelinesWithViolatedSecretConfigIds.containsKey(pipelineName)) {
-                                pipelinesWithViolatedSecretConfigIds.put(pipelineName, new ArrayList<>());
-                            }
-                            LOGGER.error("[Material Update] Failure", format("Pipeline '%s' does not have permission to refer to secrets using SecretConfig: '%s'", pipelineName.toString(), secretConfig.getId()));
-                            pipelinesWithViolatedSecretConfigIds.get(pipelineName).add(secretConfig.getId());
-
-                        }
-                    });
-                });
-
-        if (pipelinesWithViolatedSecretConfigIds.size() == pipelines.size()) {
-            String secretConfigId = secretParams.get(0).getSecretConfigId();
-            String errorMessage = format("Material with url: '%s' does not have permissions to refer to secret config with id: '%s'. The pipelines which use this material are: '%s'", scmMaterial.getUriForDisplay(), secretConfigId, pipelinesWithViolatedSecretConfigIds.keySet().toString());
-            LOGGER.error("[Material Update] Failure", errorMessage);
-            throw new RulesViolationException(errorMessage);
-        }
         return true;
-
-
     }
 
     public boolean validateSecretConfigReferences(EnvironmentConfig environmentConfig) {

35c1e9343016b35679ed520d96f72609e5b565cc
kierarad <kieraradman@gmail.com>
2019-07-25 13:48:27 -0700

    Create dropdown for new pipeline button (behind toggle new_pipeline_dropdown)

--- server/webapp/WEB-INF/rails/webpack/models/dashboard/dashboard_groups.js
+++ server/webapp/WEB-INF/rails/webpack/models/dashboard/dashboard_groups.js
@@ -108,7 +108,8 @@ class PipelineGroup extends Group {
     return {
       show: `${Routes.pipelineGroupsPath()}#group-${this.name}`,
       edit: Routes.pipelineGroupEditPath(this.name),
-      new: `${SparkRoutes.newCreatePipelinePath()}?group=${this.name}`
+      new: `${SparkRoutes.newCreatePipelinePath()}?group=${this.name}`,
+      asCode: `${SparkRoutes.createPipelineAsCodePath()}?group=${this.name}`
     };
   }
 }

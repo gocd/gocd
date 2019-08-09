gocd/gocd

commit d2e24644a741fcab8b6c84bf63ba948cf57e9808

Using data test id in spec

server/webapp/WEB-INF/rails/webpack/views/components/wizard/index.tsx
@@ -33,7 +33,8 @@ export abstract class Step {
 
   footer(wizard: Wizard): m.Children {
     return [
-      <Buttons.Cancel onclick={wizard.close.bind(wizard)}>Cancel</Buttons.Cancel>,
+      <Buttons.Cancel onclick={wizard.close.bind(wizard)}
+                      dataTestId="cancel">Cancel</Buttons.Cancel>,
       <Buttons.Primary dataTestId="previous" onclick={wizard.previous.bind(wizard)}
                        disabled={wizard.isFirstStep()}>Previous</Buttons.Primary>,
       <Buttons.Primary dataTestId="next" onclick={wizard.next.bind(wizard)}

server/webapp/WEB-INF/rails/webpack/views/components/wizard/spec/index_spec.tsx
@@ -18,7 +18,6 @@ import * as m from "mithril";
 import * as simulateEvent from "simulate-event";
 import * as Buttons from "views/components/buttons";
 import {Step, Wizard} from "views/components/wizard/index";
-import * as btnCss from "../../buttons/index.scss";
 import * as styles from "../index.scss";
 
 describe("WizardSpec", () => {
@@ -87,17 +86,17 @@ describe("WizardSpec", () => {
     it("should show cancel button", () => {
       wizard.render();
 
-      expect(findByClass(btnCss.btnCancel)).toBeInDOM();
-      expect(findByClass(btnCss.btnCancel)).toHaveText("Cancel");
+      expect(findByDataTestId("cancel")).toBeInDOM();
+      expect(findByDataTestId("cancel")).toHaveText("Cancel");
     });
 
     it("should close wizard on click", () => {
       wizard.render();
 
       expect(findByClass(styles.wizard)).toBeInDOM();
-      expect(findByClass(btnCss.btnCancel)).toBeInDOM();
+      expect(findByDataTestId("cancel")).toBeInDOM();
 
-      simulateEvent.simulate(findByClass(btnCss.btnCancel)[0], "click");
+      simulateEvent.simulate(findByDataTestId("cancel")[0], "click");
 
       expect(findByClass(styles.wizard)).not.toBeInDOM();
     });

commit ed4e32d5a60174d489cdcabaf3c4b778d11f49c3

Fixed jasmine test failure introduced in #6734

server/webapp/WEB-INF/rails/webpack/views/components/buttons/index.tsx
@@ -42,7 +42,6 @@ export interface Attrs extends OnClickHandler, RestyleAttrs<Styles> {
   disabled?: boolean;
   dropdown?: boolean;
   align?: Alignment;
-  dataTestId?: string;
 }
 
 abstract class Button extends RestyleViewComponent<Styles, Attrs> {
@@ -79,7 +78,6 @@ abstract class Button extends RestyleViewComponent<Styles, Attrs> {
 
     return (
       <button {...Button.onlyHtmlAttrs(vnode.attrs)}
-              data-test-id={vnode.attrs.dataTestId}
               class={classnames(
                 this.css.button,
                 {[this.css.btnSmall]: isSmall, [this.css.btnDropdown]: isDropdown},

server/webapp/WEB-INF/rails/webpack/views/components/wizard/index.tsx
@@ -34,10 +34,10 @@ export abstract class Step {
   footer(wizard: Wizard): m.Children {
     return [
       <Buttons.Cancel onclick={wizard.close.bind(wizard)}
-                      dataTestId="cancel">Cancel</Buttons.Cancel>,
-      <Buttons.Primary dataTestId="previous" onclick={wizard.previous.bind(wizard)}
+                      data-test-id="cancel">Cancel</Buttons.Cancel>,
+      <Buttons.Primary data-test-id="previous" onclick={wizard.previous.bind(wizard)}
                        disabled={wizard.isFirstStep()}>Previous</Buttons.Primary>,
-      <Buttons.Primary dataTestId="next" onclick={wizard.next.bind(wizard)}
+      <Buttons.Primary data-test-id="next" onclick={wizard.next.bind(wizard)}
                        disabled={wizard.isLastStep()}>Next</Buttons.Primary>
     ];
   }

server/webapp/WEB-INF/rails/webpack/views/components/wizard/spec/index_spec.tsx
@@ -150,7 +150,7 @@ describe("WizardSpec", () => {
 
   describe("Custom Footer", () => {
     it("should render wizard with custom footer", () => {
-      const customButton = <Buttons.Primary dataTestId="customButton">Custom Button</Buttons.Primary>;
+      const customButton = <Buttons.Primary data-test-id="customButton">Custom Button</Buttons.Primary>;
       wizard.addStep(new SampleStep("last Step", "this is last step", customButton));
 
       wizard.defaultStepIndex(4).render();
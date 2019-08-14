1049153e8e97a6726a2c7f62c5ef86bb5f06e406
Sneha Patil <snehapatil1391@gmail.com>
2019-08-14 13:20:25 +0530

    Rewrite internal environments APIs in Spark (#6657).

---
+++ api/api-internal-environments-v1/src/main/java/com/thoughtworks/go/apiv1/internalenvironments/InternalEnvironmentsControllerV1.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.thoughtworks.go.apiv1.internalenvironments;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.go.api.ApiController;
+import com.thoughtworks.go.api.ApiVersion;
+import com.thoughtworks.go.api.spring.ApiAuthenticationHelper;
+import com.thoughtworks.go.config.CaseInsensitiveString;
+import com.thoughtworks.go.server.service.EnvironmentConfigService;
+import com.thoughtworks.go.spark.Routes;
+import com.thoughtworks.go.spark.spring.SparkSpringController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import spark.Request;
+import spark.Response;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static spark.Spark.*;
+
+@Component
+public class InternalEnvironmentsControllerV1 extends ApiController implements SparkSpringController {
+
+    private final ApiAuthenticationHelper apiAuthenticationHelper;
+    private final EnvironmentConfigService environmentConfigService;
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    @Autowired
+    public InternalEnvironmentsControllerV1(ApiAuthenticationHelper apiAuthenticationHelper, EnvironmentConfigService environmentConfigService) {
+        super(ApiVersion.v1);
+        this.apiAuthenticationHelper = apiAuthenticationHelper;
+        this.environmentConfigService = environmentConfigService;
+    }
+
+    @Override
+    public String controllerBasePath() {
+        return Routes.InternalEnvironments.BASE;
+    }
+
+    @Override
+    public void setupRoutes() {
+        path(controllerBasePath(), () -> {
+            before("", mimeType, this::setContentType);
+            before("/*", mimeType, this::setContentType);
+
+            before("", mimeType, this.apiAuthenticationHelper::checkAdminUserAnd403);
+
+            get("", mimeType, this::index);
+        });
+    }
+
+    public String index(Request request, Response response) throws IOException {
+        List<String> environmentNames = environmentConfigService.environmentNames()
+                .stream().map(CaseInsensitiveString::toString).collect(Collectors.toList());
+        return GSON.toJson(environmentNames);
+    }
+}

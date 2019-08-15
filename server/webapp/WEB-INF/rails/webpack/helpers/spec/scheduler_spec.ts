70ca34c63f2e5a42be7fc88f61d0e6fed963d60e
Marques Lee <marques.lee@thoughtworks.com>
2019-08-15 16:32:51 -0700

    Add tests for Scheduler, Anchor, and AnchorVM

---
+++ server/webapp/WEB-INF/rails/webpack/helpers/spec/scheduler_spec.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Scheduler} from "../scheduler";
+
+describe("Scheduler", () => {
+  // this is really the only thing that can be tested without some awkward hack; testing
+  // the exec delay until onload fires would be difficult
+  it("immediately runs a task once the page has loaded", (done) => {
+    const a = jasmine.createSpy("first()"),
+          b = jasmine.createSpy("second()"),
+          c = jasmine.createSpy("third()");
+
+    const scheduler = new Scheduler();
+
+    scheduler.schedule(a);
+    scheduler.schedule(b);
+    scheduler.schedule(c);
+
+    scheduler.schedule(() => {
+      expect(a).toHaveBeenCalled();
+      expect(b).toHaveBeenCalled();
+      expect(c).toHaveBeenCalled();
+      done();
+    });
+  });
+});

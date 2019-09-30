da4102920b092254bece10be55dad1aab8a20bcb
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2019-09-30 14:41:28 +0530

    Fix build

--- jar-class-loader/src/main/java/com/thoughtworks/gocd/AssertJava.java
+++ jar-class-loader/src/main/java/com/thoughtworks/gocd/AssertJava.java
@@ -23,7 +23,7 @@ public class AssertJava {
     private static TreeSet<JavaVersion> SUPPORTED_VERSIONS = new TreeSet<>(Arrays.asList(
             JavaVersion.VERSION_11,
             JavaVersion.VERSION_12,
-            JavaVersion.VERSION_13,
+            JavaVersion.VERSION_13
     ));
 
     public static void assertVMVersion() {

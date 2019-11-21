256c032cf4c5c8b10f48705f9afbc9245506607e
Akshay Dewan <akshayde@thoughtworks.com>
2019-11-21 11:30:16 +0530

    Add ajaxOperation property to buttons
    For buttons making ajax calls, use the ajaxOperation attribute instead of onclick.
    This will display a spinner on the button while the operation is in progress,
    and prevent duplicate requests when the button is clicked while in progress.

---
+++ server/src/main/webapp/WEB-INF/rails/webpack/views/components/buttons/spec/index_spec.tsx
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {ApiResult} from "helpers/api_request_builder";
+import m from "mithril";
+import {Primary} from "views/components/buttons/index";
+import {TestHelper} from "views/pages/spec/test_helper";
+import * as buttonStyles from "../index.scss";
+
+describe("Button", () => {
+
+  const helper = new TestHelper();
+
+  afterEach(helper.unmount.bind(helper));
+
+  describe("Ajax Operation", () => {
+
+    it("should show a spinner icon when ajax request is in progress", (done) => {
+      const promise = new Promise<ApiResult<any>>((resolve) => {
+        resolve();
+      }).then(() => {
+        expect(helper.byTestId("ajax-button")).toHaveClass(buttonStyles.iconSpinner);
+        done();
+      });
+      helper.mount(() => <Primary dataTestId="ajax-button" ajaxOperation={() => promise}>Save</Primary>);
+      expect(helper.byTestId("ajax-button")).not.toHaveClass(buttonStyles.iconSpinner);
+      helper.clickByTestId("ajax-button");
+    });
+
+    it("should ignore clicks when ajax operation is in progress", (done) => {
+      const spy                           = jasmine.createSpy();
+      const promises: Array<Promise<any>> = [];
+      const simulateRemoteCall            = () => {
+        const promise = new Promise<any>((resolve) => {
+          setTimeout(() => {
+            spy();
+            resolve();
+          });
+        });
+        promises.push(promise);
+        return promise;
+      };
+
+      helper.mount(() => <Primary dataTestId="ajax-button"
+                                  ajaxOperation={simulateRemoteCall}>Save</Primary>);
+      helper.clickByTestId("ajax-button");
+      helper.clickByTestId("ajax-button");
+      Promise.all(promises).finally(() => {
+        expect(spy).toHaveBeenCalledTimes(1);
+        done();
+      });
+    });
+  });
+});

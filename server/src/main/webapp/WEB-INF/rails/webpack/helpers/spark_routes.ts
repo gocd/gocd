6c05e298ef338a11cfd1b3e546b25240fef35faf
Akshay Dewan <akshayde@thoughtworks.com>
2019-12-05 15:08:41 +0530

    Fix bad rebase

--- server/src/main/webapp/WEB-INF/rails/webpack/helpers/spark_routes.ts
+++ server/src/main/webapp/WEB-INF/rails/webpack/helpers/spark_routes.ts
@@ -447,7 +447,6 @@ export class SparkRoutes {
     return `/go/admin/templates/${templateName}/permissions`;
   }
 
-<<<<<<< HEAD
   static getEnvironmentPathOnSPA(environmentName: string) {
     return `/go/admin/environments/#!${environmentName}`;
   }

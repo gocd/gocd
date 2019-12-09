476c89a48c80dccf51e98c5e50593265c69bde16
Akshay Dewan <akshayde@thoughtworks.com>
2019-12-09 12:40:01 +0530

    Fix view specs

--- server/src/main/webapp/WEB-INF/rails/spec/views/admin/tasks/plugin/edit_html_spec.rb
+++ server/src/main/webapp/WEB-INF/rails/spec/views/admin/tasks/plugin/edit_html_spec.rb
@@ -24,6 +24,8 @@ describe "/admin/tasks/plugin/edit.html.erb" do
   before :each do
     assign(:cruise_config, config = BasicCruiseConfig.new)
     set(config, "md5", "abcd1234")
+    @pipeline_config = PipelineConfigMother.createPipelineConfig("pipeline-name", "foo", ["build-1"].to_java(java.lang.String))
+    assign(:pipeline, @pipeline_config)
 
     allow(view).to receive(:admin_task_update_path).and_return("task_update_path")
     assign(:task, @task = simple_exec_task)

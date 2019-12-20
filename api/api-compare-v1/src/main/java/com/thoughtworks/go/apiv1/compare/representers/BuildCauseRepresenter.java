0618053230e773adb87dfe4f6f778a535896bbc8
Kritika Singh <kritika.singh1994@gmail.com>
2019-12-20 10:48:45 +0530

    Adding representers

---
+++ api/api-compare-v1/src/main/java/com/thoughtworks/go/apiv1/compare/representers/BuildCauseRepresenter.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.thoughtworks.go.apiv1.compare.representers;
+
+import com.thoughtworks.go.api.base.OutputWriter;
+import com.thoughtworks.go.domain.buildcause.BuildCause;
+
+public class BuildCauseRepresenter {
+    public static void toJSON(OutputWriter outputWriter, BuildCause buildCause) {
+        outputWriter
+                .add("trigger_message", buildCause.getBuildCauseMessage())
+                .add("approver", buildCause.getApprover())
+                .add("trigger_forced", buildCause.isForced())
+                .addChildList("material_revisions", listWriter -> buildCause.getMaterialRevisions()
+                        .forEach(revision -> listWriter.addChild(revisionWriter -> MaterialRevisionRepresenter.toJSON(revisionWriter, revision))));
+    }
+}

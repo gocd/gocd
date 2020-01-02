fa1c20b7450a60af1434b0dcb4f473f6fa14ff18
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2020-01-02 10:58:58 +0530

    Update license headers year to 2020

--- api/api-internal-environments-v1/src/test/groovy/com/thoughtworks/go/apiv1/internalenvironments/representers/EntityConfigOriginRepresenterTest.groovy
+++ api/api-internal-environments-v1/src/test/groovy/com/thoughtworks/go/apiv1/internalenvironments/representers/EntityConfigOriginRepresenterTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 ThoughtWorks, Inc.
+ * Copyright 2020 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.thoughtworks.go.apiv1.internalenvironments.representers
 
 import com.thoughtworks.go.config.remote.ConfigRepoConfig

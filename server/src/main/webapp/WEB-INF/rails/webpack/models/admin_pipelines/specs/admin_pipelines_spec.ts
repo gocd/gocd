b17091e82d83711e985ce077017aedbee0bc1d0a
Kritika Singh <kritika.singh1994@gmail.com>
2020-01-10 12:55:12 +0530

    Update UI for admin permissions on pipeline grp edit modal
     - if a less restrivtive permission is set, the more restrictive permissions are checked and disabled. e.g. if an entity is an admin, then it will have view and operate permissions as well
     - the updated grp payload send to update API call will set the users/roles to uppermost permissions. e.g. if an entity is an admin, the same won't be present in view or operate.

--- server/src/main/webapp/WEB-INF/rails/webpack/models/admin_pipelines/specs/admin_pipelines_spec.ts
+++ server/src/main/webapp/WEB-INF/rails/webpack/models/admin_pipelines/specs/admin_pipelines_spec.ts
@@ -149,12 +149,12 @@ describe('PipelineGroupViewModel', () => {
 
       const updatedPipelineGroup = pipelineGroupViewModel.getUpdatedPipelineGroup();
 
-      expect(updatedPipelineGroup.authorization().view().users()).toEqual(["user1", "admin"]);
-      expect(updatedPipelineGroup.authorization().operate().users()).toEqual(["user1", "superUser", "admin"]);
+      expect(updatedPipelineGroup.authorization().view().users()).toEqual([]);
+      expect(updatedPipelineGroup.authorization().operate().users()).toEqual(["user1", "superUser"]);
       expect(updatedPipelineGroup.authorization().admin().users()).toEqual(["admin"]);
 
-      expect(updatedPipelineGroup.authorization().view().roles()).toEqual(["role1", "admin"]);
-      expect(updatedPipelineGroup.authorization().operate().roles()).toEqual(["role2", "admin"]);
+      expect(updatedPipelineGroup.authorization().view().roles()).toEqual(["role1"]);
+      expect(updatedPipelineGroup.authorization().operate().roles()).toEqual(["role2"]);
       expect(updatedPipelineGroup.authorization().admin().roles()).toEqual(["admin"]);
       expect(updatedPipelineGroup.name()).toEqual(pipelineGroup.name());
     });

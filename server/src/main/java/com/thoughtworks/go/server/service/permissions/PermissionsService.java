28b0890689f65ea20f3c97f61401f9b8c42db915
GaneshSPatil <ganeshpl@thoughtworks.com>
2020-02-07 12:25:58 +0530

    Add support to fetch permissions for specified entities

---
+++ server/src/main/java/com/thoughtworks/go/server/service/permissions/PermissionsService.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thoughtworks.go.server.service.permissions;
+
+import com.thoughtworks.go.server.newsecurity.utils.SessionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class PermissionsService {
+    private List<PermissionProvider> providers = new ArrayList<>();
+
+    @Autowired
+    public PermissionsService(PermissionProvider... availableProviders) {
+        providers.addAll(Arrays.asList(availableProviders));
+    }
+
+    public List<String> allEntitiesSupportsPermission() {
+        return providers.stream().map(PermissionProvider::name).collect(Collectors.toList());
+    }
+
+    public Map<String, Object> getPermissions(List<String> requestedTypes) {
+        Map<String, Object> json = new LinkedHashMap<>();
+
+        providers.stream().filter(p -> requestedTypes.contains(p.name()))
+                .forEach(p -> json.put(p.name(), p.permissions(SessionUtils.currentUsername())));
+
+        return json;
+    }
+}
---
+++ server/src/test-fast/java/com/thoughtworks/go/server/service/permissions/entity/ElasticAgentProfilePermissionTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.thoughtworks.go.server.service.permissions.entity;
+
+import com.thoughtworks.go.config.BasicCruiseConfig;
+import com.thoughtworks.go.config.elastic.ClusterProfile;
+import com.thoughtworks.go.config.elastic.ElasticProfile;
+import com.thoughtworks.go.server.domain.Username;
+import com.thoughtworks.go.server.newsecurity.utils.SessionUtils;
+import com.thoughtworks.go.server.security.GoAuthority;
+import com.thoughtworks.go.server.security.userdetail.GoUserPrinciple;
+import com.thoughtworks.go.server.service.GoConfigService;
+import com.thoughtworks.go.server.service.SecurityService;
+import com.thoughtworks.go.server.service.permissions.PermissionProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.*;
+
+import static com.thoughtworks.go.config.policy.SupportedAction.ADMINISTER;
+import static com.thoughtworks.go.config.policy.SupportedAction.VIEW;
+import static com.thoughtworks.go.config.policy.SupportedEntity.ELASTIC_AGENT_PROFILE;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class ElasticAgentProfilePermissionTest {
+    @Mock
+    private GoConfigService goConfigService;
+    @Mock
+    private SecurityService securityService;
+
+    private Username username;
+    private PermissionProvider permission;
+    private BasicCruiseConfig cruiseConfig;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+
+        cruiseConfig = new BasicCruiseConfig();
+        username = new Username("Bob" + UUID.randomUUID());
+        permission = new ElasticAgentProfilePermission(goConfigService, securityService);
+
+        when(goConfigService.getMergedConfigForEditing()).thenReturn(cruiseConfig);
+        SessionUtils.setCurrentUser(new GoUserPrinciple(username.getUsername().toString(), username.getUsername().toString(), GoAuthority.ROLE_ANONYMOUS.asAuthority()));
+    }
+
+    @Test
+    void shouldGetNameOfThePermission() {
+        assertThat(permission.name()).isEqualTo("elastic_agent_profile");
+    }
+
+    @Test
+    void shouldReturnEmptyListWhenUserHasNoPermissionsOnElasticAgentProfiles() {
+        cruiseConfig.getElasticConfig().getClusterProfiles().add(new ClusterProfile("dev_cluster", "ecs"));
+        cruiseConfig.getElasticConfig().getClusterProfiles().add(new ClusterProfile("prod_cluster", "ecs"));
+
+        cruiseConfig.getElasticConfig().getProfiles().add(new ElasticProfile("build-agent", "dev_cluster"));
+        cruiseConfig.getElasticConfig().getProfiles().add(new ElasticProfile("deploy-agent", "prod_cluster"));
+
+        Map<String, Object> permissions = permission.permissions(username);
+
+        Map<String, Object> elasticAgentProfile = new LinkedHashMap<>();
+        elasticAgentProfile.put("view", Collections.emptyList());
+        elasticAgentProfile.put("administer", Collections.emptyList());
+
+        assertThat(permissions).isEqualTo(elasticAgentProfile);
+    }
+
+    @Test
+    void shouldReturnUserPermissibleElasticAgentProfiles() {
+        cruiseConfig.getElasticConfig().getClusterProfiles().add(new ClusterProfile("dev_cluster", "ecs"));
+        cruiseConfig.getElasticConfig().getClusterProfiles().add(new ClusterProfile("prod_cluster", "ecs"));
+
+        cruiseConfig.getElasticConfig().getProfiles().add(new ElasticProfile("build-agent", "dev_cluster"));
+        cruiseConfig.getElasticConfig().getProfiles().add(new ElasticProfile("deploy-agent", "prod_cluster"));
+
+        when(securityService.doesUserHasPermissions(SessionUtils.currentUsername(), VIEW, ELASTIC_AGENT_PROFILE, "build-agent", "dev_cluster")).thenReturn(true);
+        when(securityService.doesUserHasPermissions(SessionUtils.currentUsername(), VIEW, ELASTIC_AGENT_PROFILE, "deploy-agent", "prod_cluster")).thenReturn(true);
+
+        when(securityService.doesUserHasPermissions(SessionUtils.currentUsername(), ADMINISTER, ELASTIC_AGENT_PROFILE, "build-agent", "dev_cluster")).thenReturn(true);
+        when(securityService.doesUserHasPermissions(SessionUtils.currentUsername(), ADMINISTER, ELASTIC_AGENT_PROFILE, "deploy-agent", "prod_cluster")).thenReturn(false);
+
+        Map<String, Object> permissions = permission.permissions(username);
+
+        Map<String, Object> elasticAgentProfile = new LinkedHashMap<>();
+        elasticAgentProfile.put("view", Arrays.asList("build-agent", "deploy-agent"));
+        elasticAgentProfile.put("administer", Arrays.asList("build-agent"));
+
+        assertThat(permissions).isEqualTo(elasticAgentProfile);
+    }
+}
+

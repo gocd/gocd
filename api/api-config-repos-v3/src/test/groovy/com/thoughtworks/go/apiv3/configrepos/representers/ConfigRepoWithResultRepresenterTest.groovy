b69e7dabd814c3e896adb33e944b1e0c7a171b9b
GaneshSPatil <ganeshpl@thoughtworks.com>
2020-02-10 13:42:23 +0530

    Add missing config repos API v3 representer tests

---
+++ api/api-config-repos-v3/src/test/groovy/com/thoughtworks/go/apiv3/configrepos/representers/ConfigRepoWithResultRepresenterTest.groovy
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thoughtworks.go. apiv3.configrepos.representers
+
+import com.thoughtworks.go. apiv3.configrepos.ConfigRepoWithResult
+import com.thoughtworks.go.config.PartialConfigParseResult
+import com.thoughtworks.go.config.materials.mercurial.HgMaterialConfig
+import static com.thoughtworks.go.helper.MaterialConfigsMother.hg
+import com.thoughtworks.go.config.remote.ConfigRepoConfig
+import com.thoughtworks.go.domain.config.Configuration
+import com.thoughtworks.go.domain.materials.Modification
+import com.thoughtworks.go.helper.ModificationsMother
+import com.thoughtworks.go.spark.Routes
+import org.junit.jupiter.api.Test
+
+import static com.thoughtworks.go.api.base.JsonOutputWriter.jsonDate
+import static com.thoughtworks.go.api.base.JsonUtils.toObjectString
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson
+
+class ConfigRepoWithResultRepresenterTest {
+  private static final String TEST_PLUGIN_ID = "test.configrepo.plugin"
+  private static final String TEST_REPO_URL = "https://fakeurl.com"
+
+  @Test
+  void toJSON() {
+    String id = "foo"
+    ConfigRepoWithResult result = repo(id)
+
+    String json = toObjectString({ w ->
+      ConfigRepoWithResultRepresenter.toJSON(w, result)
+    })
+
+    String self = "http://test.host/go${Routes.ConfigRepos.id(id)}"
+    String find = "http://test.host/go${Routes.ConfigRepos.find()}"
+
+    assertThatJson(json).isEqualTo([
+      _links                     : [
+        self: [href: self],
+        doc : [href: Routes.ConfigRepos.DOC],
+        find: [href: find],
+      ],
+
+      id                         : id,
+      plugin_id                  : TEST_PLUGIN_ID,
+      material                   : [
+        type      : "hg",
+        attributes: [
+          name       : null,
+          url        : TEST_REPO_URL,
+          auto_update: true
+        ]
+      ],
+      configuration              : [
+        [key: "foo", value: "bar"],
+        [key: "baz", value: "quu"]
+      ],
+      material_update_in_progress: false,
+      parse_info                 : [
+        error                     : "Boom!",
+        good_modification         : null,
+        latest_parsed_modification: [
+          "username"     : "lgao",
+          "email_address": "foo@bar.com",
+          "revision"     : "foo-123",
+          "comment"      : "Fixing the not checked in files",
+          "modified_time": jsonDate(result.result().latestParsedModification.modifiedTime)
+        ]
+      ]
+    ])
+  }
+
+  static ConfigRepoWithResult repo(String id) {
+    Modification modification = ModificationsMother.oneModifiedFile("${id}-123")
+    Exception exception = new Exception("Boom!")
+
+    PartialConfigParseResult expectedParseResult = PartialConfigParseResult.parseFailed(modification, exception)
+
+    Configuration c = new Configuration()
+    c.addNewConfigurationWithValue("foo", "bar", false)
+    c.addNewConfigurationWithValue("baz", "quu", false)
+
+    HgMaterialConfig materialConfig = hg(TEST_REPO_URL, "")
+    ConfigRepoConfig repo = ConfigRepoConfig.createConfigRepoConfig(materialConfig, TEST_PLUGIN_ID, id)
+    repo.setConfiguration(c)
+
+    return new ConfigRepoWithResult(repo, expectedParseResult, false)
+  }
+}
---
+++ api/api-config-repos-v3/src/test/groovy/com/thoughtworks/go/apiv3/configrepos/representers/MaterialsRepresenterTest.groovy
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thoughtworks.go.apiv3.configrepos.representers
+
+import com.thoughtworks.go.api.base.OutputWriter
+import com.thoughtworks.go.api.representers.JsonReader
+import com.thoughtworks.go.api.util.GsonTransformer
+import com.thoughtworks.go.config.exceptions.UnprocessableEntityException
+import com.thoughtworks.go.config.materials.PackageMaterialConfig
+import com.thoughtworks.go.domain.materials.MaterialConfig
+import org.junit.jupiter.api.Test
+
+import static com.thoughtworks.go.api.base.JsonUtils.toObjectString
+import static com.thoughtworks.go.helper.MaterialConfigsMother.git
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson
+import static org.junit.jupiter.api.Assertions.assertEquals
+import static org.junit.jupiter.api.Assertions.assertThrows
+import static org.mockito.Mockito.mock
+
+class MaterialsRepresenterTest {
+  private static final String REPO_URL = "https://guthib.com/chewbacca"
+  private static final String BRANCH = "wookie"
+
+  @Test
+  void toJSON() {
+    MaterialConfig config = git(REPO_URL, BRANCH)
+    String json = toObjectString({ w -> MaterialsRepresenter.toJSON(w, config) })
+
+    assertThatJson(json).isEqualTo([
+      type      : "git",
+      attributes: [
+        name       : null,
+        url        : REPO_URL,
+        branch     : BRANCH,
+        auto_update: true
+      ]
+    ])
+  }
+
+  @Test
+  void 'toJSON() fails on unsupported material type'() {
+    MaterialConfig config = new PackageMaterialConfig("foobar");
+    Throwable error = assertThrows(IllegalArgumentException.class, {
+      MaterialsRepresenter.toJSON(mock(OutputWriter.class), config)
+    })
+
+    assertEquals("Cannot serialize unsupported material type: PackageMaterialConfig", error.getMessage())
+  }
+
+  @Test
+  void fromJSON() {
+    JsonReader json = GsonTransformer.instance.jsonReaderFrom([
+      type      : "git",
+      attributes: [
+        name       : null,
+        url        : REPO_URL,
+        branch     : BRANCH,
+        auto_update: true
+      ]
+    ])
+
+    MaterialConfig expected = git(REPO_URL, BRANCH)
+    assertEquals(expected, MaterialsRepresenter.fromJSON(json))
+  }
+
+  @Test
+  void 'fromJSON fails for unsupported type'() {
+    Throwable error = assertThrows(UnprocessableEntityException.class, {
+      JsonReader json = GsonTransformer.instance.jsonReaderFrom([
+        type      : 'package',
+        attributes: [:]
+      ])
+      MaterialsRepresenter.fromJSON(json)
+    })
+    assertEquals("Unsupported material type: package. It has to be one of 'git, hg, svn, p4 and tfs'.", error.getMessage())
+  }
+}

35a2600595047712b15d1b409ee799e86ccb74b6
Kritika Singh <kritika.singh1994@gmail.com>
2020-02-19 16:05:22 +0530

    Send all the selected permissions to the api without filtering

--- server/src/main/webapp/WEB-INF/rails/webpack/models/admin_pipelines/admin_pipelines.ts
+++ server/src/main/webapp/WEB-INF/rails/webpack/models/admin_pipelines/admin_pipelines.ts
@@ -78,17 +78,17 @@ export class PipelineGroupViewModel {
   }
 
   getUpdatedPipelineGroup(): PipelineGroup {
+    const viewAccess    = new AuthorizedUsersAndRoles(
+      this.authorizedUsers().filter((user) => !_.isEmpty(user.name()) && user.view()).map((user) => user.name()),
+      this.authorizedRoles().filter((role) => !_.isEmpty(role.name()) && role.view()).map((role) => role.name())
+    );
+    const operateAccess = new AuthorizedUsersAndRoles(
+      this.authorizedUsers().filter((user) => !_.isEmpty(user.name()) && user.operate()).map((user) => user.name()),
+      this.authorizedRoles().filter((role) => !_.isEmpty(role.name()) && role.operate()).map((role) => role.name())
+    );
     const adminAccess   = new AuthorizedUsersAndRoles(
       this.authorizedUsers().filter((user) => !_.isEmpty(user.name()) && user.admin()).map((user) => user.name()),
       this.authorizedRoles().filter((role) => !_.isEmpty(role.name()) && role.admin()).map((role) => role.name())
-    );
-    const operateAccess = new AuthorizedUsersAndRoles(
-      this.authorizedUsers().filter((user) => !_.isEmpty(user.name()) && user.operate() && !user.admin()).map((user) => user.name()),
-      this.authorizedRoles().filter((role) => !_.isEmpty(role.name()) && role.operate() && !role.admin()).map((role) => role.name())
-    );
-    const viewAccess    = new AuthorizedUsersAndRoles(
-      this.authorizedUsers().filter((user) => !_.isEmpty(user.name()) && user.view() && !(user.admin() || user.operate())).map((user) => user.name()),
-      this.authorizedRoles().filter((role) => !_.isEmpty(role.name()) && role.view() && !(role.admin() || role.operate())).map((role) => role.name())
     );
     return new PipelineGroup(this.name(), new Authorization(viewAccess, adminAccess, operateAccess));
   }

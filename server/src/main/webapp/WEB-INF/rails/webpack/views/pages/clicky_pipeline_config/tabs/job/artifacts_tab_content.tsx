613ed3dfd49ae5f20170b0c27f5b274658514db1
GaneshSPatil <ganeshpl@thoughtworks.com>
2020-02-25 10:47:26 +0530

    [Pipeline Config SPA] Add support for external artifacts on Job Artifacts Tab

--- server/src/main/webapp/WEB-INF/rails/webpack/views/pages/clicky_pipeline_config/tabs/job/artifacts_tab_content.tsx
+++ server/src/main/webapp/WEB-INF/rails/webpack/views/pages/clicky_pipeline_config/tabs/job/artifacts_tab_content.tsx
@@ -14,12 +14,19 @@
  * limitations under the License.
  */
 
+import {bind} from "classnames/bind";
 import m from "mithril";
 import Stream from "mithril/stream";
-import {Artifact, Artifacts, ArtifactType, GoCDArtifact} from "models/pipeline_configs/artifact";
+import {ArtifactStores} from "models/artifact_stores/artifact_stores";
+import {ArtifactStoresCRUD} from "models/artifact_stores/artifact_stores_crud";
+import {Artifact, Artifacts, ArtifactType, ExternalArtifact, GoCDArtifact} from "models/pipeline_configs/artifact";
 import {Job} from "models/pipeline_configs/job";
 import {PipelineConfig} from "models/pipeline_configs/pipeline_config";
 import {TemplateConfig} from "models/pipeline_configs/template_config";
+import {ExtensionTypeString} from "models/shared/plugin_infos_new/extension_type";
+import {ArtifactExtension} from "models/shared/plugin_infos_new/extensions";
+import {PluginInfos} from "models/shared/plugin_infos_new/plugin_info";
+import {PluginInfoCRUD} from "models/shared/plugin_infos_new/plugin_info_crud";
 import {Secondary} from "views/components/buttons";
 import {FlashMessage, MessageType} from "views/components/flash_message";
 import {SelectField, SelectFieldOptions, TextField} from "views/components/forms/input_fields";
@@ -29,13 +36,20 @@ import {TooltipSize} from "views/components/tooltip";
 import {PipelineConfigRouteParams} from "views/pages/clicky_pipeline_config/pipeline_config";
 import styles from "views/pages/clicky_pipeline_config/tabs/job/artifacts.scss";
 import {TabContent} from "views/pages/clicky_pipeline_config/tabs/tab_content";
+import * as foundationStyles from "views/pages/new_plugins/foundation_hax.scss";
+
+const AngularPluginNew     = require("views/shared/angular_plugin_new").AngularPluginNew;
+const foundationClassNames = bind(foundationStyles);
 
 export class ArtifactsTabContent extends TabContent<Job> {
+  public artifactStores: Stream<ArtifactStores> = Stream(new ArtifactStores());
+  public pluginInfos: Stream<PluginInfos>       = Stream(new PluginInfos());
   private addArtifactType: Stream<ArtifactType> = Stream();
 
-  constructor() {
+  constructor(fetchData?: () => void) {
     super();
     this.addArtifactType(ArtifactType.build);
+    fetchData ? fetchData() : this.fetchData();
   }
 
   content(pipelineConfig: PipelineConfig,
@@ -57,7 +71,7 @@ export class ArtifactsTabContent extends TabContent<Job> {
         case ArtifactType.test:
           return this.getBuiltInArtifactView(artifact as GoCDArtifact, this.removalFn(entity.artifacts(), index));
         case ArtifactType.external:
-          return <div>external one!</div>;
+          return this.getExternalArtifactView(artifact as ExternalArtifact, this.removalFn(entity.artifacts(), index));
       }
     });
 
@@ -94,7 +108,7 @@ export class ArtifactsTabContent extends TabContent<Job> {
   }
 
   private getBuiltInArtifactView(artifact: GoCDArtifact, removeEntityFn: () => void) {
-    return <div data-test-id={`${artifact.type()}-artifact-view`}>
+    return <div class={styles.artifactContainer} data-test-id={`${artifact.type()}-artifact-view`}>
       {this.getBuiltInArtifactHeaders()}
       <div class={styles.builtInArtifactContainer}>
         <div class={styles.artifactType} data-test-id="artifact-type">
@@ -113,21 +127,107 @@ export class ArtifactsTabContent extends TabContent<Job> {
     </div>;
   }
 
+  private getExternalArtifactHeaders() {
+    return <div class={styles.builtInArtifactHeader} data-test-id="tabs-header">
+      <span data-test-id="type-header">
+        Type: <Tooltip.Info size={TooltipSize.small}
+                            content={"There are 3 types of artifacts - build, test and external. When 'Test Artifact' is selected, Go will use this artifact to generate a test report. Test information is placed in the Failures and Test sub-tabs. Test results from multiple jobs are aggregated on the stage detail pages. This allows you to see the results of tests from both functional and unit tests even if they are run in different jobs. When artifact type external is selected, you can configure the external artifact store to which you can push an artifact."}/>
+      </span>
+      <span class={styles.idHeader} data-test-id="id-header">
+        Id: <Tooltip.Info size={TooltipSize.small}
+                          content={"This id is used to identify the artifact that is pushed to an external store. The id is used later in a downstream pipeline to fetch the artifact from the external store."}/>
+      </span>
+      <span data-test-id="store-id-header">
+        Store Id: <Tooltip.Info size={TooltipSize.small}
+                                content={"This is a reference to the global artifact store defined in the config. At the time of publishing the artifact to an external store, the plugin makes use of the global properties associated with this store id."}/>
+      </span>
+    </div>;
+  }
+
+  private getExternalArtifactView(artifact: ExternalArtifact, removeEntityFn: () => void) {
+    let pluginConfigurations: m.Child;
+    if (!!artifact.storeId()) {
+      const found      = this.artifactStores().find(store => store.id() === artifact.storeId())!;
+      const pluginInfo = this.pluginInfos().findByPluginId(found.pluginId())!;
+
+      const artifactExtension = pluginInfo.extensionOfType(ExtensionTypeString.ARTIFACT) as ArtifactExtension;
+      pluginConfigurations    = (<div class={`${foundationClassNames(foundationStyles.foundationGridHax,
+                                                                     foundationStyles.foundationFormHax)}
+                                                                     ${styles.pluginView}`}>
+        <AngularPluginNew pluginInfoSettings={Stream(artifactExtension.artifactConfigSettings)}
+                          configuration={artifact.configuration()}/>
+      </div>);
+    }
+
+    return <div class={styles.artifactContainer} data-test-id={`${artifact.type()}-artifact-view`}>
+      {this.getExternalArtifactHeaders()}
+      <div className={styles.builtInArtifactContainer}>
+        <div className={styles.artifactType} data-test-id="artifact-type">
+          {artifact.type()[0].toUpperCase() + artifact.type().slice(1)} Artifact
+        </div>
+        <TextField dataTestId={`artifact-id-${artifact.artifactId() || ""}`}
+                   placeholder="id"
+                   property={artifact.artifactId}/>
+        <SelectField property={artifact.storeId}>
+          <SelectFieldOptions selected={artifact.storeId()}
+                              items={this.artifactStores().map(s => s.id())}/>
+        </SelectField>
+        <Icons.Close data-test-id={`remove-artifact`}
+                     iconOnly={true}
+                     onclick={() => removeEntityFn()}/>
+      </div>
+      {pluginConfigurations}
+    </div>;
+  }
+
   private removalFn(collection: Artifacts, index: number) {
     return () => collection.splice(index, 1);
   }
 
   private addArtifactView(artifacts: Artifacts) {
-    return (<div class={styles.addArtifactWrapper} data-test-id="add-artifact-wrapper">
-      <SelectField property={this.addArtifactType}>
-        <SelectFieldOptions selected={this.addArtifactType()}
-                            items={[ArtifactType.build, ArtifactType.test]}/>
-      </SelectField>
-      <Secondary small={true} onclick={this.addArtifact.bind(this, artifacts)}>Add Artifact</Secondary>
+    let noArtifactStoreError: m.Child;
+
+    if (this.addArtifactType() === ArtifactType.external && this.artifactStores().length === 0) {
+      noArtifactStoreError = <FlashMessage type={MessageType.alert}
+                                           message={"Can not define external artifact! No Artifact store configured."}/>;
+    }
+
+    return (<div data-test-id="add-artifact-wrapper">
+      {noArtifactStoreError}
+      <div class={styles.addArtifactWrapper}>
+        <SelectField property={this.addArtifactType}>
+          <SelectFieldOptions selected={this.addArtifactType()}
+                              items={[ArtifactType.build, ArtifactType.test, ArtifactType.external]}/>
+        </SelectField>
+        <Secondary small={true}
+                   disabled={!!noArtifactStoreError}
+                   onclick={this.addArtifact.bind(this, artifacts)}>
+          Add Artifact
+        </Secondary>
+      </div>
     </div>);
   }
 
   private addArtifact(artifacts: Artifacts) {
     artifacts.push(Artifact.fromJSON({type: this.addArtifactType()}));
+  }
+
+  private fetchData() {
+    this.pageLoading();
+    return Promise.all([
+                         PluginInfoCRUD.all({type: ExtensionTypeString.ARTIFACT}),
+                         ArtifactStoresCRUD.all()
+                       ])
+                  .then((results) => {
+                    results[0].do((successResponse) => {
+                      this.pluginInfos(successResponse.body);
+                      this.pageLoaded();
+                    }, this.pageLoadFailure);
+
+                    results[1].do((successResponse) => {
+                      this.artifactStores(successResponse.body);
+                      this.pageLoaded();
+                    }, this.pageLoadFailure);
+                  });
   }
 }
--- server/src/main/webapp/WEB-INF/rails/webpack/views/pages/clicky_pipeline_config/tabs/tab_content.tsx
+++ server/src/main/webapp/WEB-INF/rails/webpack/views/pages/clicky_pipeline_config/tabs/tab_content.tsx
@@ -17,22 +17,46 @@
 import m from "mithril";
 import {PipelineConfig} from "models/pipeline_configs/pipeline_config";
 import {TemplateConfig} from "models/pipeline_configs/template_config";
+import {PageLoadError} from "views/components/page_load_error";
+import {Spinner} from "views/components/spinner";
 import {PipelineConfigRouteParams} from "views/pages/clicky_pipeline_config/pipeline_config";
+import {PageState} from "views/pages/page";
 
 export abstract class TabContent<T> {
+  private pageState = PageState.OK;
+
   //render only selected tab
   public content(pipelineConfig: PipelineConfig,
                  templateConfig: TemplateConfig,
                  routeParams: PipelineConfigRouteParams,
                  isSelectedTab: boolean): m.Children {
-    if (isSelectedTab) {
-      const entity = this.selectedEntity(pipelineConfig, routeParams) as T;
-      return this.renderer(entity, templateConfig);
+    switch (this.pageState) {
+      case PageState.FAILED:
+        return <PageLoadError message={`There was a problem fetching ${this.name()} tab`}/>;
+      case PageState.LOADING:
+        return <Spinner/>;
+      case PageState.OK:
+        if (isSelectedTab) {
+          const entity = this.selectedEntity(pipelineConfig, routeParams) as T;
+          return this.renderer(entity, templateConfig);
+        }
+        return <div> Not a selected tab</div>;
     }
-    return <div> Not a selected tab</div>;
   }
 
   public abstract name(): string;
+
+  public pageLoadFailure() {
+    this.pageState = PageState.FAILED;
+  }
+
+  public pageLoading() {
+    this.pageState = PageState.LOADING;
+  }
+
+  public pageLoaded() {
+    this.pageState = PageState.OK;
+  }
 
   protected abstract renderer(entity: T, templateConfig: TemplateConfig): m.Children;
 

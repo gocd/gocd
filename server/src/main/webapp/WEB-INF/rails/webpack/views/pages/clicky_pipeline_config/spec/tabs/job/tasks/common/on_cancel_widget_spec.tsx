ddcf3f5377b20f3506e603f9b28e20e0e36f7f33
GaneshSPatil <ganeshpl@thoughtworks.com>
2020-02-27 15:58:08 +0530

    [Pipeline Config SPA] Ant Task Modal

---
+++ server/src/main/webapp/WEB-INF/rails/webpack/views/pages/clicky_pipeline_config/spec/tabs/job/tasks/common/on_cancel_widget_spec.tsx
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import m from "mithril";
+import Stream from "mithril/stream";
+import {AntTask, Task} from "models/pipeline_configs/task";
+import {OnCancelTaskWidget} from "views/pages/clicky_pipeline_config/tabs/job/tasks/common/on_cancel_widget";
+import {TestHelper} from "views/pages/spec/test_helper";
+
+describe("On Cancel Task Widget", () => {
+  const helper = new TestHelper();
+
+  afterEach(helper.unmount.bind(helper));
+
+  it("should not render any on cancel task when none specified", () => {
+    const task: Stream<Task | undefined> = Stream();
+    mount(task);
+
+    expect(helper.byTestId("on-cancel-view")).toBeInDOM();
+    expect(helper.byTestId("form-field-input-on-cancel-task")).not.toBeChecked();
+    expect(helper.byTestId("on-cancel-body")).not.toBeInDOM();
+  });
+
+  it("should render on cancel task when specified", () => {
+    const task: Stream<Task> = Stream(new AntTask("build.xml", "default", "pwd", []));
+    mount(task as Stream<Task | undefined>);
+
+    expect(helper.byTestId("on-cancel-view")).toBeInDOM();
+    expect(helper.byTestId("form-field-input-on-cancel-task")).toBeChecked();
+    expect(helper.byTestId("on-cancel-body")).toBeInDOM();
+
+    expect(helper.byTestId("form-field-input-build-file")).toHaveValue("build.xml");
+    expect(helper.byTestId("form-field-input-target")).toHaveValue("default");
+    expect(helper.byTestId("form-field-input-working-directory")).toHaveValue("pwd");
+  });
+
+  const mount = (onCancel: Stream<Task | undefined>) => {
+    helper.mount(() => {
+      return <OnCancelTaskWidget onCancel={onCancel}/>;
+    });
+  };
+});
---
+++ server/src/main/webapp/WEB-INF/rails/webpack/views/pages/clicky_pipeline_config/spec/tabs/job/tasks/common/run_if_widget_spec.tsx
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import m from "mithril";
+import Stream from "mithril/stream";
+import {RunIfCondition} from "models/pipeline_configs/task";
+import {RunIfConditionWidget} from "views/pages/clicky_pipeline_config/tabs/job/tasks/common/run_if_widget";
+
+import {TestHelper} from "views/pages/spec/test_helper";
+
+describe("Run If Condition Widget", () => {
+  const helper = new TestHelper();
+
+  afterEach(helper.unmount.bind(helper));
+  it("should render run if condition header", () => {
+    const runIf = Stream(["passed"] as RunIfCondition[]);
+    mount(runIf);
+
+    expect(helper.byTestId("run-if-condition")).toContainText("Run If Conditions");
+  });
+
+  it("should render passed selection", () => {
+    const runIf = Stream(["passed"] as RunIfCondition[]);
+    mount(runIf);
+
+    expect(helper.byTestId("form-field-input-passed")).toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-any")).not.toBeChecked();
+  });
+
+  it("should render failed selection", () => {
+    const runIf = Stream(["failed"] as RunIfCondition[]);
+    mount(runIf);
+
+    expect(helper.byTestId("form-field-input-passed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).toBeChecked();
+    expect(helper.byTestId("form-field-input-any")).not.toBeChecked();
+  });
+
+  it("should render any selection", () => {
+    const runIf = Stream(["any"] as RunIfCondition[]);
+    mount(runIf);
+
+    expect(helper.byTestId("form-field-input-passed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-any")).toBeChecked();
+  });
+
+  it("should select passed selection", () => {
+    const runIf = Stream([] as RunIfCondition[]);
+    mount(runIf);
+
+    expect(helper.byTestId("form-field-input-passed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-any")).not.toBeChecked();
+    expect(runIf()).toEqual([]);
+
+    helper.clickByTestId("form-field-input-passed");
+
+    expect(helper.byTestId("form-field-input-passed")).toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-any")).not.toBeChecked();
+    expect(runIf()).toEqual(["passed"]);
+  });
+
+  it("should select failed selection", () => {
+    const runIf = Stream([] as RunIfCondition[]);
+    mount(runIf);
+
+    expect(helper.byTestId("form-field-input-passed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-any")).not.toBeChecked();
+    expect(runIf()).toEqual([]);
+
+    helper.clickByTestId("form-field-input-failed");
+
+    expect(helper.byTestId("form-field-input-passed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).toBeChecked();
+    expect(helper.byTestId("form-field-input-any")).not.toBeChecked();
+    expect(runIf()).toEqual(["failed"]);
+  });
+
+  it("should select any selection", () => {
+    const runIf = Stream([] as RunIfCondition[]);
+    mount(runIf);
+
+    expect(helper.byTestId("form-field-input-passed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-any")).not.toBeChecked();
+    expect(runIf()).toEqual([]);
+
+    helper.clickByTestId("form-field-input-any");
+
+    expect(helper.byTestId("form-field-input-passed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-any")).toBeChecked();
+    expect(runIf()).toEqual(["any"]);
+  });
+
+  it("should clear existing selection when any is selected", () => {
+    const runIf = Stream(["passed", "failed"] as RunIfCondition[]);
+    mount(runIf);
+
+    expect(helper.byTestId("form-field-input-passed")).toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).toBeChecked();
+    expect(helper.byTestId("form-field-input-any")).not.toBeChecked();
+    expect(runIf()).toEqual(["passed", "failed"]);
+
+    helper.clickByTestId("form-field-input-any");
+
+    expect(helper.byTestId("form-field-input-passed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-passed")).toBeDisabled();
+    expect(helper.byTestId("form-field-input-failed")).not.toBeChecked();
+    expect(helper.byTestId("form-field-input-failed")).toBeDisabled();
+    expect(helper.byTestId("form-field-input-any")).toBeChecked();
+
+    expect(runIf()).toEqual(["any"]);
+  });
+
+  function mount(runIf: Stream<RunIfCondition[]>) {
+    helper.mount(() => {
+      return <RunIfConditionWidget runIf={runIf}/>;
+    });
+  }
+});

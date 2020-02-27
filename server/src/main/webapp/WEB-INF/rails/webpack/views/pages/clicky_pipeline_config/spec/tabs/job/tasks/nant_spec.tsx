588db1816241469ca4e85da464d5380a9fba0eb2
GaneshSPatil <ganeshpl@thoughtworks.com>
2020-02-27 16:18:00 +0530

    [Pipeline Config SPA] NAnt Task Modal

---
+++ server/src/main/webapp/WEB-INF/rails/webpack/views/pages/clicky_pipeline_config/spec/tabs/job/tasks/nant_spec.tsx
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {NantTask, NantTaskAttributes, Task} from "models/pipeline_configs/task";
+import {NantTaskModal} from "views/pages/clicky_pipeline_config/tabs/job/tasks/nant";
+import {TestHelper} from "views/pages/spec/test_helper";
+
+describe("Nant Task Modal", () => {
+  const helper = new TestHelper();
+
+  afterEach(helper.unmount.bind(helper));
+
+  it("should render nant task modal", () => {
+    mount();
+    expect(helper.byTestId("nant-task-modal")).toBeInDOM();
+  });
+
+  it("should render build file input", () => {
+    mount();
+
+    const buildFileHelpText = "Relative path to a NAnt build file. If not specified, the path defaults to 'default.build'.";
+
+    expect(helper.byTestId("form-field-label-build-file")).toContainText("Build File");
+    expect(helper.byTestId("form-field-input-build-file")).toBeInDOM();
+    expect(helper.byTestId("form-field-input-build-file")).toBeInDOM();
+    expect(helper.qa("span")[0]).toContainText(buildFileHelpText);
+  });
+
+  it("should bind build file input to model", () => {
+    const nantTask = new NantTask("build.xml", "target", "nant-path-exe", "/tmp", []);
+    mount(nantTask);
+
+    const attributes = nantTask.attributes() as NantTaskAttributes;
+
+    expect(attributes.buildFile()).toBe("build.xml");
+    expect(helper.byTestId("form-field-input-build-file")).toHaveValue("build.xml");
+
+    helper.oninput(`[data-test-id="form-field-input-build-file"]`, "new-build.xml");
+
+    expect(attributes.buildFile()).toBe("new-build.xml");
+    expect(helper.byTestId("form-field-input-build-file")).toHaveValue("new-build.xml");
+  });
+
+  it("should render target input", () => {
+    mount();
+
+    const buildFileHelpText = "NAnt target(s) to run. If not specified, defaults to the default target of the build file.";
+
+    expect(helper.byTestId("form-field-label-target")).toContainText("Target");
+    expect(helper.byTestId("form-field-input-target")).toBeInDOM();
+    expect(helper.byTestId("form-field-input-target")).toBeInDOM();
+    expect(helper.qa("span")[1]).toContainText(buildFileHelpText);
+  });
+
+  it("should bind target input to model", () => {
+    const nantTask = new NantTask("build.xml", "default", "nant-path-exec", "/tmp", []);
+    mount(nantTask);
+
+    const attributes = nantTask.attributes() as NantTaskAttributes;
+
+    expect(attributes.target()).toBe("default");
+    expect(helper.byTestId("form-field-input-target")).toHaveValue("default");
+
+    helper.oninput(`[data-test-id="form-field-input-target"]`, "new-default");
+
+    expect(attributes.target()).toBe("new-default");
+    expect(helper.byTestId("form-field-input-target")).toHaveValue("new-default");
+  });
+
+  it("should render Nant Path input", () => {
+    mount();
+
+    const buildFileHelpText = "Path of the directory in which NAnt is installed. By default Go will assume that NAnt is in the system path.";
+
+    expect(helper.byTestId("form-field-label-nant-path")).toContainText("NAnt Path");
+    expect(helper.byTestId("form-field-input-nant-path")).toBeInDOM();
+    expect(helper.byTestId("form-field-input-nant-path")).toBeInDOM();
+    expect(helper.qa("span")[3]).toContainText(buildFileHelpText);
+  });
+
+  it("should bind nant path input to model", () => {
+    const nantTask = new NantTask("build.xml", "default", "nant-path-exec", "/tmp", []);
+    mount(nantTask);
+
+    const attributes = nantTask.attributes() as NantTaskAttributes;
+
+    expect(attributes.nantPath()).toBe("nant-path-exec");
+    expect(helper.byTestId("form-field-input-nant-path")).toHaveValue("nant-path-exec");
+
+    helper.oninput(`[data-test-id="form-field-input-nant-path"]`, "new-nant-path-exec");
+
+    expect(attributes.nantPath()).toBe("new-nant-path-exec");
+    expect(helper.byTestId("form-field-input-nant-path")).toHaveValue("new-nant-path-exec");
+  });
+
+  it("should render working directory input", () => {
+    mount();
+
+    const buildFileHelpText = "The directory from where nant is invoked.";
+
+    expect(helper.byTestId("form-field-label-working-directory")).toContainText("Working Directory");
+    expect(helper.byTestId("form-field-input-working-directory")).toBeInDOM();
+    expect(helper.byTestId("form-field-input-working-directory")).toBeInDOM();
+    expect(helper.qa("span")[2]).toContainText(buildFileHelpText);
+  });
+
+  it("should bind working directory input to model", () => {
+    const nantTask = new NantTask("build.xml", "target", "nant-path-exec", "tmp", []);
+    mount(nantTask);
+
+    const attributes = nantTask.attributes() as NantTaskAttributes;
+
+    expect(attributes.workingDirectory()).toBe("tmp");
+    expect(helper.byTestId("form-field-input-working-directory")).toHaveValue("tmp");
+
+    helper.oninput(`[data-test-id="form-field-input-working-directory"]`, "new-tmp");
+
+    expect(attributes.workingDirectory()).toBe("new-tmp");
+    expect(helper.byTestId("form-field-input-working-directory")).toHaveValue("new-tmp");
+  });
+
+  it("should render run if condition", () => {
+    mount();
+    expect(helper.byTestId("run-if-condition")).toBeInDOM();
+  });
+
+  it("should render run on cancel", () => {
+    mount();
+    expect(helper.byTestId("on-cancel-view")).toBeInDOM();
+  });
+
+  it("should not render run if condition for on cancel task", () => {
+    mount(undefined, false);
+
+    expect(helper.byTestId("nant-on-cancel-view")).not.toBeInDOM();
+    expect(helper.byTestId("run-if-condition")).toBeFalsy();
+  });
+
+  it("should not render on cancel for on cancel task", () => {
+    mount(undefined, false);
+
+    expect(helper.byTestId("nant-on-cancel-view")).not.toBeInDOM();
+    expect(helper.byTestId("on-cancel-view")).toBeFalsy();
+  });
+
+  function mount(task?: Task | undefined, shouldShowOnCancel: boolean = true) {
+    helper.mount(() => {
+      return new NantTaskModal(task, shouldShowOnCancel, jasmine.createSpy()).body();
+    });
+  }
+});
--- server/src/main/webapp/WEB-INF/rails/webpack/views/pages/clicky_pipeline_config/tabs/job/tasks/nant.tsx
+++ server/src/main/webapp/WEB-INF/rails/webpack/views/pages/clicky_pipeline_config/tabs/job/tasks/nant.tsx
@@ -15,22 +15,52 @@
  */
 
 import m from "mithril";
-import {NantTask, Task} from "models/pipeline_configs/task";
+import {NantTask, NantTaskAttributes, Task} from "models/pipeline_configs/task";
+import {TextField} from "views/components/forms/input_fields";
 import {AbstractTaskModal} from "views/pages/clicky_pipeline_config/tabs/job/tasks/abstract";
+import {OnCancelTaskWidget} from "views/pages/clicky_pipeline_config/tabs/job/tasks/common/on_cancel_widget";
+import {RunIfConditionWidget} from "views/pages/clicky_pipeline_config/tabs/job/tasks/common/run_if_widget";
 
 export class NantTaskModal extends AbstractTaskModal {
   private readonly showOnCancel: boolean;
+  private readonly task: Task;
 
   constructor(task: Task | undefined, showOnCancel: boolean, onAdd: (t: Task) => void) {
     super(onAdd);
-
     this.showOnCancel = showOnCancel;
+    this.task         = task ? task : new NantTask(undefined, undefined, undefined, undefined, [], undefined);
   }
 
   body(): m.Children {
-    return <div>
-      this will return nant task
-      {this.showOnCancel}
+    const attributes = this.task.attributes() as NantTaskAttributes;
+
+    let onCancel: m.Child | undefined;
+
+    if (this.showOnCancel) {
+      onCancel = <div data-test-id="nant-on-cancel-view">
+        <RunIfConditionWidget runIf={attributes.runIf}/>
+        <h3>Advanced Option</h3>
+        <OnCancelTaskWidget onCancel={attributes.onCancel}/>
+      </div>;
+    }
+
+    return <div data-test-id="nant-task-modal">
+      <h3>Basic Settings</h3>
+      <TextField helpText="Relative path to a NAnt build file. If not specified, the path defaults to 'default.build'."
+                 label="Build File"
+                 placeholder="default.build"
+                 property={attributes.buildFile}/>
+      <TextField helpText="NAnt target(s) to run. If not specified, defaults to the default target of the build file."
+                 label="Target"
+                 placeholder="default"
+                 property={attributes.target}/>
+      <TextField helpText="The directory from where nant is invoked."
+                 label="Working Directory"
+                 property={attributes.workingDirectory}/>
+      <TextField helpText="Path of the directory in which NAnt is installed. By default Go will assume that NAnt is in the system path."
+                 label="NAnt Path"
+                 property={attributes.nantPath}/>
+      {onCancel}
     </div>;
   }
 
@@ -39,6 +69,6 @@ export class NantTaskModal extends AbstractTaskModal {
   }
 
   getTask(): Task {
-    return new NantTask(undefined, undefined, undefined, undefined, []);
+    return this.task;
   }
 }

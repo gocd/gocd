3115999a5563d7776b48b6ecace8f36e6467fb67
Kritika Singh <kritika.singh1994@gmail.com>
2020-03-11 11:00:47 +0530

    Bump up package api to v2

---
+++ api/api-packages-v2/src/main/java/com/thoughtworks/go/apiv2/packages/representers/PackageDefinitionsRepresenter.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thoughtworks.go.apiv2.packages.representers;
+
+import com.thoughtworks.go.api.base.OutputWriter;
+import com.thoughtworks.go.domain.packagerepository.PackageDefinition;
+import com.thoughtworks.go.spark.Routes;
+
+import java.util.List;
+
+import static com.thoughtworks.go.CurrentGoCDVersion.apiDocsUrl;
+
+public class PackageDefinitionsRepresenter {
+    public static void toJSON(OutputWriter outputWriter, List<PackageDefinition> packages) {
+        outputWriter.addLinks(outputLinkWriter -> {
+            outputLinkWriter.addLink("self", Routes.Packages.BASE);
+            outputLinkWriter.addAbsoluteLink("doc", apiDocsUrl("#packages"));
+        });
+        outputWriter.addEmbedded(embeddedWriter -> {
+            embeddedWriter.addChildList("packages", listWriter -> {
+                packages.forEach(packageDefinition -> listWriter.addChild(childWriter -> PackageDefinitionRepresenter.toJSON(childWriter, packageDefinition)));
+            });
+        });
+    }
+}
---
+++ api/api-packages-v2/src/test/groovy/com/thoughtworks/go/apiv2/packages/representers/PackageRepositoryRepresenterTest.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thoughtworks.go.apiv2.packages.representers
+
+import com.thoughtworks.go.api.util.GsonTransformer
+import com.thoughtworks.go.domain.packagerepository.PackageRepository
+import org.junit.jupiter.api.Test
+
+import static com.thoughtworks.go.CurrentGoCDVersion.apiDocsUrl
+import static com.thoughtworks.go.api.base.JsonUtils.toObjectString
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson
+import static org.junit.jupiter.api.Assertions.*
+
+class PackageRepositoryRepresenterTest {
+
+  def expectedJson = [
+    _links: [
+      self: [
+        href: "http://test.host/go/api/admin/repositories/package-repo-id-1"
+      ],
+      doc : [
+        href: apiDocsUrl("#package-repositories")
+      ],
+      find: [
+        href: "http://test.host/go/api/admin/repositories/:repo_id"
+      ]
+    ],
+    id    : "package-repo-id-1",
+    name  : "package-repo-name-1"
+  ]
+
+  @Test
+  void 'should deserialize package repository'() {
+    def jsonReader = GsonTransformer.instance.jsonReaderFrom(expectedJson)
+    def packageRepository = PackageRepositoryRepresenter.fromJSON(jsonReader)
+
+    assertEquals('package-repo-id-1', packageRepository.id)
+    assertEquals('package-repo-name-1', packageRepository.name)
+  }
+
+  @Test
+  void 'should serialize package repository'() {
+    def packageRepository = new PackageRepository('package-repo-id-1', 'package-repo-name-1', null, null)
+    def actualJson = toObjectString({ PackageRepositoryRepresenter.toJSON(it, packageRepository) })
+
+    assertThatJson(actualJson).isEqualTo(expectedJson)
+  }
+}

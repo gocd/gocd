49966157ad62bda0123f5cebeb928e41299b6a5c
GaneshSPatil <ganeshpl@thoughtworks.com>
2020-04-22 19:43:52 +0530

    Redirect to new pipeline config spa and templates spa when SPA toggle is enabled

---
+++ server/src/main/java/com/thoughtworks/go/server/RedirectToPipelineConfigSPAToggle.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thoughtworks.go.server;
+
+import com.thoughtworks.go.server.service.support.toggle.Toggles;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RedirectToPipelineConfigSPAToggle {
+    public void redirect(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (Toggles.isToggleOn(Toggles.NEW_PIPELINE_CONFIG_SPA)) {
+            String pipelineOrTemplate = request.getRequestURI().split("/")[3];
+            String pipelineOrTemplateName = request.getRequestURI().split("/")[4];
+            response.sendRedirect("/go/admin/" + pipelineOrTemplate + "/" + pipelineOrTemplateName + "/edit");
+        }
+    }
+}

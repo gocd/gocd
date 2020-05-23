f2f05c5b03b325c4d9a9ff670dc4cff103bb42d5
Marques Lee <marques.lee@thoughtworks.com>
2020-05-23 08:14:36 -0700

    Git 2.26 changes unshallowing behavior for submodules on failure.

--- common/src/test/java/com/thoughtworks/go/domain/materials/git/EnabledOnGitVersionCondition.java
+++ common/src/test/java/com/thoughtworks/go/domain/materials/git/EnabledOnGitVersionCondition.java
@@ -23,28 +23,88 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.io.File;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
+import static com.thoughtworks.go.domain.materials.git.EnabledOnGitVersions.WILDCARD;
+import static java.lang.String.format;
 import static org.junit.platform.commons.util.AnnotationUtils.findAnnotation;
 
 public class EnabledOnGitVersionCondition implements ExecutionCondition {
-
+    private static final Pattern IS_VERSION = Pattern.compile("^\\d+\\.\\d+\\.\\d+$");
 
     @Override
     public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
-        Optional<EnabledOnGitVersionsAbove> annotation = findAnnotation(context.getElement(), EnabledOnGitVersionsAbove.class);
-        if (annotation.isPresent()) {
-            String version = annotation.get().value();
-            Version requiredVersion = Version.parseVersion(version);
-            GitCommand git = new GitCommand(null, new File(""), GitMaterialConfig.DEFAULT_BRANCH, false, null);
-            Version gitVersion = git.version().getVersion();
-            if (gitVersion.compareTo(requiredVersion) >= 0) {
-                return ConditionEvaluationResult.enabled(String.format(
-                        "Git version required for this test is %s or later. Detected Version is %s", requiredVersion, gitVersion));
+        Optional<EnabledOnGitVersions> maybe = findAnnotation(context.getElement(), EnabledOnGitVersions.class);
+        if (maybe.isPresent()) {
+            final EnabledOnGitVersions annotation = maybe.get();
+            final Version gitVersion = fetchGitVersion();
+
+            if (noneSpecified(annotation)) {
+                return ConditionEvaluationResult.enabled("Version requirements not provided. Running by default.");
+            }
+
+            if (atLeast(annotation.from(), gitVersion) && atMost(annotation.through(), gitVersion)) {
+                return ConditionEvaluationResult.enabled(
+                        format("Git version %s satisfies %s", gitVersion, criteria(annotation))
+                );
             } else {
-                return ConditionEvaluationResult.disabled(String.format(
-                        "Git version required for this test is %s or later. Detected Version is %s", requiredVersion, gitVersion));
+                return ConditionEvaluationResult.disabled(
+                        format("Git version %s does not satisfy %s", gitVersion, criteria(annotation))
+                );
             }
         }
-        return ConditionEvaluationResult.enabled("Version not provided. Running by default");
+
+        return ConditionEvaluationResult.enabled("Version requirements not provided. Running by default.");
+    }
+
+    private boolean atLeast(final String required, Version actual) {
+        if (WILDCARD.equals(required)) {
+            return true;
+        }
+
+        if (!IS_VERSION.matcher(required).matches()) {
+            throw new IllegalArgumentException(format("Bad version [%s]; `from` must be in major.minor.patch format!", required));
+        }
+        return actual.compareTo(Version.parseVersion(required)) >= 0;
+    }
+
+    private boolean atMost(final String required, Version actual) {
+        if (WILDCARD.equals(required)) {
+            return true;
+        }
+
+        if (!IS_VERSION.matcher(required).matches()) {
+            throw new IllegalArgumentException(format("Bad version [%s]; `through` must be in major.minor.patch format!", required));
+        }
+        return actual.compareTo(Version.parseVersion(required)) <= 0;
+    }
+
+    private boolean bothSpecified(EnabledOnGitVersions annotation) {
+        return !WILDCARD.equals(annotation.from()) && !WILDCARD.equals(annotation.through());
+    }
+
+    private boolean noneSpecified(EnabledOnGitVersions annotation) {
+        return WILDCARD.equals(annotation.from()) && WILDCARD.equals(annotation.through());
+    }
+
+    private String criteria(EnabledOnGitVersions annotation) {
+        if (noneSpecified(annotation)) {
+            return "<any version>";
+        }
+
+        if (bothSpecified(annotation)) {
+            return format("[from: %s, through: %s]", annotation.from(), annotation.through());
+        }
+
+        if (!WILDCARD.equals(annotation.from())) {
+            return format("[from: %s]", annotation.from());
+        }
+
+        return format("[through: %s]", annotation.through());
+    }
+
+    private Version fetchGitVersion() {
+        return new GitCommand(null, new File(""), GitMaterialConfig.DEFAULT_BRANCH, false, null).
+                version().getVersion();
     }
 }

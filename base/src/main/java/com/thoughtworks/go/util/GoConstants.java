a52fe8f680c09d4fb6b30d317ed23c47e1b88c47
GaneshSPatil <ganeshpl@thoughtworks.com>
2020-07-03 15:20:55 +0530

    Define xml schema migration to add an echo task for the jobs with no tasks.

--- base/src/main/java/com/thoughtworks/go/util/GoConstants.java
+++ base/src/main/java/com/thoughtworks/go/util/GoConstants.java
@@ -53,7 +53,7 @@ public class GoConstants {
 
     public static final String PRODUCT_NAME = "go";
 
-    public static final int CONFIG_SCHEMA_VERSION = 136;
+    public static final int CONFIG_SCHEMA_VERSION = 137;
 
     public static final String APPROVAL_SUCCESS = "success";
     public static final String APPROVAL_MANUAL = "manual";

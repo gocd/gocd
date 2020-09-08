fc49207e1f19c054a51cde531ff3caf3cc5e546a
Kritika Singh <kritika.singh1994@gmail.com>
2020-09-08 10:16:15 +0530

    Added tests to verify param resolution when material check connection is called

--- api/api-internal-material-test-v1/src/test/groovy/com/thoughtworks/go/apiv1/internalmaterialtest/InternalMaterialTestControllerV1Test.groovy
+++ api/api-internal-material-test-v1/src/test/groovy/com/thoughtworks/go/apiv1/internalmaterialtest/InternalMaterialTestControllerV1Test.groovy
@@ -17,6 +17,8 @@ package com.thoughtworks.go.apiv1.internalmaterialtest
 
 import com.thoughtworks.go.api.SecurityTestTrait
 import com.thoughtworks.go.api.spring.ApiAuthenticationHelper
+import com.thoughtworks.go.config.CaseInsensitiveString
+import com.thoughtworks.go.config.PipelineConfig
 import com.thoughtworks.go.config.materials.PasswordDeserializer
 import com.thoughtworks.go.config.materials.git.GitMaterial
 import com.thoughtworks.go.domain.materials.MaterialConfig
@@ -24,17 +26,17 @@ import com.thoughtworks.go.domain.materials.ValidationBean
 import com.thoughtworks.go.domain.materials.git.GitCommand
 import com.thoughtworks.go.server.service.MaterialConfigConverter
 import com.thoughtworks.go.server.service.SecretParamResolver
-import com.thoughtworks.go.spark.AdminUserSecurity
 import com.thoughtworks.go.spark.ControllerTrait
 import com.thoughtworks.go.spark.GroupAdminUserSecurity
-import com.thoughtworks.go.spark.NormalUserSecurity
 import com.thoughtworks.go.spark.SecurityServiceTrait
 import com.thoughtworks.go.util.SystemEnvironment
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.mockito.ArgumentCaptor
 import org.mockito.Mock
 
+import static org.junit.jupiter.api.Assertions.assertEquals
 import static org.mockito.ArgumentMatchers.*
 import static org.mockito.Mockito.*
 import static org.mockito.MockitoAnnotations.initMocks
@@ -93,54 +95,54 @@ class InternalMaterialTestControllerV1Test implements SecurityServiceTrait, Cont
       void 'should render error if material type is invalid'() {
         postWithApiHeader(controller.controllerBasePath(), ["type": "some-random-type"])
         assertThatResponse()
-            .isUnprocessableEntity()
-            .hasJsonMessage("Your request could not be processed. Invalid material type 'some-random-type'. It has to be one of [git, hg, svn, p4, tfs, dependency, package, plugin].")
+          .isUnprocessableEntity()
+          .hasJsonMessage("Your request could not be processed. Invalid material type 'some-random-type'. It has to be one of [git, hg, svn, p4, tfs, dependency, package, plugin].")
       }
 
       @Test
       void 'should render error if material type does not support check connection functionality'() {
         postWithApiHeader(controller.controllerBasePath(), ["type": "dependency"])
         assertThatResponse()
-            .isUnprocessableEntity()
-            .hasJsonMessage("Your request could not be processed. The material of type 'dependency' does not support connection testing.")
+          .isUnprocessableEntity()
+          .hasJsonMessage("Your request could not be processed. The material of type 'dependency' does not support connection testing.")
       }
 
       @Test
       void 'should render error if material is not valid'() {
         postWithApiHeader(controller.controllerBasePath(), [
-            "type"      : "git",
-            "attributes": [
-                "url"               : "",
-                "encrypted_password": "encrypted-password",
-                "password"          : "password"
-            ]
+          "type"      : "git",
+          "attributes": [
+            "url"               : "",
+            "encrypted_password": "encrypted-password",
+            "password"          : "password"
+          ]
         ])
 
         def expectedJSON = [
-            "message": "There was an error with the material configuration.\n- url: URL cannot be blank",
-            "data"   : [
-                "errors"    : [
-                    "url": ["URL cannot be blank"]
-                ],
-                "type"      : "git",
-                "attributes": [
-                    "url"             : "",
-                    "destination"     : null,
-                    "filter"          : null,
-                    "invert_filter"   : false,
-                    "name"            : null,
-                    "auto_update"     : true,
-                    "branch"          : "master",
-                    "submodule_folder": null,
-                    "shallow_clone"   : false
-                ]
+          "message": "There was an error with the material configuration.\n- url: URL cannot be blank",
+          "data"   : [
+            "errors"    : [
+              "url": ["URL cannot be blank"]
+            ],
+            "type"      : "git",
+            "attributes": [
+              "url"             : "",
+              "destination"     : null,
+              "filter"          : null,
+              "invert_filter"   : false,
+              "name"            : null,
+              "auto_update"     : true,
+              "branch"          : "master",
+              "submodule_folder": null,
+              "shallow_clone"   : false
             ]
+          ]
         ]
 
         assertThatResponse()
-            .isUnprocessableEntity()
-            .hasJsonMessage("There was an error with the material configuration.\\n- url: URL cannot be blank")
-            .hasJsonBody(expectedJSON)
+          .isUnprocessableEntity()
+          .hasJsonMessage("There was an error with the material configuration.\\n- url: URL cannot be blank")
+          .hasJsonBody(expectedJSON)
       }
 
       @Test
@@ -151,15 +153,15 @@ class InternalMaterialTestControllerV1Test implements SecurityServiceTrait, Cont
         when(materialConfigConverter.toMaterial(any(MaterialConfig.class))).thenReturn(material)
 
         postWithApiHeader(controller.controllerBasePath(), [
-            "type"      : "git",
-            "attributes": [
-                "url": "some-url"
-            ]
+          "type"      : "git",
+          "attributes": [
+            "url": "some-url"
+          ]
         ])
 
         assertThatResponse()
-            .isUnprocessableEntity()
-            .hasJsonMessage("some-error")
+          .isUnprocessableEntity()
+          .hasJsonMessage("some-error")
       }
 
       @Test
@@ -170,15 +172,15 @@ class InternalMaterialTestControllerV1Test implements SecurityServiceTrait, Cont
         when(materialConfigConverter.toMaterial(any(MaterialConfig.class))).thenReturn(material)
 
         postWithApiHeader(controller.controllerBasePath(), [
-            "type"      : "git",
-            "attributes": [
-                "url": "some-url"
-            ]
+          "type"      : "git",
+          "attributes": [
+            "url": "some-url"
+          ]
         ])
 
         assertThatResponse()
-            .isOk()
-            .hasJsonMessage("Connection OK.")
+          .isOk()
+          .hasJsonMessage("Connection OK.")
       }
 
       @Test
@@ -202,6 +204,56 @@ class InternalMaterialTestControllerV1Test implements SecurityServiceTrait, Cont
         assertThatResponse()
           .isOk()
           .hasJsonMessage("Connection OK.")
+      }
+
+      @Test
+      void 'should resolve params when pipeline_name is provided'() {
+        def material = mock(GitMaterial.class)
+        def validationBean = ValidationBean.valid()
+        when(material.checkConnection(any())).thenReturn(validationBean)
+        when(materialConfigConverter.toMaterial(any(MaterialConfig.class))).thenReturn(material)
+        when(goConfigService.pipelineConfigNamed(any(CaseInsensitiveString.class))).thenReturn(new PipelineConfig())
+
+        postWithApiHeader(controller.controllerBasePath(), [
+          "pipeline_name": "some-pipeline-name",
+          "type"         : "git",
+          "attributes"   : [
+            "url": "http://some-url/val##"
+          ]
+        ])
+
+        verify(goConfigService).pipelineConfigNamed(any(CaseInsensitiveString.class))
+
+        def captor = ArgumentCaptor.forClass(MaterialConfig.class)
+        verify(materialConfigConverter).toMaterial(captor.capture())
+        assertEquals(captor.value.getUriForDisplay(), "http://some-url/val#")
+
+        assertThatResponse()
+          .isOk()
+      }
+
+      @Test
+      void 'should resolve params even when pipeline_name is not provided'() {
+        def material = mock(GitMaterial.class)
+        def validationBean = ValidationBean.valid()
+        when(material.checkConnection(any())).thenReturn(validationBean)
+        when(materialConfigConverter.toMaterial(any(MaterialConfig.class))).thenReturn(material)
+
+        postWithApiHeader(controller.controllerBasePath(), [
+          "type"      : "git",
+          "attributes": [
+            "url": "http://some-url/val##"
+          ]
+        ])
+
+        verify(goConfigService, never()).pipelineConfigNamed(any(CaseInsensitiveString.class))
+
+        def captor = ArgumentCaptor.forClass(MaterialConfig.class)
+        verify(materialConfigConverter).toMaterial(captor.capture())
+        assertEquals(captor.value.getUriForDisplay(), "http://some-url/val#")
+
+        assertThatResponse()
+          .isOk()
       }
     }
   }

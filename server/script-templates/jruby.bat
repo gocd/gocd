c17eedffcf9e61d221af9375ca90ff3dca5f66de
Chad Wilson <chadw@thoughtworks.com>
2021-12-05 23:18:57 +0800

    Make jruby bin stubs a bit more robust when there are spaces in paths

--- server/script-templates/jruby.bat
+++ server/script-templates/jruby.bat
@@ -1,13 +1,13 @@
 @echo off
 
-<% print environment.collect { entry -> """set ${entry.key}=${entry.value}""" }.join("\n") %>
+<% print environment.collect { entry -> """set ${entry.key}=\"${entry.value}\"""" }.join("\n") %>
 rem add jruby and rubygem binstubs to PATH
-set PATH=${additionalJRubyPaths.join(File.pathSeparator)};%PATH%
+set PATH="${additionalJRubyPaths.join(File.pathSeparator)};%PATH%"
 
 "${javaExecutable}" ^
 <% print jvmArgs.collect { entry -> $/  "${entry}"/$ }.join(" ^\n") %> ^
 <% print systemProperties.collect { entry -> $/  "-D${entry}"/$ }.join(" ^\n") %> ^
   -cp ^
-  ${jrubyJar};${classpath.join(';')} ^
+  "${jrubyJar};${classpath.join(';')}" ^
   ${mainClassName} ^
   %*

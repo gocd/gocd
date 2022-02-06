f8f08be05508c2bf6f5718bc357e8e8ae9ea024a
Chad Wilson <chadw@thoughtworks.com>
2022-02-06 22:47:20 +0800

    Remove unused/dead code

--- util/src/main/java/com/thoughtworks/go/util/UrlUtil.java
+++ util/src/main/java/com/thoughtworks/go/util/UrlUtil.java
@@ -15,31 +15,16 @@
  */
 package com.thoughtworks.go.util;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URIBuilder;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
 import java.net.URLEncoder;
-import java.util.List;
-
-import static com.thoughtworks.go.util.ExceptionUtils.bomb;
+import java.nio.charset.StandardCharsets;
 
 public class UrlUtil {
-
-    private static final String UTF_8 = "UTF-8";
-
     public static String encodeInUtf8(String url) {
         String[] parts = url.split("/");
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < parts.length; i++) {
             String part = parts[i];
-            try {
-                builder.append(URLEncoder.encode(part, UTF_8));
-            } catch (UnsupportedEncodingException e) {
-                bomb(e);
-            }
+            builder.append(URLEncoder.encode(part, StandardCharsets.UTF_8));
             if (i < parts.length - 1) {
                 builder.append('/');
             }
@@ -49,34 +34,4 @@ public class UrlUtil {
         }
         return builder.toString();
     }
-
-    public static String urlWithQuery(String oldUrl, String paramName, String paramValue) throws URISyntaxException {
-        URIBuilder uriBuilder = new URIBuilder(oldUrl);
-        uriBuilder.addParameter(paramName, paramValue);
-        return uriBuilder.toString();
-    }
-
-    public static String getQueryParamFromUrl(String url, String paramName) {
-        try {
-            List<NameValuePair> queryParams = new URIBuilder(url).getQueryParams();
-            for (NameValuePair pair : queryParams) {
-                if (pair.getName().equals(paramName)) {
-                    return pair.getValue();
-                }
-            }
-            return StringUtils.EMPTY;
-        } catch (URISyntaxException e) {
-            return StringUtils.EMPTY;
-        }
-    }
-
-    public static String concatPath(String baseUrl, String path) {
-        StringBuilder builder = new StringBuilder(baseUrl);
-        if(!baseUrl.endsWith("/")) {
-            builder.append('/');
-        }
-        builder.append(path);
-        return builder.toString();
-    }
-
 }

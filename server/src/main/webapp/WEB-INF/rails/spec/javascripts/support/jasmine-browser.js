87a66e8d5f0b3387336c0b0dbe319cf0692f8fa6
Chad Wilson <chadw@thoughtworks.com>
2022-06-10 12:32:38 +0800

    Run old Jasmine tests with headless Firefox

--- server/src/main/webapp/WEB-INF/rails/spec/javascripts/support/jasmine-browser.js
+++ server/src/main/webapp/WEB-INF/rails/spec/javascripts/support/jasmine-browser.js
@@ -35,6 +35,6 @@ module.exports = {
     'random': false,
   },
   browser: {
-    name: 'chrome' === process.env.BROWSER ? "headlessChrome" : 'firefox',
+    name: 'chrome' === process.env.BROWSER ? "headlessChrome" : 'headlessFirefox',
   }
 };

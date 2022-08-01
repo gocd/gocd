c69b8c93561f8862f413817b07fb671999ee1f0c
Chad Wilson <chadw@thoughtworks.com>
2022-08-01 23:11:43 +0800

    Trim strings when parsing plugin and bundle XML
    The old pre-JAXB parser did this I believe; this makes it consistent and avoids annoying unnecessary whitespace being included in descriptions, URLs etc.

---
+++ plugin-infra/go-plugin-infra/src/main/java/com/thoughtworks/go/plugin/infra/plugininfo/TrimStringAdapter.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.thoughtworks.go.plugin.infra.plugininfo;
+
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+import org.apache.commons.lang3.StringUtils;
+
+public class TrimStringAdapter extends XmlAdapter<String, String> {
+    @Override
+    public String unmarshal(String v) {
+        return StringUtils.trim(v);
+    }
+
+    /**
+     * Not used, but included for completeness
+     */
+    @Override
+    public String marshal(String v) {
+        return StringUtils.trim(v);
+    }
+}

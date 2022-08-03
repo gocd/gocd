cde40e730dbeb7ee48bdf926454689956b64f2a6
Chad Wilson <chadw@thoughtworks.com>
2022-08-03 23:06:10 +0800

    Remove unused jquery.validate plugin
    I can't see any usage of this for a long time, let's try removing it.

--- server/src/main/webapp/WEB-INF/rails/app/assets/javascripts/application.js
+++ server/src/main/webapp/WEB-INF/rails/app/assets/javascripts/application.js
@@ -35,7 +35,6 @@
 //= require "lib/jquery-ui-1.7.3.custom.min.js"
 //= require "lib/jquery.dirtyform.js"
 //= require "lib/jquery.highlight-3.0.js"
-//= require "lib/jquery.validate-1.5.5.js"
 //= require "lib/modalbox-1.6.1.js"
 //= require "lib/trimpath-template-1.0.38.js"
 //= require "lib/lodash.js"

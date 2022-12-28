3d0663e1054390903e1b22fa0f288c6007fe0eb1
Chad Wilson <chadw@thoughtworks.com>
2022-12-28 11:51:18 +0800

    Remove gcc-libs from Alpine images
    gcc-libs is large, and provides libs that don't seem to be required for general JRE usage, or by GoCD in particular, based on https://archlinux.org/packages/core/x86_64/gcc-libs/
    
    These were possibly here as dependencies for JDK tools rather than the main JRE, or because the AdoptOpenJDK image this originally came from was more general purpose. Removing these makes it a little easier to evaluate creating an aarch64 image.

--- buildSrc/src/main/groovy/com/thoughtworks/go/build/docker/Distro.groovy
+++ buildSrc/src/main/groovy/com/thoughtworks/go/build/docker/Distro.groovy
@@ -63,7 +63,7 @@ enum Distro implements DistroBehavior {
       //
       // zlib/libz.so.1 is required by the JRE, and we need a glibc-linked version. Can probably be latest available version.
       return [
-        '# install glibc/gcc-libs/zlib for the Tanuki Wrapper, and use by glibc-linked Adoptium JREs',
+        '# install glibc/zlib for the Tanuki Wrapper, and use by glibc-linked Adoptium JREs',
         '  apk add --no-cache tzdata --virtual .build-deps curl binutils zstd',
         '  GLIBC_VER="2.34-r0"',
         '  ALPINE_GLIBC_REPO="https://github.com/sgerrand/alpine-pkg-glibc/releases/download"',
@@ -80,13 +80,6 @@ enum Distro implements DistroBehavior {
         '  apk add --no-cache /tmp/glibc-i18n-${GLIBC_VER}.apk',
         '  /usr/glibc-compat/bin/localedef --force --inputfile POSIX --charmap UTF-8 "$LANG" || true',
         '  echo "export LANG=$LANG" > /etc/profile.d/locale.sh',
-        '  curl -LfsS ${GCC_LIBS_URL} -o /tmp/gcc-libs.tar.zst',
-        '  echo "${GCC_LIBS_SHA256} */tmp/gcc-libs.tar.zst" | sha256sum -c -',
-        '  mkdir /tmp/gcc',
-        '  zstd -d /tmp/gcc-libs.tar.zst --output-dir-flat /tmp',
-        '  tar -xf /tmp/gcc-libs.tar -C /tmp/gcc',
-        '  mv /tmp/gcc/usr/lib/libgcc* /tmp/gcc/usr/lib/libstdc++* /usr/glibc-compat/lib',
-        '  strip /usr/glibc-compat/lib/libgcc_s.so.* /usr/glibc-compat/lib/libstdc++.so*',
         '  curl -LfsS ${ZLIB_URL} -o /tmp/libz.tar.zst',
         '  echo "${ZLIB_SHA256} */tmp/libz.tar.zst" | sha256sum -c -',
         '  mkdir /tmp/libz',
@@ -94,8 +87,8 @@ enum Distro implements DistroBehavior {
         '  tar -xf /tmp/libz.tar -C /tmp/libz',
         '  mv /tmp/libz/usr/lib/libz.so* /usr/glibc-compat/lib',
         '  apk del --purge .build-deps glibc-i18n',
-        '  rm -rf /tmp/*.apk /tmp/gcc /tmp/gcc-libs.tar* /tmp/libz /tmp/libz.tar* /var/cache/apk/*',
-        '# end installing glibc/gcc-libs/zlib',
+        '  rm -rf /tmp/*.apk /tmp/libz /tmp/libz.tar* /var/cache/apk/*',
+        '# end installing glibc/zlib',
       ] + super.getInstallJavaCommands(project)
     }
   },

1ceda04cbcd6b20bf072526088dd127a1feb11ef
Chad Wilson <chadw@thoughtworks.com>
2023-09-24 22:17:37 +0800

    Remove some dead Ruby test code

--- server/src/main/webapp/WEB-INF/rails/spec/support/custom_matchers.rb
+++ server/src/main/webapp/WEB-INF/rails/spec/support/custom_matchers.rb
@@ -22,21 +22,6 @@ RSpec::Matchers.define :receive_render_with do |args|
   end
 end
 
-RSpec::Matchers.define :receive_redirect_to do |expected_url|
-  match do |controller|
-    expect(controller).to receive(:redirect_to) do |actual|
-      expect(actual).to RSpec::Matchers::BuiltIn::Match.new(expected_url)
-    end
-  end
-end
-
-RSpec::Matchers.define :be_nil_or_empty do
-  match do |actual|
-    actual.nil? or actual.size == 0
-  end
-end
-
-
 class ReachedControllerError < StandardError
 end
 

99326c4d67072d47c53309edb840233ce01f78e9
Chad Wilson <chadw@thoughtworks.com>
2023-10-01 13:52:54 +0800

    Use latest FreeMarker DTD version

--- buildSrc/src/main/groovy/com/thoughtworks/go/build/docker/BuildDockerImageTask.groovy
+++ buildSrc/src/main/groovy/com/thoughtworks/go/build/docker/BuildDockerImageTask.groovy
@@ -168,7 +168,7 @@ class BuildDockerImageTask extends DefaultTask {
   }
 
   void writeTemplateToFile(String templateFile, File outputFile) {
-    Configuration configuration = new Configuration(Configuration.VERSION_2_3_28)
+    Configuration configuration = new Configuration(Configuration.VERSION_2_3_32)
     configuration.setDefaultEncoding("utf-8")
     configuration.setLogTemplateExceptions(true)
     configuration.setNumberFormat("computer")

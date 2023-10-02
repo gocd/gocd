6469b94f7a1c0d2decd204e3e052d0a8b57b2220
Chad Wilson <chadw@thoughtworks.com>
2023-10-02 22:38:51 +0800

    Upgrade Rails from v6.1 to v7.0

--- server/src/main/webapp/WEB-INF/rails/config/initializers/filter_parameter_logging.rb
+++ server/src/main/webapp/WEB-INF/rails/config/initializers/filter_parameter_logging.rb
@@ -1,22 +1,8 @@
-#
-# Copyright Thoughtworks, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # Be sure to restart your server when you modify this file.
 
-# Configure sensitive parameters which will be filtered from the log file.
+# Configure parameters to be partially matched (e.g. passw matches password) and filtered from the log file.
+# Use this to limit dissemination of sensitive information.
+# See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :password, :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
 ]

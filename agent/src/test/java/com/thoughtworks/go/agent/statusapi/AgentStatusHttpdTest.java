1706d2b9af1f47fe1d8fe38d6c30003af6383940
Ravi Kumar P <292991+ravikp@users.noreply.github.com>
2023-11-02 11:37:44 +0530

    gocd/gocd#10428 | Migrate agent health status test cases from NanoHttpd to jdk built-in http server

--- agent/src/test/java/com/thoughtworks/go/agent/statusapi/AgentStatusHttpdTest.java
+++ agent/src/test/java/com/thoughtworks/go/agent/statusapi/AgentStatusHttpdTest.java
@@ -15,18 +15,32 @@
  */
 package com.thoughtworks.go.agent.statusapi;
 
+import com.sun.net.httpserver.HttpServer;
 import com.thoughtworks.go.util.SystemEnvironment;
-import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,76 +53,166 @@ class AgentStatusHttpdTest {
 
     private AgentStatusHttpd agentStatusHttpd;
 
+    private String hostName = "0.0.0.0";
+    private int port = 8093;
+
     @BeforeEach
     void setUp() {
         this.agentStatusHttpd = new AgentStatusHttpd(systemEnvironment, new IsConnectedToServerV1(agentHealthHolder));
     }
 
-    @Test
-    void shouldReturnMethodNotAllowedOnNonGetNonHeadRequests() throws Exception {
-        when(session.getMethod()).thenReturn(NanoHTTPD.Method.POST);
-        NanoHTTPD.Response response = this.agentStatusHttpd.serve(session);
-        assertThat(response.getStatus()).isEqualTo(NanoHTTPD.Response.Status.METHOD_NOT_ALLOWED);
-        assertThat(response.getMimeType()).isEqualTo("text/plain; charset=utf-8");
-        assertThat(IOUtils.toString(response.getData(), StandardCharsets.UTF_8)).isEqualTo("This method is not allowed. Please use GET or HEAD.");
+    @AfterEach
+    void tearDown() {
+        this.agentStatusHttpd.destroy();
+        reset(systemEnvironment);
     }
 
     @Test
-    void shouldReturnNotFoundForBadUrl() throws Exception {
-        when(session.getMethod()).thenReturn(NanoHTTPD.Method.GET);
-        when(session.getUri()).thenReturn("/foo");
-
-        NanoHTTPD.Response response = this.agentStatusHttpd.serve(session);
-        assertThat(response.getStatus()).isEqualTo(NanoHTTPD.Response.Status.NOT_FOUND);
-        assertThat(response.getMimeType()).isEqualTo("text/plain; charset=utf-8");
-        assertThat(IOUtils.toString(response.getData(), StandardCharsets.UTF_8)).isEqualTo("The page you requested was not found");
+    void shouldReturnMethodNotAllowedOnNonGetNonHeadRequests() {
+        setupAgentStatusParameters();
+        startAgentStatusEndpointServer();
+        processHttpRequest(createPostRequest("/health/latest/isConnectedToServer"), (response, httpEntity) -> {
+            assertThat(statusCode(response)).isEqualTo(HttpStatus.SC_METHOD_NOT_ALLOWED);
+            assertThat(contentType(httpEntity)).isEqualTo("text/plain; charset=utf-8");
+            assertThat(responseBody(httpEntity)).isEqualTo("This method is not allowed. Please use GET or HEAD.");
+        });
     }
 
     @Test
-    void shouldRouteToIsConnectedToServerHandler() throws Exception {
-        when(session.getMethod()).thenReturn(NanoHTTPD.Method.GET);
-        when(session.getUri()).thenReturn("/health/latest/isConnectedToServer");
+    void shouldReturnNotFoundForBadUrl() {
+        setupAgentStatusParameters();
+        startAgentStatusEndpointServer();
+        processHttpRequest(createGetRequest("/foo"), (response, httpEntity) -> {
+            assertThat(statusCode(response)).isEqualTo(HttpStatus.SC_NOT_FOUND);
+            assertThat(contentType(httpEntity)).isEqualTo("text/plain; charset=utf-8");
+            assertThat(responseBody(httpEntity)).isEqualTo("The page you requested was not found");
+        });
+    }
+
+    @Test
+    void shouldRouteToIsConnectedToServerHandler() {
+        setupAgentStatusParameters();
+        startAgentStatusEndpointServer();
+        processHttpRequest(createGetRequest("/health/latest/isConnectedToServer"), (response, httpEntity) -> {
+            assertThat(statusCode(response)).isEqualTo(HttpStatus.SC_OK);
+            assertThat(contentType(httpEntity)).isEqualTo("text/plain; charset=utf-8");
+            assertThat(responseBody(httpEntity)).isEqualTo("OK!");
+        });
+    }
+
+
+    @Test
+    void shouldRouteToIsConnectedToServerV1Handler() {
         when(agentHealthHolder.hasLostContact()).thenReturn(false);
+        setupAgentStatusParameters();
+        startAgentStatusEndpointServer();
 
-        NanoHTTPD.Response response = this.agentStatusHttpd.serve(session);
-        assertThat(response.getStatus()).isEqualTo(NanoHTTPD.Response.Status.OK);
-        assertThat(response.getMimeType()).isEqualTo("text/plain; charset=utf-8");
-        assertThat(IOUtils.toString(response.getData(), StandardCharsets.UTF_8)).isEqualTo("OK!");
+        processHttpRequest(createGetRequest("/health/v1/isConnectedToServer"), (response, httpEntity) -> {
+            assertThat(statusCode(response)).isEqualTo(HttpStatus.SC_OK);
+            assertThat(contentType(httpEntity)).isEqualTo("text/plain; charset=utf-8");
+            assertThat(responseBody(httpEntity)).isEqualTo("OK!");
+        });
     }
 
     @Test
-    void shouldRouteToIsConnectedToServerV1Handler() throws Exception {
-        when(session.getMethod()).thenReturn(NanoHTTPD.Method.GET);
-        when(session.getUri()).thenReturn("/health/v1/isConnectedToServer");
-        when(agentHealthHolder.hasLostContact()).thenReturn(false);
-
-        NanoHTTPD.Response response = this.agentStatusHttpd.serve(session);
-        assertThat(response.getStatus()).isEqualTo(NanoHTTPD.Response.Status.OK);
-        assertThat(response.getMimeType()).isEqualTo("text/plain; charset=utf-8");
-        assertThat(IOUtils.toString(response.getData(), StandardCharsets.UTF_8)).isEqualTo("OK!");
+    void shouldNotInitializeServerIfSettingIsTurnedOff() {
+        try (MockedStatic<HttpServer> mockedStaticHttpServer = mockStatic(HttpServer.class)) {
+            when(systemEnvironment.getAgentStatusEnabled()).thenReturn(false);
+            agentStatusHttpd.init();
+            mockedStaticHttpServer.verifyNoInteractions();
+        }
     }
 
     @Test
-    void shouldNotInitializeServerIfSettingIsTurnedOff() throws Exception {
-        when(systemEnvironment.getAgentStatusEnabled()).thenReturn(false);
-        AgentStatusHttpd spy = spy(agentStatusHttpd);
-        spy.init();
-        verify(spy, never()).start();
+    void shouldInitializeServerIfSettingIsTurnedOn() {
+        try (MockedStatic<HttpServer> mockedStaticHttpServer = mockStatic(HttpServer.class)) {
+            var serverSocket = setupAgentStatusParameters();
+            var spiedHttpServer = spy(HttpServer.class);
+            when(spiedHttpServer.getAddress()).thenReturn(serverSocket);
+
+            mockedStaticHttpServer.when(() -> HttpServer.create(eq(serverSocket), anyInt())).thenReturn(spiedHttpServer);
+
+            agentStatusHttpd.init();
+
+            verify(spiedHttpServer).start();
+        }
     }
 
     @Test
-    void shouldInitializeServerIfSettingIsTurnedOn() throws Exception {
+    void initShouldNotBlowUpIfServerDoesNotStart() {
+
+        try (MockedStatic<HttpServer> mockedStaticHttpServer = mockStatic(HttpServer.class)) {
+            setupAgentStatusParameters();
+
+            var serverSocket = new InetSocketAddress(hostName, port);
+            var spiedHttpServer = spy(HttpServer.class);
+            doThrow(new RuntimeException("Server had a problem starting up!")).when(spiedHttpServer).start();
+
+            mockedStaticHttpServer.when(() -> HttpServer.create(eq(serverSocket), anyInt())).thenReturn(spiedHttpServer);
+
+            assertThatNoException().isThrownBy(() -> agentStatusHttpd.init());
+        }
+    }
+
+    private InetSocketAddress setupAgentStatusParameters() {
         when(systemEnvironment.getAgentStatusEnabled()).thenReturn(true);
-        AgentStatusHttpd spy = spy(agentStatusHttpd);
-        spy.init();
-        verify(spy).start();
+        when(systemEnvironment.getAgentStatusHostname()).thenReturn(hostName);
+        when(systemEnvironment.getAgentStatusPort()).thenReturn(port);
+
+        return new InetSocketAddress(hostName, port);
     }
 
-    @Test
-    void initShouldNotBlowUpIfServerDoesNotStart() throws Exception {
-        when(systemEnvironment.getAgentStatusEnabled()).thenReturn(true);
-        AgentStatusHttpd spy = spy(agentStatusHttpd);
-        doThrow(new RuntimeException("Server had a problem starting up!")).when(spy).start();
-        assertThatCode(spy::init).doesNotThrowAnyException();
+    private void startAgentStatusEndpointServer() {
+        try {
+            this.agentStatusHttpd.init();
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to start the server for the test as a pre-requisite", e);
+        }
+    }
+    
+    private static String responseBody(HttpEntity httpEntity) throws IOException {
+        return EntityUtils.toString(httpEntity, StandardCharsets.UTF_8);
+    }
+
+    private static String contentType(HttpEntity httpEntity) {
+        return httpEntity.getContentType().getValue();
+    }
+
+    private static int statusCode(HttpResponse response) {
+        return response.getStatusLine().getStatusCode();
+    }
+
+    private HttpUriRequest createGetRequest(String uri) {
+        return new HttpGet(agentStatusUrl(uri));
+    }
+
+    private HttpUriRequest createPostRequest(String uri) {
+        return new HttpPost(agentStatusUrl(uri));
+    }
+
+    private String agentStatusUrl(String path) {
+        return String.format("http://%s:%d/%s", hostName, port, path);
+    }
+
+    private void processHttpRequest(HttpUriRequest httpRequest, HttpResponseConsumer consumer) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        try {
+            response = httpclient.execute(httpRequest);
+            HttpEntity entity = response.getEntity();
+            consumer.accept(response, entity);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to retrieve the http response", e);
+        } finally {
+            try {
+                if (response != null) response.close();
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to close the response", e);
+            }
+        }
+    }
+
+    private interface HttpResponseConsumer {
+        void accept(HttpResponse response, HttpEntity httpEntity) throws Exception;
     }
 }

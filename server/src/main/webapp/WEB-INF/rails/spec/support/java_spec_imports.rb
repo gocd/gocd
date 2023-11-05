7433e67ec750e7501dca35936042a55e60b2e13f
Chad Wilson <chadw@thoughtworks.com>
2023-11-05 14:10:07 +0800

    Clean up unnecessary JRuby imports of Java classses
    Most of these were only needed historically for long-removed Rails controllers/pages. Others
    are now only used/required for test code, so imports moved to specs.

--- server/src/main/webapp/WEB-INF/rails/spec/support/java_spec_imports.rb
+++ server/src/main/webapp/WEB-INF/rails/spec/support/java_spec_imports.rb
@@ -31,4 +31,51 @@ module JavaSpecImports
   java_import com.thoughtworks.go.domain.valuestreammap.PipelineDependencyNode unless defined? PipelineDependencyNode
   java_import com.thoughtworks.go.domain.valuestreammap.SCMDependencyNode unless defined? SCMDependencyNode
   java_import com.thoughtworks.go.server.cache.GoCache unless defined? GoCache
+  java_import com.thoughtworks.go.domain.buildcause.BuildCause unless defined? BuildCause
+  java_import com.thoughtworks.go.domain.ConfigErrors unless defined? ConfigErrors
+  java_import com.thoughtworks.go.config.BasicCruiseConfig unless defined? BasicCruiseConfig
+  java_import com.thoughtworks.go.domain.JobInstances unless defined? JobInstances
+
+  java_import com.thoughtworks.go.domain.MaterialRevisions unless defined? MaterialRevisions
+  java_import com.thoughtworks.go.domain.MaterialRevision unless defined? MaterialRevision
+  java_import com.thoughtworks.go.domain.materials.dependency.DependencyMaterialRevision unless defined? DependencyMaterialRevision
+  java_import com.thoughtworks.go.domain.materials.Modification unless defined? Modification
+  java_import com.thoughtworks.go.domain.materials.ModifiedAction unless defined? ModifiedAction
+  java_import com.thoughtworks.go.domain.materials.ModifiedFile unless defined? ModifiedFile
+
+  java_import com.thoughtworks.go.config.materials.svn.SvnMaterial unless defined? SvnMaterial
+  java_import com.thoughtworks.go.config.materials.git.GitMaterial unless defined? GitMaterial
+
+  java_import com.thoughtworks.go.config.PipelineConfigs unless defined? PipelineConfigs
+
+  java_import com.thoughtworks.go.domain.valuestreammap.ValueStreamMap unless defined? ValueStreamMap
+  java_import com.thoughtworks.go.domain.PipelineIdentifier unless defined? PipelineIdentifier
+  java_import com.thoughtworks.go.domain.PipelinePauseInfo unless defined? PipelinePauseInfo
+
+  java_import com.thoughtworks.go.domain.Stages unless defined? Stages
+  java_import com.thoughtworks.go.domain.Stage unless defined? Stage
+  java_import com.thoughtworks.go.config.TrackingTool unless defined? TrackingTool
+  java_import com.thoughtworks.go.presentation.pipelinehistory.JobHistory unless defined? JobHistory
+  java_import com.thoughtworks.go.presentation.pipelinehistory.NullStageHistoryItem unless defined? NullStageHistoryItem
+  java_import com.thoughtworks.go.presentation.pipelinehistory.PipelineInstanceModel unless defined? PipelineInstanceModel
+  java_import com.thoughtworks.go.presentation.pipelinehistory.PipelineModel unless defined? PipelineModel
+  java_import com.thoughtworks.go.presentation.pipelinehistory.StageHistoryPage unless defined? StageHistoryPage
+  java_import com.thoughtworks.go.presentation.pipelinehistory.StageInstanceModels unless defined? StageInstanceModels
+  java_import com.thoughtworks.go.presentation.pipelinehistory.StageInstanceModel unless defined? StageInstanceModel
+  java_import com.thoughtworks.go.server.domain.JobDurationStrategy unless defined? JobDurationStrategy
+  java_import com.thoughtworks.go.server.domain.Username unless defined? Username
+  java_import com.thoughtworks.go.serverhealth.HealthStateScope unless defined? HealthStateScope
+  java_import com.thoughtworks.go.serverhealth.HealthStateType unless defined? HealthStateType
+  java_import com.thoughtworks.go.server.ui.JobInstanceModel unless defined? JobInstanceModel
+  java_import com.thoughtworks.go.server.ui.StageSummaryModel unless defined? StageSummaryModel
+  java_import com.thoughtworks.go.server.ui.StageSummaryModels unless defined? StageSummaryModels
+  java_import com.thoughtworks.go.server.util.Pagination unless defined? Pagination
+  java_import org.joda.time.Duration unless defined? Duration
+  java_import com.thoughtworks.go.domain.GoConfigRevision unless defined? GoConfigRevision
+  java_import com.thoughtworks.go.util.TimeProvider unless defined? TimeProvider
+  java_import com.thoughtworks.go.plugin.infra.plugininfo.GoPluginDescriptor unless defined? GoPluginDescriptor
+  java_import com.thoughtworks.go.plugin.domain.analytics.SupportedAnalytics unless defined? SupportedAnalytics
+  java_import com.thoughtworks.go.plugin.domain.analytics.AnalyticsPluginInfo unless defined? AnalyticsPluginInfo
+  java_import com.thoughtworks.go.plugin.domain.common.CombinedPluginInfo unless defined? CombinedPluginInfo
+
 end

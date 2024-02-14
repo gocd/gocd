79c60a2e3d433ce39b7743dc4fa80d06ad91556f
Chad Wilson <chadw@thoughtworks.com>
2024-02-14 11:32:14 +0800

    Make fields final that support synchronization

--- common/src/main/java/com/thoughtworks/go/server/domain/AgentInstances.java
+++ common/src/main/java/com/thoughtworks/go/server/domain/AgentInstances.java
@@ -45,9 +45,9 @@ import static org.apache.commons.lang3.StringUtils.join;
 public class AgentInstances implements Iterable<AgentInstance> {
     private SystemEnvironment systemEnvironment;
 
-    private Map<String, AgentInstance> uuidToAgentInstanceMap = new ConcurrentHashMap<>();
+    private final Map<String, AgentInstance> uuidToAgentInstanceMap = new ConcurrentHashMap<>();
 
-    private AgentStatusChangeListener agentStatusChangeListener;
+    private final AgentStatusChangeListener agentStatusChangeListener;
 
     public AgentInstances(AgentStatusChangeListener listener) {
         this.agentStatusChangeListener = listener;

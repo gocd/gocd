740cb1b330f5fee876f502e577c9a77adc177829
Joshua Li <joshuarli98@gmail.com>
2024-05-29 12:56:13 -0700

    [skip ci] feedback

--- buildSrc/src/main/resources/gocd-docker-server/cron/cleanup-old-artifacts.sh
+++ buildSrc/src/main/resources/gocd-docker-server/cron/cleanup-old-artifacts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# 14 day retention
-echo "deleting the following artifact files older than 14 days:"
-find /godata/artifacts/pipelines/ -type f -ctime +14 -print -delete
+# 30 day retention
+echo "deleting the following artifact files older than 30 days:"
+find /godata/artifacts/pipelines/ -type f -mtime +30 -print -delete
 
 find /godata/artifacts/pipelines/ -type d -empty -delete

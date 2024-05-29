d5b52c48589d1e5899423af6820f51a69884aec9
Joshua Li <joshuarli98@gmail.com>
2024-05-29 13:41:27 -0700

    we did it

--- buildSrc/src/main/resources/gocd-docker-server/cron/cleanup-old-artifacts.sh
+++ buildSrc/src/main/resources/gocd-docker-server/cron/cleanup-old-artifacts.sh
@@ -1,8 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-# 30 day retention
-echo "deleting the following artifact files older than 30 days:"
-find /godata/artifacts/pipelines/ -type f -mtime +30 -print -delete
+set -eu
+trap 'echo "ERROR $?"' ERR
+# tag our output and send it to gcloud-logging, too:
+gcloud_logging=/proc/1/fd/2  # stderr of our root process
+exec >& >(logger -st "[$(basename $0)]" | tee "$gcloud_logging")
+HERE="$(dirname "$0")"
 
-find /godata/artifacts/pipelines/ -type d -empty -delete
+. "$HERE/.tfvars"
+PATH="$PATH:$HOME/prefix/google-cloud-sdk/bin"
+dest="$tf_backup_bucket/gocd-artifact-archive"
 
+main() {
+    set -x
+
+    : "Collecting the following artifact files older than 30 days:"
+    # rather than -exec zip archive.zip {} + which can run into exec length limits
+    # we assume no linebreaks in file names and just go with piping -print
+    dt="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
+    find /godata/artifacts/pipelines/ -type f -mtime +30 -print > files.txt
+    zip "${dt}.zip" -@ < files.txt
+
+    # gsutil is deprecated: https://cloud.google.com/storage/docs/gsutil#should-you-use
+    : "Uploading to GCS bucket ${dest}."
+    gcloud storage cp "${dt}.zip" "$dest" && rm "${dt}.zip"
+
+    : "Deleting from disk:"
+    xargs rm -v < files.txt
+    find /godata/artifacts/pipelines/ -type d -empty -delete
+}
+
+main "$@"

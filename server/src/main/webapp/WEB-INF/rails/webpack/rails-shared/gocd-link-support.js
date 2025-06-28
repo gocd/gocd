91247eb851e9f3c540e2317610c1fcffb0e95388
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-06-28 16:39:06 +1200

    Remove dead link support
    The Pipeline Instance page links only seemed to work from old/legacy code which has long since been removed. These routes 404 now.

--- server/src/main/webapp/WEB-INF/rails/webpack/rails-shared/gocd-link-support.js
+++ server/src/main/webapp/WEB-INF/rails/webpack/rails-shared/gocd-link-support.js
@@ -50,11 +50,6 @@
       openLinkInNewTab(jobDetailsPagePath);
     },
 
-    "pipeline_instance_page": function pipelineInstancePage(params) {
-      var pipelineInstancePath = '/go/internal/pipelines/' + pipelineName(params) + '/' + pipelineCounter(params);
-      openLinkInNewTab(pipelineInstancePath);
-    },
-
     "vsm_page": function vsmPage(params) {
       var vsmPath = '/go/pipelines/value_stream_map/' + pipelineName(params) + '/' + pipelineCounter(params);
       openLinkInNewTab(vsmPath);

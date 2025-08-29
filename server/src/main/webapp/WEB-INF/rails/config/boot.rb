f94a2ee6b445f995d38922b97085b0ff8fdfe1ee
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-08-29 12:25:02 +0800

    Bump jruby-complete from 9.4.13.0 to 9.4.14.0

--- server/src/main/webapp/WEB-INF/rails/config/boot.rb
+++ server/src/main/webapp/WEB-INF/rails/config/boot.rb
@@ -7,16 +7,3 @@ if ENV["RAILS_ENV"] == "production" and not ENV.has_key?("RAILS_GROUPS")
 else
   require "bundler/setup"
 end
-
-# workaround JRuby 9.4.13.0 issue with the require used within win32/registry.rb, used by resolv.rb via net/http and Capybara
-# See https://github.com/jruby/jruby/issues/8866
-if ENV["RAILS_ENV"] == "test" and ::RbConfig::CONFIG['host_os'] =~ /mswin/
-  module Kernel
-    alias_method :original_require, :require
-
-    def require(path)
-      original_require(path == 'Win32API' ? 'win32api' : path)
-    end
-  end
-end
-

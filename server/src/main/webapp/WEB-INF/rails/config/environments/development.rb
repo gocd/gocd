2a0005e883ce4acf4a3cf6a692497bef8e2a6283
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-09-09 16:50:19 +0800

    Fix development-mode rendering of stage details/VSM etc

--- server/src/main/webapp/WEB-INF/rails/config/environments/development.rb
+++ server/src/main/webapp/WEB-INF/rails/config/environments/development.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
-  config.action_view.annotate_rendered_view_with_filenames = true
+  config.action_view.annotate_rendered_view_with_filenames = false # Breaks inclusion of _body_data_attrs attribute-only fragment
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true

07f9367169f7c9f3b3b7f8d9d7a0b036d60028bb
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-03-10 16:48:50 +0800

    Revert "chore: try removing yarn extensions for windows again"
    This reverts commit 12b3b03588074ed121e48792950ee3ffbd3dab5a.

--- buildSrc/src/main/groovy/com/thoughtworks/go/build/YarnInstallTask.groovy
+++ buildSrc/src/main/groovy/com/thoughtworks/go/build/YarnInstallTask.groovy
@@ -64,7 +64,7 @@ class YarnInstallTask extends DefaultTask {
     execOperations.exec { execTask ->
       execTask.environment("FORCE_COLOR", "true")
       execTask.workingDir = this.getWorkingDir()
-      execTask.commandLine = ["yarn", "install", OperatingSystem.current().isWindows() ? "--no-immutable" : "--immutable"]
+      execTask.commandLine = OperatingSystem.current().isWindows() ? ["yarn.exe", "install", "--no-immutable"] : ["yarn", "install", "--immutable"]
     }  
   }
 }
